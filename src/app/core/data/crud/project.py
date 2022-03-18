13c498785cf7c80589b6ce9f1cec5689bd935ec7
floschne <florian.schneider-1@uni-hamburg.de>
2022-03-18 13:41:56 +0100

    extended create method for projects to assoc sys user and create sys codes

--- src/app/core/data/crud/project.py
+++ src/app/core/data/crud/project.py
@@ -1,8 +1,10 @@
+from fastapi.encoders import jsonable_encoder
 from sqlalchemy import delete
 from sqlalchemy.orm import Session
 
+from app.core.data.crud.code import crud_code
 from app.core.data.crud.crud_base import CRUDBase
-from app.core.data.crud.user import crud_user
+from app.core.data.crud.user import crud_user, SYSTEM_USER_ID
 from app.core.data.dto.project import ProjectCreate, ProjectUpdate
 from app.core.data.orm.code import CodeORM
 from app.core.data.orm.project import ProjectORM
@@ -11,6 +13,24 @@ from app.core.data.orm.user import UserORM
 
 
 class CRUDProject(CRUDBase[ProjectORM, ProjectCreate, ProjectUpdate]):
+
+    def create(self, db: Session, *, create_dto: ProjectCreate) -> ProjectORM:
+        # TODO Flo: create ES indices
+        # 1) create the project
+        dto_obj_data = jsonable_encoder(create_dto)
+        db_obj = self.model(**dto_obj_data)
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+
+        # 2) associate the system user
+        self.associate_user(db=db, id=db_obj.id, user_id=SYSTEM_USER_ID)
+
+        # 3) create system codes
+        crud_code.create_system_codes_for_project(db=db, proj_id=db_obj.id)
+
+        return db_obj
+
     def remove_all_source_documents(self, db: Session, *, id: int) -> ProjectORM:
         db_obj = self.read(db=db, id=id)
         statement = delete(SourceDocumentORM).where(SourceDocumentORM.project_id == db_obj.id)
