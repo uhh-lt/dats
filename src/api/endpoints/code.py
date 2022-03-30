144b54a1ff28964394973ecca5f5ba83c790ec0d
floschne <florian.schneider-1@uni-hamburg.de>
2022-03-30 17:53:00 +0200

    renamed id parameter to code_id in code endpoint

--- src/api/endpoints/code.py
+++ src/api/endpoints/code.py
@@ -34,69 +34,69 @@ async def get_code_by_current_code_id(*,
                                       current_code_id: int) -> Optional[CodeRead]:
     # TODO Flo: only if the user has access?
     cc_db_obj = crud_current_code.read(db=db, id=current_code_id)
-    c_db_obj = crud_code.read(db=db, id=cc_db_obj.code_id)
+    c_db_obj = crud_code.read(db=db, id=cc_db_obj.id)
     return CodeRead.from_orm(c_db_obj)
 
 
-@router.get("/{id}", tags=tags,
+@router.get("/{code_id}", tags=tags,
             response_model=Optional[CodeRead],
             summary="Returns the Code",
             description="Returns the Code with the given ID.")
 async def get_by_id(*,
                     db: Session = Depends(SQLService().get_db_session),
-                    id: int) -> Optional[CodeRead]:
+                    code_id: int) -> Optional[CodeRead]:
     # TODO Flo: only if the user has access?
-    db_obj = crud_code.read(db=db, id=id)
+    db_obj = crud_code.read(db=db, id=code_id)
     return CodeRead.from_orm(db_obj)
 
 
-@router.patch("/{id}", tags=tags,
+@router.patch("/{code_id}", tags=tags,
               response_model=CodeRead,
               summary="Updates the Code",
               description="Updates the Code with the given ID.")
 async def update_by_id(*,
                        db: Session = Depends(SQLService().get_db_session),
-                       id: int,
+                       code_id: int,
                        code: CodeUpdate) -> Optional[CodeRead]:
     # TODO Flo: only if the user has access?
-    db_obj = crud_code.update(db=db, id=id, update_dto=code)
+    db_obj = crud_code.update(db=db, id=code_id, update_dto=code)
     return CodeRead.from_orm(db_obj)
 
 
-@router.delete("/{id}", tags=tags,
+@router.delete("/{code_id}", tags=tags,
                response_model=Optional[CodeRead],
                summary="Deletes the Code",
                description="Deletes the Code with the given ID.")
 async def delete_by_id(*,
                        db: Session = Depends(SQLService().get_db_session),
-                       id: int) -> Optional[CodeRead]:
+                       code_id: int) -> Optional[CodeRead]:
     # TODO Flo: only if the user has access?
-    db_obj = crud_code.remove(db=db, id=id)
+    db_obj = crud_code.remove(db=db, id=code_id)
     return CodeRead.from_orm(db_obj)
 
 
-@router.put("/{id}/memo", tags=tags,
+@router.put("/{code_id}/memo", tags=tags,
             response_model=Optional[MemoReadCode],
             summary="Adds a Memo to the Code",
             description="Adds a Memo to the Code with the given ID if it exists")
 async def add_memo(*,
                    db: Session = Depends(SQLService().get_db_session),
-                   id: int,
+                   code_id: int,
                    memo: MemoCreate) -> Optional[MemoReadCode]:
     # TODO Flo: only if the user has access?
-    db_obj = crud_memo.create_for_code(db=db, code_id=id, create_dto=memo)
+    db_obj = crud_memo.create_for_code(db=db, id=code_id, create_dto=memo)
     memo_as_in_db_dto = MemoInDB.from_orm(db_obj)
     attached_code = db_obj.attached_to.code
     return MemoReadCode(**memo_as_in_db_dto.dict(exclude={"attached_to"}), attached_code_id=attached_code.id)
 
 
-@router.get("/{id}/memo", tags=tags,
+@router.get("/{code_id}/memo", tags=tags,
             response_model=Optional[MemoReadCode],
             summary="Returns the Memo attached to the Code",
             description="Returns the Memo attached to the Code with the given ID if it exists.")
 async def get_memo(*,
                    db: Session = Depends(SQLService().get_db_session),
-                   id: int) -> Optional[MemoReadCode]:
-    code_db_obj = crud_code.read(db=db, id=id)
+                   code_id: int) -> Optional[MemoReadCode]:
+    code_db_obj = crud_code.read(db=db, id=code_id)
     memo_as_in_db_dto = MemoInDB.from_orm(code_db_obj.object_handle.attached_memo)
     return MemoReadCode(**memo_as_in_db_dto.dict(exclude={"attached_to"}), attached_code_id=code_db_obj.id)
