2427124cbdda3e2e4071964abc4534e2cf2c508a
floschne <florian.schneider-1@uni-hamburg.de>
2022-05-13 14:14:45 +0200

    fixed test es script

--- src/test_es.sh
+++ src/test_es.sh
@@ -1,18 +1,26 @@
-#!/bin/sh
+#!/bin/bash
 
+ES_HOST=${ES_HOST:-elasticsearch}
+ES_PORT=${ES_PORT:-9200}
 ES="${ES_HOST}:${ES_PORT}"
 ES_MIN_HEALTH=${ES_MIN_HEALTH:-75}
 
-while [ "$(curl -s -o /dev/null -I -w "%{http_code}" "${ES}")" -ne 200 ]; do
+echo "Trying to reach ElasticSearch via: ${ES}"
+echo "Minimum Health Requirement: ${ES_MIN_HEALTH}"
+
+while [[ "$(curl -s -o /dev/null -I -w "%{http_code}" "${ES}")" -ne 200 ]]; do
   echo "ElasticSearch not reachable at ${ES}!"
   curl -sSI "${ES}"
   echo "Retrying in 1s!"
   sleep 1
 done
 
+CURRENT_HEALTH="$(curl -s "${ES}"/_cat/health | tail -c 7 | cut -f1 -d "." | xargs)"
+
 # check elasticsearch health
-while [ "$(curl -s "${ES}"/_cat/health | cut -f3 -d "-" | cut -f1 -d ".")" -lt ${ES_MIN_HEALTH} ]; do
-  echo "ElasticSearch ${ES} not healthy: $(curl -sS "${ES}"/_cat/health)"
+while [[ "$CURRENT_HEALTH" -le "$ES_MIN_HEALTH" ]]; do
+  echo "ElasticSearch ${ES} not healthy! Current Health Status: $(curl -sS "${ES}"/_cat/health)"
   echo "Retrying in 1s!"
   sleep 1
+  CURRENT_HEALTH="$(curl -s "${ES}"/_cat/health | tail -c 7 | cut -f1 -d "." | xargs)"
 done
