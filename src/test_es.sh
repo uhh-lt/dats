6c0ce40230475b840bb862fea300e1e3620103d2
floschne <florian.schneider-1@uni-hamburg.de>
2022-05-13 12:52:42 +0200

    improved test es script

--- src/test_es.sh
+++ src/test_es.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
-set -e
+ES="${ES_HOST}:${ES_PORT}"
+ES_MIN_HEALTH=${ES_MIN_HEALTH:-75}
+
+while [ "$(curl -s -o /dev/null -I -w "%{http_code}" "${ES}")" -ne 200 ]; do
+  echo "ElasticSearch not reachable at ${ES}!"
+  curl -sSI "${ES}"
+  echo "Retrying in 1s!"
+  sleep 1
+done
 
 # check elasticsearch health
-while ! curl -s "${ES_HOST}:${ES_PORT}"/_cat/health | grep -q "100.0%"; do
-  echo "ElasticSearch ${ES_HOST}:${ES_PORT} not healthy: $(curl -sS "${ES_HOST}:${ES_PORT}"/_cat/health)"
+while [ "$(curl -s "${ES}"/_cat/health | cut -f3 -d "-" | cut -f1 -d ".")" -lt ${ES_MIN_HEALTH} ]; do
+  echo "ElasticSearch ${ES} not healthy: $(curl -sS "${ES}"/_cat/health)"
   echo "Retrying in 1s!"
   sleep 1
 done
