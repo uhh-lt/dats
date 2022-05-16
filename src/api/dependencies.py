6e9cde8097ea67d5511c04450f25c50154e967b4
floschne <florian.schneider-1@uni-hamburg.de>
2022-05-16 11:48:28 +0200

    fixed skip limit params description

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -18,12 +18,12 @@ reusable_oauth2_scheme = OAuth2PasswordBearer(tokenUrl=conf.api.auth.jwt.token_u
 
 
 async def skip_limit_params(skip: Optional[int] = Query(title="Skip",
-                                                        description="The maximum number of returned elements",
+                                                        description="The number of elements to skip (offset)",
                                                         ge=0,
                                                         le=10e6,
                                                         default=0),
                             limit: Optional[int] = Query(title="Limit",
-                                                         description="The number of elements to skip (offset)",
+                                                         description="The maximum number of returned elements",
                                                          ge=1,
                                                          le=1000,
                                                          default=100)) -> Dict[str, int]:
