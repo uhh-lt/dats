eb768890a7725c387691391f286c4afb8343d182
floschne <florian.schneider-1@uni-hamburg.de>
2022-08-08 10:43:38 -0400

    first sketch of feedback in redis

--- src/app/core/db/redis_service.py
+++ src/app/core/db/redis_service.py
@@ -1,6 +1,11 @@
+import uuid
+from datetime import datetime
+from typing import Optional, List
+
 import redis
 from loguru import logger
 
+from app.core.data.dto.feedback import FeedbackCreate, FeedbackRead
 from app.util.singleton_meta import SingletonMeta
 from config import conf
 
@@ -30,6 +35,10 @@ class RedisService(metaclass=SingletonMeta):
         for client in self.__clients.values():
             client.close()
 
+    @staticmethod
+    def _generate_random_key() -> str:
+        return str(uuid.uuid4())
+
     def _get_client(self, typ: str):
         if not typ.lower() in self.__clients:
             raise KeyError(f"Redis Client '{typ.lower()}' does not exist!")
@@ -40,3 +49,33 @@ class RedisService(metaclass=SingletonMeta):
         logger.warning(f"Flushing Redis Client DB '{typ}'!")
         client.flushdb()
         client.save()
+
+    def store_feedback(self, feedback: FeedbackCreate) -> Optional[FeedbackRead]:
+        client = self._get_client('feedback')
+        key = self._generate_random_key()
+        fb = FeedbackRead(id=key,
+                          user_content=feedback.user_content,
+                          user_id=feedback.user_id,
+                          created=datetime.now())
+        if client.set(key.encode('utf-8'), fb.json()) != 1:
+            logger.error(f"Cannot store Feedback!")
+            return None
+
+        logger.debug(f"Successfully stored Feedback!")
+
+        return fb
+
+    def load_feedback(self, key: str) -> Optional[FeedbackRead]:
+        client = self._get_client('feedback')
+        fb = await client.get(key.encode('utf-8'))
+        if fb is None:
+            logger.error(f"Feedback with ID {key} does not exist!")
+            return None
+        else:
+            logger.debug(f"Successfully loaded Feedback {key}")
+            return FeedbackRead.parse_raw(fb)
+
+    @logger.catch(reraise=True)
+    def get_all_feedbacks(self) -> List[FeedbackRead]:
+        client = self._get_client('feedback')
+        return [self.load_feedback(str(key, 'utf-8')) for key in client.keys()]
