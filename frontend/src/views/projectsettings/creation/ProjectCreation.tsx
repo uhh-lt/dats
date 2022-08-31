e741a77c83eab049817380d10aca546b78ed7a8d
Tim Fischer <tim.fischer@uni-hamburg.de>
2022-08-31 15:48:19 +0200

    validate that project name does not contain spaces

--- frontend/src/views/projectsettings/creation/ProjectCreation.tsx
+++ frontend/src/views/projectsettings/creation/ProjectCreation.tsx
@@ -87,6 +87,7 @@ function ProjectCreation() {
               fullWidth
               {...register("name", {
                 required: "Project name is required",
+                validate: (value: string) => !/\s/g.test(value) || "Project name must not contain spaces",
               })}
               error={Boolean(errors.name)}
               helperText={<ErrorMessage errors={errors} name="name" />}
