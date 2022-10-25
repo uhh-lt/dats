ddf5a1e583bda2b7aae2edcd3d993fba9f7cb201
floschne <florian.schneider-1@uni-hamburg.de>
2022-10-25 18:15:23 +0000

    Large docker improvements and refactoring   - improved dockerfiles by adding backend deps container   - added support for GPU   - improved docker build script   - removed GPU dependend python deps from poetry and force installed them directly via pip in deps Dockerfile

--- backend/docker_build.sh
+++ backend/docker_build.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-PS3="Which image? "
-select image in backend_api celery_text_worker celery_image_worker celery_archive_worker celery_simsearch_worker; do
+PS3="Which image(s)? "
+select image in all_but_deps backend_deps backend_api celery_text_worker celery_image_worker celery_archive_worker celery_simsearch_worker; do
   case $image in
+  all_but_deps)
+    break
+    ;;
+  backend_deps)
+    break
+    ;;
   backend_api)
     break
     ;;
@@ -26,48 +32,51 @@ select image in backend_api celery_text_worker celery_image_worker celery_archiv
 done
 echo
 
-PS3="Install Jupyter (for dev)? "
-select jupyter in true false; do
-  case $jupyter in
-  true)
-    break
-    ;;
-  false)
-    break
-    ;;
-  *)
-    echo "Invalid option $REPLY"
-    ;;
-  esac
+if [ $image == backend_deps ]; then
+  PS3="Install Jupyter (for dev)? "
+  select jupyter in true false; do
+    case $jupyter in
+    true)
+      break
+      ;;
+    false)
+      break
+      ;;
+    *)
+      echo "Invalid option $REPLY"
+      ;;
+    esac
+    echo
+  done
   echo
-done
-echo
 
-PS3="Which is the target platform? "
-select platform in mac_m1 debian; do
-  case $platform in
-  debian)
-    tini="tini"
-    break
-    ;;
-  mac_m1)
-    tini="tini-arm64"
-    break
-    ;;
-  *)
-    echo "Invalid option $REPLY"
-    ;;
-  esac
+  PS3="Which is the target platform? "
+  select platform in mac_m1 debian; do
+    case $platform in
+    debian)
+      tini="tini"
+      break
+      ;;
+    mac_m1)
+      tini="tini-arm64"
+      break
+      ;;
+    *)
+      echo "Invalid option $REPLY"
+      ;;
+    esac
+    echo
+  done
   echo
-done
-echo
+
+fi
 
 read -r -p "docker tag: " docker_tag
 echo
 
 PS3="Push after build? "
 select push in true false; do
-  case $jupyter in
+  case $push in
   true)
     break
     ;;
@@ -85,42 +94,64 @@ echo
 echo "Starting build with the following settings: "
 echo "--------------"
 echo "Image: ${image}"
-echo "Build Jupyter: ${jupyter}"
-echo "Target Platform: ${platform}"
-echo "docker_tag: ${docker_tag}"
+if [ $image == backend_deps ]; then
+  echo "Build Jupyter: ${jupyter}"
+  echo "Target Platform: ${platform}"
+fi
+echo "docker tag: ${docker_tag}"
 echo "push after build: ${push}"
 echo "--------------"
 
 read -p "Are you sure (y/n) ? " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-  if [ $image == backend_api ]; then
-    docker build -f Dockerfile_backend_api --build-arg INSTALL_JUPYTER="${jupyter}" --build-arg TINI_BINARY="${tini}" -t uhhlt/dwts_backend_api:"${docker_tag}" .
+  if [ $image == backend_deps ]; then
+    docker build -f Dockerfile_backend_deps --build-arg INSTALL_JUPYTER="${jupyter}" --build-arg TINI_BINARY="${tini}" -t uhhlt/dwts_backend_deps:"${docker_tag}" .
+
+    if [ $push == true ]; then
+      docker login && docker push uhhlt/dwts_backend_deps:"${docker_tag}"
+    fi
+  elif [ $image == backend_api ]; then
+    docker build -f Dockerfile_backend_api -t uhhlt/dwts_backend_api:"${docker_tag}" .
 
     if [ $push == true ]; then
       docker login && docker push uhhlt/dwts_backend_api:"${docker_tag}"
     fi
   elif [ $image == celery_text_worker ]; then
-    docker build -f Dockerfile_celery_text_worker --build-arg INSTALL_JUPYTER="${jupyter}" --build-arg TINI_BINARY="${tini}" -t uhhlt/dwts_backend_celery_text:"${docker_tag}" .
+    docker build -f Dockerfile_celery_text_worker -t uhhlt/dwts_backend_celery_text:"${docker_tag}" .
 
     if [ $push == true ]; then
       docker login && docker push uhhlt/dwts_backend_celery_text:"${docker_tag}"
     fi
   elif [ $image == celery_image_worker ]; then
-    docker build -f Dockerfile_celery_image_worker --build-arg INSTALL_JUPYTER="${jupyter}" --build-arg TINI_BINARY="${tini}" -t uhhlt/dwts_backend_celery_image:"${docker_tag}" .
+    docker build -f Dockerfile_celery_image_worker -t uhhlt/dwts_backend_celery_image:"${docker_tag}" .
 
     if [ $push == true ]; then
       docker login && docker push uhhlt/dwts_backend_celery_image:"${docker_tag}"
     fi
   elif [ $image == celery_archive_worker ]; then
-    docker build -f Dockerfile_celery_archive_worker --build-arg INSTALL_JUPYTER="${jupyter}" --build-arg TINI_BINARY="${tini}" -t uhhlt/dwts_backend_celery_archive:"${docker_tag}" .
+    docker build -f Dockerfile_celery_archive_worker -t uhhlt/dwts_backend_celery_archive:"${docker_tag}" .
 
     if [ $push == true ]; then
       docker login && docker push uhhlt/dwts_backend_celery_archive:"${docker_tag}"
     fi
   elif [ $image == celery_simsearch_worker ]; then
-    docker build -f Dockerfile_celery_simsearch_worker --build-arg INSTALL_JUPYTER="${jupyter}" --build-arg TINI_BINARY="${tini}" -t uhhlt/dwts_backend_celery_simsearch:"${docker_tag}" .
+    docker build -f Dockerfile_celery_simsearch_worker -t uhhlt/dwts_backend_celery_simsearch:"${docker_tag}" .
 
     if [ $push == true ]; then
+      docker login && docker push uhhlt/dwts_backend_celery_simsearch:"${docker_tag}"
+    fi
+  elif [ $image == all_but_deps ]; then
+    docker build -f Dockerfile_backend_api -t uhhlt/dwts_backend_api:"${docker_tag}" .
+    docker build -f Dockerfile_celery_text_worker -t uhhlt/dwts_backend_celery_text:"${docker_tag}" .
+    docker build -f Dockerfile_celery_image_worker -t uhhlt/dwts_backend_celery_image:"${docker_tag}" .
+    docker build -f Dockerfile_celery_archive_worker -t uhhlt/dwts_backend_celery_archive:"${docker_tag}" .
+    docker build -f Dockerfile_celery_simsearch_worker -t uhhlt/dwts_backend_celery_simsearch:"${docker_tag}" .
+
+    if [ $push == true ]; then
+      docker login && docker push uhhlt/dwts_backend_api:"${docker_tag}"
+      docker login && docker push uhhlt/dwts_backend_celery_text:"${docker_tag}"
+      docker login && docker push uhhlt/dwts_backend_celery_image:"${docker_tag}"
+      docker login && docker push uhhlt/dwts_backend_celery_archive:"${docker_tag}"
       docker login && docker push uhhlt/dwts_backend_celery_simsearch:"${docker_tag}"
     fi
   fi
