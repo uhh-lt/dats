a332e9f49822dc0bd13330a8d543be15d7f20661
floschne <florian.schneider-1@uni-hamburg.de>
2022-10-28 16:38:22 +0000

    using solo pool for celery archive worker if no concurrency was chosen

--- backend/src/celery_archive_worker_entrypoint.sh
+++ backend/src/celery_archive_worker_entrypoint.sh
@@ -5,4 +5,4 @@ set -e
 LOG_LEVEL=${LOG_LEVEL:-debug}
 CELERY_ARCHIVE_WORKER_CONCURRENCY=${CELERY_ARCHIVE_WORKER_CONCURRENCY:-1}
 
-celery -A app.docprepro.archive.preprocess worker -Q archiveQ,celery -l "$LOG_LEVEL" -c "$CELERY_ARCHIVE_WORKER_CONCURRENCY" # TODO Flo: Env vars for parameters
+poetry run celery -A app.docprepro.archive.preprocess worker -Q archiveQ,celery -l "$LOG_LEVEL" -c "$CELERY_ARCHIVE_WORKER_CONCURRENCY"
