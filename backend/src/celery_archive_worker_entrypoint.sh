a610f57a757293be70b2b734f04bbe4593ca0a07
floschne <florian.schneider-1@uni-hamburg.de>
2022-11-01 12:39:39 +0000

    added missing if clause to run celery archive with solo pool if set

--- backend/src/celery_archive_worker_entrypoint.sh
+++ backend/src/celery_archive_worker_entrypoint.sh
@@ -5,4 +5,8 @@ set -e
 LOG_LEVEL=${LOG_LEVEL:-debug}
 CELERY_ARCHIVE_WORKER_CONCURRENCY=${CELERY_ARCHIVE_WORKER_CONCURRENCY:-1}
 
-poetry run celery -A app.docprepro.archive.preprocess worker -Q archiveQ,celery -l "$LOG_LEVEL" -c "$CELERY_ARCHIVE_WORKER_CONCURRENCY"
+if [ "$CELERY_IMAGE_WORKER_CONCURRENCY" -le 1 ]; then
+  celery -A app.docprepro.archive.preprocess worker -Q archiveQ,celery -l "$LOG_LEVEL" -P solo
+else
+  celery -A app.docprepro.archive.preprocess worker -Q archiveQ,celery -l "$LOG_LEVEL" -c "$CELERY_ARCHIVE_WORKER_CONCURRENCY"
+fi
