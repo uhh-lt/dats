43409cb1be9ce0b8275fdf18c025a0773a024cf0
floschne <florian.schneider-1@uni-hamburg.de>
2022-12-01 13:21:40 +0000

    added missing frontend code for SDocHTML and SDocSents

---
+++ frontend/src/api/openapi/models/SourceDocumentHTML.ts
@@ -0,0 +1,14 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type SourceDocumentHTML = {
+  /**
+   * ID of the SourceDocument the content belongs to.
+   */
+  source_document_id: number;
+  /**
+   * The (html) content of the SourceDocument.
+   */
+  html: string;
+};
---
+++ frontend/src/api/openapi/models/SourceDocumentSentences.ts
@@ -0,0 +1,18 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type SourceDocumentSentences = {
+  /**
+   * ID of the SourceDocument the Sentences belong to.
+   */
+  source_document_id: number;
+  /**
+   * The Sentences of the SourceDocument the Sentences belong to.
+   */
+  sentences: Array<string>;
+  /**
+   * The list of character offsets of the Sentences
+   */
+  sentence_character_offsets?: Array<Array<any>>;
+};
