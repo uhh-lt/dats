8f3adf528fc453972772279a910e319a5a704067
floschne <florian.schneider-1@uni-hamburg.de>
2022-12-01 16:22:43 +0000

    added quick fix for detect language

--- backend/src/app/docprepro/text/detect_language.py
+++ backend/src/app/docprepro/text/detect_language.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from langdetect import detect_langs
+from loguru import logger
 from tqdm import tqdm
 
 from app.core.data.dto.source_document import SDocStatus
@@ -13,9 +14,13 @@ def detect_language_(pptds: List[PreProTextDoc]) -> List[PreProTextDoc]:
         return pptds
 
     for pptd in tqdm(pptds, desc="Detecting language... "):
-        pptd.metadata["language"] = detect_langs(pptd.text)[0].lang  # TODO Flo: what to do with mixed lang docs?
+        try:
+            pptd.metadata["language"] = detect_langs(pptd.text)[0].lang  # TODO Flo: what to do with mixed lang docs?
+        except Exception as e:
+            logger.warning(f"Cannot detect language of SDoc {pptd.sdoc_id}")
+            pptd.metadata["language"] = "en"
 
-        # Flo: update sdoc status
+        # update sdoc status
         update_sdoc_status(sdoc_id=pptd.sdoc_id, sdoc_status=SDocStatus.detect_language)
 
     return pptds
