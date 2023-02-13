8da1421cfdb9e768a0c492cb833902094234549e
0wessarg <christoph_wessarges@hotmail.com>
2023-02-13 15:07:55 +0000

    current progress (unfinished)

--- frontend/src/views/search/DocumentViewer/ImageViewer.tsx
+++ frontend/src/views/search/DocumentViewer/ImageViewer.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useMemo, useRef } from "react";
 import * as d3 from "d3";
 import { Box } from "@mui/material";
 import ImageContextMenu, { ImageContextMenuHandle } from "../../../components/ContextMenu/ImageContextMenu";
+import MetadataHooks from "../../../api/MetadataHooks";
 
 interface ImageViewerProps {
   sdoc: SourceDocumentRead;
@@ -12,11 +13,25 @@ interface ImageViewerProps {
   height: number;
 }
 
+const getImageDimensions = (url: string): Promise<{ width: number; height: number }> => {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.onload = () =>
+      resolve({
+        width: img.width,
+        height: img.height,
+      });
+    img.onerror = (error) => reject(error);
+    img.src = url;
+  });
+};
+
 function ImageViewer({ sdoc, adoc, showEntities, height }: ImageViewerProps) {
   const svgRef = useRef<SVGSVGElement>(null);
   const gRef = useRef<SVGGElement>(null);
   const bboxRef = useRef<SVGGElement>(null);
   const textRef = useRef<SVGGElement>(null);
+  const imgRef = useRef<SVGImageElement>(null);
   const imageContextMenuRef = useRef<ImageContextMenuHandle>(null);
 
   // global server state (react-query)
@@ -48,62 +63,73 @@ function ImageViewer({ sdoc, adoc, showEntities, height }: ImageViewerProps) {
   }, [zoom]);
 
   useEffect(() => {
-    // select nodes & links
-    const rect = d3.select(bboxRef.current).selectAll<SVGRectElement, BBoxAnnotationReadResolvedCode>("rect");
-    const text = d3.select(textRef.current).selectAll<SVGTextElement, BBoxAnnotationReadResolvedCode>("text");
-    const data = showEntities ? annotations.data || [] : [];
+    const createViewport = async () => {
+      // select nodes & links
+      const img = d3.select("image");
+      const rect = d3.select(bboxRef.current).selectAll<SVGRectElement, BBoxAnnotationReadResolvedCode>("rect");
+      const text = d3.select(textRef.current).selectAll<SVGTextElement, BBoxAnnotationReadResolvedCode>("text");
+      const data = showEntities ? annotations.data || [] : [];
+      const { width: imgWidth, height: imgHeight } = await getImageDimensions(sdoc.content);
+      const scaledRatio = 500 / imgHeight;
 
-    // add & remove nodes
-    rect
-      .data(data, (datum) => datum.id)
-      .join(
-        (enter) =>
-          enter
-            .append("rect")
-            .attr("x", (d) => d.x_min)
-            .attr("y", (d) => d.y_min)
-            .attr("width", (d) => d.x_max - d.x_min)
-            .attr("height", (d) => d.y_max - d.y_min)
-            .attr("fill", "transparent")
-            .attr("stroke", (d) => d.code.color)
-            .attr("stroke-width", 3),
+      if (svgRef.current) {
+        const portWidth: number = svgRef.current.width.baseVal.value;
+        img.attr("x", portWidth / 2 - (imgWidth * scaledRatio) / 2);
+      }
 
-        (update) => update.attr("x", (d) => d.x_min),
-        (exit) => exit.remove()
-      );
+      // add & remove nodes
+      rect
+        .data(data, (datum) => datum.id)
+        .join(
+          (enter) =>
+            enter
+              .append("rect")
+              .attr("x", (d) => d.x_min * scaledRatio)
+              .attr("y", (d) => d.y_min * scaledRatio)
+              .attr("width", (d) => (d.x_max - d.x_min) * scaledRatio)
+              .attr("height", (d) => (d.y_max - d.y_min) * scaledRatio)
+              .attr("fill", "transparent")
+              .attr("stroke", (d) => d.code.color)
+              .attr("stroke-width", 3),
 
-    // add click listener
-    rect.on("click", (event, d) => {
-      console.log(d);
-    });
+          (update) => update.attr("x", (d) => d.x_min),
+          (exit) => exit.remove()
+        );
 
-    // add & remove text
-    text
-      .data(data, (datum) => datum.id)
-      .join(
-        (enter) =>
-          enter
-            .append("text")
-            .attr("x", (d) => d.x_min + 3)
-            .attr("y", (d) => d.y_max - 3)
-            .attr("fill", "white")
-            .attr("stroke", "black")
-            .attr("stroke-width", 0.75)
-            .attr("font-size", `${Math.max(21, height / 17)}px`)
+      // add click listener
+      rect.on("click", (event, d) => {
+        console.log(d);
+      });
 
-            .text((d) => d.code.name),
+      // add & remove text
+      text
+        .data(data, (datum) => datum.id)
+        .join(
+          (enter) =>
+            enter
+              .append("text")
+              .attr("x", (d) => d.x_min * scaledRatio + 3)
+              .attr("y", (d) => d.y_max * scaledRatio - 3)
+              .attr("fill", "white")
+              .attr("stroke", "black")
+              .attr("stroke-width", 0.75)
+              .attr("font-size", `${Math.max(21, height / 17)}px`)
 
-        (update) => update.attr("x", (d) => d.x_min),
-        (exit) => exit.remove()
-      );
-  }, [height, annotations.data, showEntities]);
+              .text((d) => d.code.name),
+
+          (update) => update.attr("x", (d) => d.x_min),
+          (exit) => exit.remove()
+        );
+    };
+    createViewport().catch(console.error);
+  }, [height, annotations.data, showEntities, sdoc.content]);
 
   return (
     <Box onContextMenu={handleContextMenu}>
       {annotations.isError && <span>{annotations.error.message}</span>}
-      <svg ref={svgRef} width="100%" height={`${height}px`} style={{ cursor: "move" }}>
+      <svg ref={svgRef} width="100%" height={"500px"} style={{ cursor: "move" }}>
         <g ref={gRef}>
-          <image href={sdoc.content} />
+          <image href={sdoc.content} ref={imgRef} height={500} />
           <g ref={bboxRef}></g>
           <g ref={textRef}></g>
         </g>
