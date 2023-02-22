1c1d1a5e296aac43c0ad396070d4cebd9d256f98
floschne <florian.schneider-1@uni-hamburg.de>
2023-02-22 15:21:59 +0000

    finishing video and audio prepro docs after import

--- backend/src/app/docprepro/audio/__init__.py
+++ backend/src/app/docprepro/audio/__init__.py
@@ -6,6 +6,7 @@ from celery import Signature
 
 from app.docprepro.audio.models.preproaudiodoc import PreProAudioDoc
 
+from app.docprepro.text import finish_preprocessing
 # Flo: Task names (as they could be imported)
 import_audio_document = "app.docprepro.audio.preprocess.import_audio_document"
 
@@ -14,12 +15,15 @@ def audio_document_preprocessing_apply_async(doc_file_path: Path, project_id: in
     audio_document_preprocessing = (
             Signature(import_audio_document, kwargs={"doc_file_path": doc_file_path,
                                                      "project_id": project_id,
-                                                     "mime_type": mime_type}) 
-            )
+                                                     "mime_type": mime_type}) |
+            Signature(finish_preprocessing)
+    )
+
     return audio_document_preprocessing.apply_async()
 
 
 def audio_document_preprocessing_without_import_apply_async(ppads: List[PreProAudioDoc]) -> Any:
+    raise NotImplementedError
     audio_document_preprocessing = (
     )
     return audio_document_preprocessing.apply_async()
