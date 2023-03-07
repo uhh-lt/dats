2678b23f0989a8eca1abb70db96493ce9f50d634
floschne <florian.schneider-1@uni-hamburg.de>
2023-03-07 16:47:13 +0000

    prugin temp files when shuttingdown

--- backend/src/main.py
+++ backend/src/main.py
@@ -116,6 +116,7 @@ async def startup_event():
 @app.on_event("shutdown")
 async def shutdown_event():
     logger.info("Stopping D-WISE Tool Suite FastAPI!")
+    RepoService().purge_temporary_files()
 
 
 # include the endpoint routers
