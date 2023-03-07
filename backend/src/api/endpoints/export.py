f318786f1483dd8b22c9226cfcedd3d8631d44ce
floschne <florian.schneider-1@uni-hamburg.de>
2023-03-07 12:19:10 +0000

    added export endpoints for codes, tags, logbooks

--- backend/src/api/endpoints/export.py
+++ backend/src/api/endpoints/export.py
@@ -65,6 +65,7 @@ async def export_memo_by_id(
     # TODO Flo: only if the user has access?
     return es.export_memo(db=db, memo_id=memo_id, export_format=export_format)
 
+
 @router.get(
     "/project/{proj_id}/users",
     tags=tags,
@@ -82,6 +83,58 @@ async def export_all_user_data_from_project(
     return es.export_all_user_data_from_proj(
         db=db, proj_id=proj_id, export_format=export_format
     )
+
+
+@router.get(
+    "/project/{proj_id}/logbook/{user_id}",
+    tags=tags,
+    response_model=str,
+    summary="Returns a URL to download the logbook of the user in the project",
+    description="Returns a URL to download the logbook of the user in the project",
+)
+async def export_user_logbook_from_project(
+    *,
+    db: Session = Depends(get_db_session),
+    proj_id: int,
+    user_id: int,
+) -> str:
+    # TODO Flo: only if the user has access?
+    return es.export_logbook_memo(db=db, proj_id=proj_id, user_id=user_id)
+
+
+@router.get(
+    "/project/{proj_id}/codes",
+    tags=tags,
+    response_model=str,
+    summary="Returns a URL to download all Codes from all users in the project",
+    description="Returns a URL to download all Codes from all users in the project",
+)
+async def export_all_codes_from_project(
+    *,
+    db: Session = Depends(get_db_session),
+    proj_id: int,
+    export_format: ExportFormat = ExportFormat.CSV,
+) -> str:
+    # TODO Flo: only if the user has access?
+    return es.export_project_codes(db=db, proj_id=proj_id, export_format=export_format)
+
+
+@router.get(
+    "/project/{proj_id}/tags",
+    tags=tags,
+    response_model=str,
+    summary="Returns a URL to download all DocumentTags in the project",
+    description="Returns a URL to download all DocumentTags in the project",
+)
+async def export_all_tags_from_project(
+    *,
+    db: Session = Depends(get_db_session),
+    proj_id: int,
+    export_format: ExportFormat = ExportFormat.CSV,
+) -> str:
+    # TODO Flo: only if the user has access?
+    return es.export_project_tags(db=db, proj_id=proj_id, export_format=export_format)
+
 
 @router.get(
     "/project/{proj_id}/user/{user_id}",
