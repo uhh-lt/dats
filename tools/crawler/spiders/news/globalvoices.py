e87968d5f9fcfaaff44ae6aab6cfe25ec8d0efa9
Tim Fischer <tim.fischer@uni-hamburg.de>
2023-03-14 10:00:36 +0000

    added new crawler for globalvoices.org

---
+++ tools/crawler/spiders/news/globalvoices.py
@@ -0,0 +1,35 @@
+import scrapy
+
+from crawler.spiders.spider_base import SpiderBase
+
+
+class GlobalVoicesSpider(SpiderBase):
+    name = "globalvoices"
+    start_urls = [
+        f"https://globalvoices.org/-/world/western-europe,eastern-central-europe/page/{i}/" for i in range(50)
+    ]
+
+    start_urls += [
+        f"https://de.globalvoices.org/category/world/western-europe,eastern-central-europe/page/{i}/" for i in range(50)
+    ]
+
+    # provide arguments using the -a option
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def parse(self, response, **kwargs):
+
+        # if on the overview page
+        if response.url in self.start_urls:
+            # get a list of all links
+            urls = response.css(".post-title a::attr(href)").getall()
+
+            # visit every discussion
+            for url in urls:
+                yield scrapy.Request(response.urljoin(url), callback=self.parse)
+
+        # if in a news article
+        else:
+            # apply pipeline
+            item = self.init_incel_item(response=response)
+            yield item
