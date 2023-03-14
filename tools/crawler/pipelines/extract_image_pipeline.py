6ffff0bd26dc3e0393200dbcfd2221d9673ca637
Tim Fischer <tim.fischer@uni-hamburg.de>
2023-03-14 11:22:29 +0000

    added support for relative urls in image pipeline

--- tools/crawler/pipelines/extract_image_pipeline.py
+++ tools/crawler/pipelines/extract_image_pipeline.py
@@ -1,5 +1,6 @@
 from scrapy import Selector
 from crawler.items import IncelItem
+from urllib.parse import urlparse
 
 
 class ExtractImagePipeline:
@@ -10,12 +11,17 @@ class ExtractImagePipeline:
         image_urls = Selector(text=html).css("img::attr(src)").getall()
         image_urls = image_urls if image_urls is not None else []
 
-        # only download valid images
-        image_urls = [
-            image_url
-            for image_url in image_urls
-            if image_url.startswith("https:") or image_url.startswith("http:")
-        ]
+        # only download valid images (https:// or http:// or relative with / )
+        image_urls = []
+        for image_url in image_urls:
+            parsed_image_url = urlparse(image_url)
+            # normal url, everything is fine
+            if parsed_image_url.scheme == "http" or parsed_image_url.scheme == "https":
+                image_urls.append(image_url)
+            # relative url, prepend base url
+            elif parsed_image_url.scheme == "":
+                parsed_base_url = urlparse(item["url"])
+                image_urls.append(f"{parsed_base_url.scheme}://{parsed_base_url.netloc}{parsed_image_url.path}")
 
         item["image_urls"] = image_urls
         return item
