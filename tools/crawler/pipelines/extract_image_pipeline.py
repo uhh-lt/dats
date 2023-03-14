802a5cbb42356886ea09b4fe05d7ea90b1b4cbdd
Tim Fischer <tim.fischer@uni-hamburg.de>
2023-03-14 11:59:55 +0000

    updated image pipeline to work with // urls

--- tools/crawler/pipelines/extract_image_pipeline.py
+++ tools/crawler/pipelines/extract_image_pipeline.py
@@ -18,10 +18,14 @@ class ExtractImagePipeline:
             # normal url, everything is fine
             if parsed_image_url.scheme == "http" or parsed_image_url.scheme == "https":
                 image_urls.append(image_url)
-            # relative url, prepend base url
-            elif parsed_image_url.scheme == "":
+            # relative url starting with / -> prepend base url
+            elif parsed_image_url.scheme == "" and parsed_image_url.netloc == "":
                 parsed_base_url = urlparse(item["url"])
                 image_urls.append(f"{parsed_base_url.scheme}://{parsed_base_url.netloc}{parsed_image_url.path}")
+            # relative url starting with // -> prepend base scheme
+            elif parsed_image_url.scheme == "":
+                parsed_base_url = urlparse(item["url"])
+                image_urls.append(f"{parsed_base_url.scheme}://{parsed_image_url.netloc}{parsed_image_url.path}")
 
         item["image_urls"] = image_urls
         return item
