72850c4599a3107029dc58567f60e20c9d002cd0
Alienmaster <robert.geislinger@uni-hamburg.de>
2023-03-24 12:47:19 +0000

    Audio: Wordlevel Datastructure

---
+++ backend/src/app/docprepro/video/models/wordleveltranscription.py
@@ -0,0 +1,7 @@
+from pydantic import BaseModel, Field
+
+class WordLevelTranscription(BaseModel):
+    sdoc_id: int # TODO: Nötig?
+    text: str #TODO: Text/Word/String?
+    start_ms: int
+    end_ms: int
