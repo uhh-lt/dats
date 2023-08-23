5d0c1233d61c4973ce899a79558f78f5895931d8
floschne <florian.schneider-1@uni-hamburg.de>
2023-08-23 15:53:51 +0000

    fixed extract content for html docs

--- backend/src/app/preprocessing/pipeline/steps/text/extract_content_in_html_from_raw_text_docs.py
+++ backend/src/app/preprocessing/pipeline/steps/text/extract_content_in_html_from_raw_text_docs.py
@@ -7,11 +7,18 @@ repo = RepoService()
 
 def extract_content_in_html_from_raw_text_docs(cargo: PipelineCargo) -> PipelineCargo:
     pptd: PreProTextDoc = cargo.data["pptd"]
-    if pptd.filepath.suffix != ".txt":
+
+    if cargo.ppj_payload.mime_type not in ["text/html", "text/plain"]:
         return cargo
 
     content = pptd.filepath.read_text(encoding="utf-8")
-    content_as_html = f"<html><body><p>{content}</p></body></html>"
+    if cargo.ppj_payload.mime_type == "text/plain":
+        content_as_html = f"<html><body><p>{content}</p></body></html>"
+    elif cargo.ppj_payload.mime_type == "text/html":
+        content_as_html = content
+    else:
+        # this should never happen
+        raise ValueError(f"Unknown mime_type: {cargo.ppj_payload.mime_type}")
 
     pptd.html = content_as_html
 
