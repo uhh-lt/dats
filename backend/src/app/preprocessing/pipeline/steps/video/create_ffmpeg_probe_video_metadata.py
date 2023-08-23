7c000cd5fbe6c984d26782dda79de76069e52061
floschne <florian.schneider-1@uni-hamburg.de>
2023-08-23 08:34:16 +0000

    improved and added steps to video pipeline

--- backend/src/app/preprocessing/pipeline/steps/video/create_ffmpeg_probe_video_metadata.py
+++ backend/src/app/preprocessing/pipeline/steps/video/create_ffmpeg_probe_video_metadata.py
@@ -11,9 +11,9 @@ def create_ffmpeg_probe_video_metadata(cargo: PipelineCargo) -> PipelineCargo:
     for k, v in ffmpeg_probe["format"].items():
         ppvd.metadata[k] = v
 
-    vi = 0 if ffmpeg_probe["streams"][0]["codec_type"] == "video" else 1
-    ppvd.metadata["frame_width"] = ffmpeg_probe["streams"][vi]["width"]
-    ppvd.metadata["frame_height"] = ffmpeg_probe["streams"][vi]["height"]
-    ppvd.metadata["aspect_ratio"] = ffmpeg_probe["streams"][vi]["display_aspect_ratio"]
+    vidx = 0 if ffmpeg_probe["streams"][0]["codec_type"] == "video" else 1
+    for md in ["width", "height", "display_aspect_ratio"]:
+        if md in ffmpeg_probe["streams"][vidx]:
+            ppvd.metadata[md] = ffmpeg_probe["streams"][vidx][md]
 
     return cargo
