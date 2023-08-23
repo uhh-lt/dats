3e8991f676d8b777d1ef2dc4c91306c01bf72c2f
floschne <florian.schneider-1@uni-hamburg.de>
2023-08-23 09:00:00 +0000

    removed old docprepro code

--- backend/src/app/docprepro/audio/models/autotimespan.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-
-class AutoTimespan(BaseModel):
-    transcription: str
-    code: str
-    begin: int
-    end: int
--- backend/src/app/docprepro/image/models/autobbox.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-
-class AutoBBox(BaseModel):
-    code: str
-    x_min: int
-    y_min: int
-    x_max: int
-    y_max: int
