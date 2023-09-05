5b55a929e5e62434d05595ac204f8f98d9f6ccc4
floschne <florian.schneider-1@uni-hamburg.de>
2023-09-05 16:30:28 +0000

    added support for celery debugging

--- backend/src/celery_background_jobs_worker_entrypoint.sh
+++ backend/src/celery_background_jobs_worker_entrypoint.sh
@@ -60,7 +60,10 @@ export MKL_NUM_THREADS=1
 LOG_LEVEL=${LOG_LEVEL:-debug}
 CELERY_BACKGROUND_JOBS_WORKER_CONCURRENCY=${CELERY_BACKGROUND_JOBS_WORKER_CONCURRENCY:-1}
 
-if [ "$CELERY_BACKGROUND_JOBS_WORKER_CONCURRENCY" -le 1 ]; then
+if [ "$CELERY_DEBUG_MODE" -eq 1 ]; then
+  echo "Running celery in debug mode!"
+  pip install debugpy -t /tmp && python /tmp/debugpy --listen 0.0.0.0:6900 -m celery -A app.celery.background_jobs.tasks worker -Q bgJobsQ,celery -l "$LOG_LEVEL" -P solo
+elif [ "$CELERY_BACKGROUND_JOBS_WORKER_CONCURRENCY" -le 1 ]; then
   echo "NEVER USE THIS WORKER FOR GPU JOBS!"
   celery -A app.celery.background_jobs.tasks worker -Q bgJobsQ,celery -l "$LOG_LEVEL" -P solo
 else
