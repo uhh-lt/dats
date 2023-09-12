4e9c64d5def695926cca2b62f1d2af8ad58afa96
Tim Fischer <tim.fischer@uni-hamburg.de>
2023-09-12 07:56:17 +0000

    new redux hook: useAppStore

--- frontend/src/plugins/ReduxHooks.ts
+++ frontend/src/plugins/ReduxHooks.ts
@@ -1,6 +1,7 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import type { RootState, AppDispatch } from "../store/store";
+import { TypedUseSelectorHook, useDispatch, useSelector, useStore } from "react-redux";
+import type { AppDispatch, RootState } from "../store/store";
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppStore = () => useStore<RootState>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
