a7e66a8ebc134a2e2f279ca9da8f445864b14a4f
Tim Fischer <tim.fischer@uni-hamburg.de>
2023-09-12 11:14:21 +0000

    added crawler for earth negotiations bulletin

--- tools/crawler/items.py
+++ tools/crawler/items.py
@@ -7,11 +7,11 @@ import scrapy
 
 
 class GenericWebsiteItem(scrapy.Item):
+    url = scrapy.Field()
     raw_html = scrapy.Field()
     extracted_html = scrapy.Field()
 
     # write pipeline
-    url = scrapy.Field()
     file_name = scrapy.Field()
     output_dir = scrapy.Field()
 
@@ -49,5 +49,28 @@ class GenericWebsiteItem(scrapy.Item):
                 "visited_date": self["visited_date"]
                 if "visited_date" in self.keys()
                 else "",
+            }
+        )
+
+
+class ENBItem(scrapy.Item):
+    url = scrapy.Field()
+    title = scrapy.Field()
+    subtitle = scrapy.Field()
+    html = scrapy.Field()
+    participants = scrapy.Field()
+    tags = scrapy.Field()
+    text = scrapy.Field()
+
+    # write pipeline
+    file_name = scrapy.Field()
+    output_dir = scrapy.Field()
+
+    def __repr__(self):
+        """only print out interesting data after exiting the Pipeline"""
+        return repr(
+            {
+                "url": self["url"] if "url" in self.keys() else "",
+                "title": self["title"] if "title" in self.keys() else "",
             }
         )
---
+++ tools/crawler/spiders/news/enb.py
@@ -0,0 +1,66 @@
+import scrapy
+from crawler.spiders.spider_base import SpiderBase
+from crawler.items import ENBItem
+
+
+class ENBSpider(scrapy.Spider):
+    name = "enb"
+    start_urls = [
+        f"https://enb.iisd.org/negotiations/un-framework-convention-climate-change-unfccc?page={i}" for i in range(9)
+    ]
+
+    # provide arguments using the -a option
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def parse(self, response, **kwargs):
+        # get a list of all links
+        urls = [
+            response.urljoin(x)
+            for x in response.css(".c-list-item__heading-link::attr(href)").getall()
+        ]
+        urls = list(set(urls))
+
+        # visit every conference
+        for url in urls:
+            yield scrapy.Request(response.urljoin(url), callback=self.parse_conference)
+
+    def parse_conference(self, response, **kwargs):
+        # visit the summary report
+        summary_report_url = response.css(".c-final-report__title-link::attr(href)").get()
+        yield scrapy.Request(response.urljoin(summary_report_url), callback=self.parse_report)
+
+    def parse_report(self, response, **kwargs):
+        # parse
+        title_html = response.css(".c-node__title").get()
+        title = response.css(".c-node__title::text").get().strip()
+
+        subtitle_html = response.css(".c-node__subtitle").get()
+        subtitle = response.css(".c-node__subtitle::text").get().strip()
+
+        body_html = response.css(".c-node__body").get()
+        body = "".join(response.css(".c-node__body *::text").getall())
+
+        tags_selectors = response.css(".c-tags__items")
+        tags = [[x.strip() for x in tag_selector.css(".c-tags__item ::text").getall() if len(x.strip()) > 0] for tag_selector in tags_selectors] 
+
+        # construct result
+        item = ENBItem()
+        item["file_name"] = title
+        item["output_dir"] = f"/home/tfischer/Development/dwts/data/enb/{subtitle.replace(' ', '_')}"
+
+        item["url"] = response.url
+        item["title"] = title
+        item["subtitle"] = subtitle
+        item["html"] = "<html>" + title_html + subtitle_html + body_html + "</html>"
+        item["text"] = title + "\n\n" + subtitle + "\n\n" + body
+        item["participants"] = tags[0] if len(tags) > 0 else []
+        item["tags"] = tags[1] if len(tags) > 1 else []
+
+        # apply pipeline (we only want to write! deactivate all other pipelines)
+        yield item
+
+        # visit next report
+        next_report_url = response.css(".c-node__button-container > a:last-child::attr(href)").get()
+        if next_report_url is not None:
+            yield scrapy.Request(response.urljoin(next_report_url), callback=self.parse_report)
