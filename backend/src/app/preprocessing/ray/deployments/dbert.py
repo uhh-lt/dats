4eb2aa82924a04e83c6dcf3034edf5614e9414d4
floschne <florian.schneider-1@uni-hamburg.de>
2023-09-12 20:56:32 +0000

    restructured and refactored ray code

--- backend/src/app/preprocessing/ray/deployments/dbert.py
+++ backend/src/app/preprocessing/ray/deployments/dbert.py
@@ -1,7 +1,7 @@
 import torch
 from fastapi import APIRouter
 from ray import serve
-from transformers import pipeline
+from transformers.pipelines import pipeline
 
 router = APIRouter()
 
@@ -21,9 +21,12 @@ class APIIngress:
 
 @serve.deployment(
     ray_actor_options={"num_gpus": 1},
-    autoscaling_config={"min_replicas": 0, "max_replicas": 2},
+    autoscaling_config={
+        "min_replicas": 0,
+        "max_replicas": 2,
+    },
 )
-class DistilBertModel:
+class DistilBertDeployment:
     def __init__(self):
         self.classifier = pipeline(
             "sentiment-analysis",
@@ -35,8 +38,3 @@ class DistilBertModel:
 
     def classify(self, sentence: str):
         return self.classifier(sentence)
-
-
-if __name__ == "__main__":
-    dbert = DistilBertModel.bind()
-    APIIngress.bind(dbert)
--- backend/src/ray/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd /dwts_code/src/ray/
-
-serve run jobs:deploy -h 0.0.0.0 
-# ray start --head
-# serve run config.yaml
-# serve build jobs:whisper_deploy -o config.yaml
-# sleep infinity
-
