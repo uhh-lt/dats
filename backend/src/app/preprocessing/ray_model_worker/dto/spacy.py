160c8af62fa82a83e9b18a69fe7f3a342851e234
floschne <florian.schneider-1@uni-hamburg.de>
2023-09-13 16:06:56 +0000

    fixed ray spacy and added ray specific config

--- backend/src/app/preprocessing/ray_model_worker/dto/spacy.py
+++ backend/src/app/preprocessing/ray_model_worker/dto/spacy.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel, Field
 
 class SpacyInput(BaseModel):
     text: str = Field(example="I love Hamburg!")
+    language: str = Field(example="en")
 
 
 class SpacyOutput(BaseModel):
