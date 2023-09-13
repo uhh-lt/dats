a3ab863829fa8957bceb082fa5e79e8416ca16de
floschne <florian.schneider-1@uni-hamburg.de>
2023-09-13 23:16:42 +0000

    fixed spacy model output in ray

--- backend/src/app/preprocessing/ray_model_worker/apps/spacy.py
+++ backend/src/app/preprocessing/ray_model_worker/apps/spacy.py
@@ -1,6 +1,6 @@
 import logging
 
-from dto.spacy import SpacyInput, SpacyOutput
+from dto.spacy import SpacyInput, SpacyPipelineOutput
 from fastapi import FastAPI
 from models.spacy import SpacyModel
 from ray import serve
@@ -17,9 +17,9 @@ class SpacyApi:
     def __init__(self, spacy_model_handle: RayServeHandle) -> None:
         self.spacy = spacy_model_handle
 
-    @api.post("/ner", response_model=SpacyOutput)
-    async def ner(self, input: SpacyInput) -> SpacyOutput:
-        predict_ref = await self.spacy.ner.remote(input)
+    @api.post("/pipeline", response_model=SpacyPipelineOutput)
+    async def pipeline(self, input: SpacyInput) -> SpacyPipelineOutput:
+        predict_ref = await self.spacy.pipeline.remote(input)
         predict_result = await predict_ref
         return predict_result
 
