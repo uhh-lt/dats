718674db24f43d21c88c034e9501c3218a1811ca
floschne <florian.schneider-1@uni-hamburg.de>
2023-09-13 23:16:42 +0000

    fixed spacy model output in ray

--- backend/src/app/preprocessing/ray_model_worker/dto/spacy.py
+++ backend/src/app/preprocessing/ray_model_worker/dto/spacy.py
@@ -1,6 +1,46 @@
-from typing import List, Tuple
+from typing import List, Optional
 
 from pydantic import BaseModel, Field
+
+
+class SpacySpan(BaseModel):
+    text: str = Field(description="The surface form of the span", example="Hamburg")
+    start_char: int = Field(
+        description="The start character index of the span in the input text", example=7
+    )
+    end_char: int = Field(
+        description="The end character index of the span in the input text", example=14
+    )
+    start_token: int = Field(
+        description="The start token index of the span in the input text", example=1
+    )
+    end_token: int = Field(
+        description="The end token index of the span in the input text", example=2
+    )
+    label: Optional[str] = Field(
+        description="The optional label of the span", example="GPE", default=None
+    )
+
+
+class SpacyToken(BaseModel):
+    text: str = Field(description="The surface form of the token", example="Hamburg")
+    start_char: int = Field(
+        description="The start character index of the token in the input text",
+        example=7,
+    )
+    end_char: int = Field(
+        description="The end character index of the token in the input text", example=14
+    )
+    pos: str = Field(description="The part-of-speech tag of the token", example="PROPN")
+    lemma: str = Field(description="The lemma of the token", example="Hamburg")
+    stopword: bool = Field(description="Whether the token is a stopword", example=False)
+    punctuation: bool = Field(
+        description="Whether the token is a punctuation", example=False
+    )
+    alpha: bool = Field(
+        description="Whether the token is an alphabetic character", example=True
+    )
+    digit: bool = Field(description="Whether the token is a digit", example=False)
 
 
 class SpacyInput(BaseModel):
@@ -8,6 +48,80 @@ class SpacyInput(BaseModel):
     language: str = Field(example="en")
 
 
-class SpacyOutput(BaseModel):
-    text: str = Field(example="I love Hamburg!")
-    nes: List[Tuple[str, str]] = Field(example=[("Hamburg", "GPE")])
+class SpacyPipelineOutput(BaseModel):
+    tokens: Optional[List[SpacyToken]] = Field(
+        example=[
+            SpacyToken(
+                text="I",
+                start_char=0,
+                end_char=1,
+                pos="PRON",
+                lemma="I",
+                stopword=True,
+                punctuation=False,
+                alpha=True,
+                digit=False,
+            ),
+            SpacyToken(
+                text="love",
+                start_char=2,
+                end_char=6,
+                pos="VERB",
+                lemma="love",
+                stopword=False,
+                punctuation=False,
+                alpha=True,
+                digit=False,
+            ),
+            SpacyToken(
+                text="Hamburg",
+                start_char=7,
+                end_char=14,
+                pos="PROPN",
+                lemma="Hamburg",
+                stopword=False,
+                punctuation=False,
+                alpha=True,
+                digit=False,
+            ),
+            SpacyToken(
+                text="!",
+                start_char=14,
+                end_char=15,
+                pos="PUNCT",
+                lemma="!",
+                stopword=False,
+                punctuation=True,
+                alpha=False,
+                digit=False,
+            ),
+        ],
+        default_factory=list,
+    )
+
+    ents: Optional[List[SpacySpan]] = Field(
+        example=[
+            SpacySpan(
+                label="GPE",
+                text="Hamburg",
+                start_char=7,
+                end_char=14,
+                start_token=1,
+                end_token=2,
+            )
+        ],
+        default_factory=list,
+    )
+
+    sents: Optional[List[SpacySpan]] = Field(
+        example=[
+            SpacySpan(
+                text="I love Hamburg!",
+                start_char=0,
+                end_char=15,
+                start_token=0,
+                end_token=3,
+            )
+        ],
+        default_factory=list,
+    )
--- backend/src/app/preprocessing/ray_model_worker/models/spacy.py
+++ backend/src/app/preprocessing/ray_model_worker/models/spacy.py
@@ -1,9 +1,9 @@
 import logging
-from typing import Dict
+from typing import Dict, List
 
 import spacy
 from config import conf
-from dto.spacy import SpacyInput, SpacyOutput
+from dto.spacy import SpacyInput, SpacyPipelineOutput, SpacySpan, SpacyToken
 from ray import serve
 from spacy import Language
 
@@ -53,19 +53,54 @@ class SpacyModel:
 
         self.spacy_models = nlp
 
-    def ner(self, input: SpacyInput) -> SpacyOutput:
-        model = (
-            self.spacy_models[input.language]
-            if input.language in self.spacy_models
+    def _get_language_specific_model(self, language: str) -> Language:
+        return (
+            self.spacy_models[language]
+            if language in self.spacy_models
             else self.spacy_models["default"]
         )
 
-        nes = []
+    def pipeline(self, input: SpacyInput) -> SpacyPipelineOutput:
+        model = self._get_language_specific_model(input.language)
         doc = model(input.text)
-        for ent in doc.ents:
-            nes.append((ent.text, ent.label_))
+        tokens: List[SpacyToken] = [
+            SpacyToken(
+                text=token.text,
+                start_char=token.idx,
+                end_char=token.idx + len(token.text),
+                label=token.ent_type_,
+                pos=token.pos_,
+                lemma=token.lemma_,
+                stopword=token.is_stop,
+                punctuation=token.is_punct,
+                alpha=token.is_alpha,
+                digit=token.is_digit,
+            )
+            for token in doc
+        ]
 
-        return SpacyOutput(
-            text=input.text,
-            nes=nes,
-        )
+        ents: List[SpacySpan] = [
+            SpacySpan(
+                text=ent.text,
+                start_char=ent.start_char,
+                end_char=ent.end_char,
+                start_token=ent.start,
+                end_token=ent.end,
+                label=ent.label_,
+            )
+            for ent in doc.ents
+        ]
+
+        sents: List[SpacySpan] = [
+            SpacySpan(
+                text=sent.text,
+                start_char=sent.start_char,
+                end_char=sent.end_char,
+                start_token=sent.start,
+                end_token=sent.end,
+                label="SENTENCE",
+            )
+            for sent in doc.sents
+        ]
+
+        return SpacyPipelineOutput(tokens=tokens, ents=ents, sents=sents)
