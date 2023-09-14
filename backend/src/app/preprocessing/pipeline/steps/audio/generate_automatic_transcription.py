621c41329331fcb699e678ada0bfc03531567030
floschne <florian.schneider-1@uni-hamburg.de>
2023-09-14 10:20:52 +0000

    replaced whisper in pipeline steps with ray modelservice calls

--- backend/src/app/preprocessing/pipeline/steps/audio/generate_automatic_transcription.py
+++ backend/src/app/preprocessing/pipeline/steps/audio/generate_automatic_transcription.py
@@ -1,67 +1,40 @@
-import os
-from functools import lru_cache
-from typing import Any, Dict
-
-import numpy as np
-import torch
-import whisper_timestamped
 from app.preprocessing.pipeline.model.audio.preproaudiodoc import PreProAudioDoc
 from app.preprocessing.pipeline.model.audio.wordleveltranscription import (
     WordLevelTranscription,
 )
 from app.preprocessing.pipeline.model.pipeline_cargo import PipelineCargo
-from config import conf
+from app.preprocessing.ray_model_service import RayModelService
+from app.preprocessing.ray_model_worker.dto.whisper import (
+    WhisperFilePathInput,
+    WhisperTranscriptionOutput,
+)
 from loguru import logger
-from scipy.io import wavfile
 
-WHISPER_MODEL = conf.preprocessing.audio.whisper.model
-DEVICE = conf.preprocessing.audio.whisper.device
-DOWNLOAD_DIR = os.environ["TRANSFORMERS_CACHE"]
-WHISPER_TRANSCRIBE_OPTIONS = conf.preprocessing.audio.whisper.options
-
-torch.set_num_threads(1)
-
-
-@lru_cache(maxsize=1)
-def load_whisper_model() -> whisper_timestamped.Whisper:
-    logger.debug("Loading whisper Model")
-    whisper_model = whisper_timestamped.load_model(
-        WHISPER_MODEL, DEVICE, download_root=DOWNLOAD_DIR
-    )
-    return whisper_model
+rms = RayModelService()
 
 
 def generate_automatic_transcription(cargo: PipelineCargo) -> PipelineCargo:
     ppad: PreProAudioDoc = cargo.data["ppad"]
-
-    # Load uncompressed audiofile
-    wav_file = str(ppad.uncompressed_audio_filepath)
-    audiodata = wavfile.read(wav_file)[1]
-
-    audionp = np.frombuffer(audiodata, np.int16).flatten().astype(np.float32) / 32768.0
-
-    whisper_model = load_whisper_model()
-
-    logger.debug(
-        (
-            f"Generating automatic transcription of {ppad.filepath}"
-            f" using Whisper '{WHISPER_MODEL}'!"
+    logger.debug(f"Generating automatic transcription for {ppad.filename} ...")
+    if ppad.uncompressed_audio_filepath is None:
+        raise ValueError(
+            f"Uncompressed audio filepath for {ppad.filename} is None. "
+            "Please run the 'convert_to_pcm' step first!"
         )
+
+    # Create Whisper Input
+    whisper_input = WhisperFilePathInput(
+        uncompressed_audio_fp=str(ppad.uncompressed_audio_filepath)
     )
-    transcribe_options: Dict[str, Any] = dict(
-        task="transcribe", **WHISPER_TRANSCRIBE_OPTIONS
-    )
-    result = whisper_timestamped.transcribe(
-        whisper_model, audionp, **transcribe_options
-    )
+    transcription: WhisperTranscriptionOutput = rms.whisper_transcribe(whisper_input)
 
     # Create Wordlevel Transcriptions
-    for segment in result["segments"]:
-        for word in segment["words"]:
+    for segment in transcription.segments:
+        for word in segment.words:
             wlt = WordLevelTranscription(
-                text=word["text"],
-                start_ms=int(word["start"] * 1000),
-                end_ms=int(word["end"] * 1000),
+                text=word.text,
+                start_ms=word.start_ms,
+                end_ms=word.end_ms,
             )
             ppad.word_level_transcriptions.append(wlt)
 
