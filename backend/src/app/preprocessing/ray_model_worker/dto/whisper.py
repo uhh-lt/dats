876670539825ee80a361333b761a1e9571561ef1
floschne <florian.schneider-1@uni-hamburg.de>
2023-09-14 10:17:42 +0000

    updated whisper dto, model and loading in ray worker

--- backend/src/app/preprocessing/ray_model_worker/dto/whisper.py
+++ backend/src/app/preprocessing/ray_model_worker/dto/whisper.py
@@ -1,11 +1,52 @@
-from typing import Any, Dict
+from typing import Any, Dict, List
 
 from pydantic import BaseModel, Field
 
 
-class WhisperInput(BaseModel):
+class WordTranscription(BaseModel):
+    start_ms: int = Field(
+        description="The start time of the word in milliseconds.", example=0
+    )
+    end_ms: int = Field(
+        description="The end time of the word in milliseconds.", example=1000
+    )
+    text: str = Field(
+        description="The surface form of the transcriped word.", example="word"
+    )
+
+
+class SegmentTranscription(BaseModel):
+    start_ms: int = Field(
+        description="The start time of the word in milliseconds.", example=0
+    )
+    end_ms: int = Field(
+        description="The end time of the word in milliseconds.", example=1000
+    )
+    words: List[WordTranscription] = Field(
+        description="The words of the transcription segment.",
+        example=[{"text": "word", "start_ms": 0, "end_ms": 1000}],
+        default_factory=list,
+    )
+
+
+class WhisperFilePathInput(BaseModel):
     uncompressed_audio_fp: str = Field(example="/path/to/uncompressed/audio.wav")
 
 
-class WhisperOutput(BaseModel):
-    out: Dict[str, Any] = Field(example={"transcript": "Hello World!"})
+class WhisperTranscriptionOutput(BaseModel):
+    segments: List[SegmentTranscription] = Field(
+        description="The transcription of the audio file.",
+        example=[
+            {
+                "start_ms": 0,
+                "end_ms": 1000,
+                "words": [
+                    {
+                        "text": "word",
+                        "start_ms": 0,
+                        "end_ms": 1000,
+                    }
+                ],
+            }
+        ],
+    )
