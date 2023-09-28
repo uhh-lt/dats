6a2299e15ac3dcf428f6955bf152057ba36f6933
floschne <florian.schneider-1@uni-hamburg.de>
2023-09-28 08:42:29 +0000

    improved entrypoint script for bg jobs worker and fixed issues for long running celery worker jobs

--- backend/src/celery_background_jobs_worker_entrypoint.sh
+++ backend/src/celery_background_jobs_worker_entrypoint.sh
@@ -13,10 +13,7 @@ CELERY_BACKGROUND_JOBS_WORKER_CONCURRENCY=${CELERY_BACKGROUND_JOBS_WORKER_CONCUR
 
 if [ "$CELERY_DEBUG_MODE" -eq 1 ]; then
   echo "Running celery in debug mode!"
-  pip install debugpy -t /tmp && python /tmp/debugpy --listen 0.0.0.0:6900 -m celery -A app.celery.background_jobs.tasks worker -Q bgJobsQ,celery -l "$LOG_LEVEL" -P solo
-elif [ "$CELERY_BACKGROUND_JOBS_WORKER_CONCURRENCY" -le 1 ]; then
-  echo "NEVER USE THIS WORKER FOR GPU JOBS!"
-  celery -A app.celery.background_jobs.tasks worker -Q bgJobsQ,celery -l "$LOG_LEVEL" -P solo
+  pip install debugpy -t /tmp && python /tmp/debugpy --listen 0.0.0.0:6900 -m celery -A app.celery.background_jobs.tasks worker -Q bgJobsQ,celery -l "$LOG_LEVEL" -c 1 --without-gossip --without-mingle --without-heartbeat
 else
-  celery -A app.celery.background_jobs.tasks worker -Q bgJobsQ,celery -l "$LOG_LEVEL" -c "$CELERY_BACKGROUND_JOBS_WORKER_CONCURRENCY"
+  celery -A app.celery.background_jobs.tasks worker -Q bgJobsQ,celery -l "$LOG_LEVEL" -c "$CELERY_BACKGROUND_JOBS_WORKER_CONCURRENCY" --without-gossip --without-mingle --without-heartbeat
 fi
