dbd84b84835ceaab30bdf135db59102036b3efea
Alienmaster <robert.geislinger@uni-hamburg.de>
2023-10-19 11:23:36 +0000

    ORMBase update for sqlalchemy 2.x

--- backend/src/app/core/data/orm/orm_base.py
+++ backend/src/app/core/data/orm/orm_base.py
@@ -1,11 +1,11 @@
 from typing import Any, Optional, Set
 
 from sqlalchemy import inspect
-from sqlalchemy.ext.declarative import as_declarative, declared_attr
+from sqlalchemy.ext.declarative import declared_attr
+from sqlalchemy.orm import DeclarativeBase
 
 
-@as_declarative()
-class ORMBase:
+class ORMBase(DeclarativeBase):
     id: Any
     __name__: str
 
