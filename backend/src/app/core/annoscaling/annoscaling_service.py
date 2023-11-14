975b942efa2e5eefd03656cc003521dd5a6ee751
Fynn Petersen-Frey <fynn.petersen-frey@uni-hamburg.de>
2023-11-14 14:16:50 +0000

    measure perf

--- backend/src/app/core/annoscaling/annoscaling_service.py
+++ backend/src/app/core/annoscaling/annoscaling_service.py
@@ -1,4 +1,5 @@
 from typing import Dict, List, Tuple
+from time import perf_counter_ns
 
 from app.core.db.sql_service import SQLService
 from app.util.singleton_meta import SingletonMeta
@@ -22,20 +23,38 @@ class AnnoScalingService(metaclass=SingletonMeta):
         return super(AnnoScalingService, cls).__new__(cls)
 
     def suggest(self, project_id: int, user_ids: List[int], code_id: int) -> List[str]:
+        start_time = perf_counter_ns()
+        # takes 4ms (small project)
         occurrences = self.__get_annotations(project_id, user_ids, code_id)
-        sdoc_sentences = self.__get_sentences({id for _,_, id in occurrences})
+        end_time = perf_counter_ns()
+        print("it took", end_time - start_time, "ns to get annotations from the DB")
+
+        start_time = perf_counter_ns()
+        # takes 2ms (small project)
+        sdoc_sentences = self.__get_sentences({id for _, _, id in occurrences})
+        end_time = perf_counter_ns()
+        print("it took", end_time - start_time, "ns to get sentences from the DB")
 
         sdoc_sent_ids = []
+
+        start_time = perf_counter_ns()
+        # takes around 0.1ms per annotation
         for start, end, sdoc_id in occurrences:
             # TODO loops are bad, need a much faster way to link annotations to sentences
             # best: do everything in DB and only return sentence ID per annotation
-            # alternative: load all from DB (in chunks?) and compute via numpy 
+            # alternative: load all from DB (in chunks?) and compute via numpy
             starts, ends, _ = sdoc_sentences[sdoc_id]
-            sent_match = self.__best_match(
-                starts, ends, start, end
-            )
+            sent_match = self.__best_match(starts, ends, start, end)
             sdoc_sent_ids.append((sdoc_id, sent_match))
+        end_time = perf_counter_ns()
+        print("it took", end_time - start_time, "ns to match annotations to sentences")
+        start_time = perf_counter_ns()
+        # takes around 20ms per object. so, 50 annotations take already 1 full second
         hits = self.sim.suggest_similar_sentences(project_id, sdoc_sent_ids)
+        end_time = perf_counter_ns()
+        print(
+            "it took", end_time - start_time, "ns to get similar sentences from index"
+        )
         sim_doc_sentences = self.__get_sentences({hit.sdoc_id for hit in hits})
 
         texts = []
@@ -72,16 +91,22 @@ class AnnoScalingService(metaclass=SingletonMeta):
             )
             res = query.all()
             return [(r[0].begin, r[0].end, r[1]) for r in res]
-    
-    def __get_sentences(self, sdoc_ids : List[int]) -> Dict[int, Tuple[List[int], List[int]]]:
+
+    def __get_sentences(
+        self, sdoc_ids: List[int]
+    ) -> Dict[int, Tuple[List[int], List[int]]]:
         with self.sqls.db_session() as db:
-            query = db.query(SourceDocumentDataORM.id, SourceDocumentDataORM.sentence_starts, SourceDocumentDataORM.sentence_ends, SourceDocumentDataORM.content).filter(SourceDocumentDataORM.id.in_(sdoc_ids))
+            query = db.query(
+                SourceDocumentDataORM.id,
+                SourceDocumentDataORM.sentence_starts,
+                SourceDocumentDataORM.sentence_ends,
+                SourceDocumentDataORM.content,
+            ).filter(SourceDocumentDataORM.id.in_(sdoc_ids))
             res = query.all()
             return {r[0]: (r[1], r[2], r[3]) for r in res}
 
-
     def __best_match(self, starts: List[int], ends: List[int], begin: int, end: int):
-        overlap = [self.__overlap(s,e , begin, end) for s,e in zip(starts, ends)]
+        overlap = [self.__overlap(s, e, begin, end) for s, e in zip(starts, ends)]
         return np.asarray(overlap).argmax().item()
 
     def __overlap(self, s1: int, e1: int, s2: int, e2: int):
