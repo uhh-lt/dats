dcf991a4324c5a5d3ab2d6b30256ff70253e142c
Rafael Epplée <hello@rafa.ee>
2023-11-16 16:48:26 +0100

    Fix type error when using SqlService().db_session()
    By adding typehints to the SingletonMeta constructor, we can tell
    pyright the correct type to use when resolving available methods on the
    singleton instance.

--- backend/src/app/util/singleton_meta.py
+++ backend/src/app/util/singleton_meta.py
@@ -1,6 +1,9 @@
 from abc import ABCMeta
+from typing import Type, TypeVar
 
 from loguru import logger
+
+SingletonInstance = TypeVar("SingletonInstance")
 
 
 class SingletonMeta(ABCMeta):
@@ -9,10 +12,12 @@ class SingletonMeta(ABCMeta):
         logger.info(f"Instantiating {class_name} Singleton...")
         super().__init__(class_name, bases, attrs)
 
-    def __call__(cls, *args, **kwargs):
-        if cls.__singleton:
-            return cls.__singleton
+    def __call__(cls: Type[SingletonInstance], *args, **kwargs) -> SingletonInstance:
+        # Not sure if there's a way to typehint the __singleton
+        # attribute somehow.
+        if cls.__singleton:  # type: ignore
+            return cls.__singleton  # type: ignore
         singleton = cls.__new__(cls, *args, **kwargs)
         singleton.__init__(*args, **kwargs)
-        cls.__singleton = singleton
+        cls.__singleton = singleton  # type: ignore
         return singleton
