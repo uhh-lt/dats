4b08563a479a90e6768aca8a1507271acc8e9a76
Tim Fischer <tim.fischer@uni-hamburg.de>
2023-11-30 14:39:14 +0000

    updated project tags to show hierarchy. Filtering function removed :( Has to be added later

--- frontend/src/views/projectsettings/update/ProjectTags.tsx
+++ frontend/src/views/projectsettings/update/ProjectTags.tsx
@@ -1,142 +1,19 @@
-import DeleteIcon from "@mui/icons-material/Delete";
-import LabelIcon from "@mui/icons-material/Label";
-import {
-  Box,
-  CardContent,
-  Divider,
-  IconButton,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-  TextField,
-  Toolbar,
-  Tooltip,
-  Typography,
-} from "@mui/material";
-import React, { useMemo, useState } from "react";
-import ProjectHooks from "../../../api/ProjectHooks";
-import TagHooks from "../../../api/TagHooks";
-import { ContextMenuPosition } from "../../../components/ContextMenu/ContextMenuPosition";
+import { Box, Divider, Toolbar, Typography } from "@mui/material";
 import TagCreateDialog from "../../../features/CrudDialog/Tag/TagCreateDialog";
-import TagEditDialog from "../../../features/CrudDialog/Tag/TagEditDialog";
-import SnackbarAPI from "../../../features/Snackbar/SnackbarAPI";
+import TagExplorer from "../../../features/TagExplorer/TagExplorer";
 import { ProjectProps } from "./ProjectProps";
-import ProjectTagsContextMenu from "./ProjectTagsContextMenu";
-import TagEditButton from "../../../features/TagExplorer/TagEditButton";
-import TagCreationButton from "../../search/ToolBar/ToolBarElements/TagMenu/TagMenuCreateButton";
 
 function ProjectTags({ project }: ProjectProps) {
-  // global server state (react query)
-  const projectTags = ProjectHooks.useGetAllTags(project.id);
-
-  const [tagFilter, setTagFilter] = useState<string>("");
-
-  // context menu
-  const [contextMenuPosition, setContextMenuPosition] = useState<ContextMenuPosition | null>(null);
-  const [contextMenuData, setContextMenuData] = useState<number>();
-  const onContextMenu = (userId: number) => (event: React.MouseEvent) => {
-    event.preventDefault();
-    setContextMenuPosition({ x: event.clientX, y: event.clientY });
-    setContextMenuData(userId);
-  };
-
-  const removeTagMutation = TagHooks.useDeleteTag();
-  const handleRemoveTag = (tagId: number) => {
-    removeTagMutation.mutate(
-      {
-        tagId: tagId,
-      },
-      {
-        onSuccess: (data) => {
-          SnackbarAPI.openSnackbar({
-            text: "Successfully removed tag " + data.title + "!",
-            severity: "success",
-          });
-        },
-      },
-    );
-  };
-
-  const projectTagsFiltered = useMemo(() => {
-    const trimmedFilter = tagFilter.trim().toLowerCase();
-    if (projectTags.data) {
-      if (trimmedFilter.length > 0) {
-        return projectTags.data.filter((tag) => tag.title.toLowerCase().includes(trimmedFilter));
-      } else {
-        return projectTags.data;
-      }
-    } else {
-      return [];
-    }
-  }, [projectTags.data, tagFilter]);
-
   return (
     <Box display="flex" className="myFlexContainer h100">
       <Toolbar variant="dense" style={{ paddingRight: "8px" }} className="myFlexFitContentContainer">
         <Typography variant="h6" color="inherit" component="div">
-          Filter tags
+          Tags
         </Typography>
-        <TextField
-          sx={{ ml: 1, flex: 1 }}
-          placeholder={"type name here..."}
-          variant="outlined"
-          size="small"
-          value={tagFilter}
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-            setTagFilter(event.target.value);
-          }}
-        />
       </Toolbar>
       <Divider />
-      {projectTags.isLoading && <CardContent>Loading project tags...</CardContent>}
-      {projectTags.isError && (
-        <CardContent>An error occurred while loading project tags for project {project.id}...</CardContent>
-      )}
-      <List disablePadding>
-        <TagCreationButton tagName={""} />
-      </List>
-      {projectTags.isSuccess && (
-        <>
-          <List disablePadding className="myFlexFillAllContainer">
-            {projectTagsFiltered.map((tag) => (
-              <ListItem
-                disablePadding
-                key={tag.id}
-                onContextMenu={onContextMenu(tag.id)}
-                secondaryAction={
-                  <>
-                    <Tooltip title={"Remove tag from project"}>
-                      <span>
-                        <IconButton onClick={() => handleRemoveTag(tag.id)}>
-                          <DeleteIcon />
-                        </IconButton>
-                      </span>
-                    </Tooltip>
-                    <TagEditButton tag={tag} />
-                  </>
-                }
-              >
-                <ListItemButton>
-                  <ListItemIcon sx={{ minWidth: "32px" }}>
-                    <LabelIcon style={{ color: tag.color }} />
-                  </ListItemIcon>
-                  <ListItemText primary={tag.title} />
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </List>
-          <TagEditDialog tags={projectTags.data} />
-        </>
-      )}
+      <TagExplorer sx={{ pt: 0 }} showButtons />
       <TagCreateDialog />
-      <ProjectTagsContextMenu
-        position={contextMenuPosition}
-        tagId={contextMenuData}
-        handleClose={() => setContextMenuPosition(null)}
-        onDeleteTag={handleRemoveTag}
-      />
     </Box>
   );
 }
