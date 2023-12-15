ce420ca798a70f6e0b767c6aafa4eb02fa1ecbfb
Rafael Epplée <hello@rafa.ee>
2023-12-15 14:45:26 +0100

    Add authorization for project metadata

--- backend/src/api/endpoints/project_metadata.py
+++ backend/src/api/endpoints/project_metadata.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from api.dependencies import get_current_user, get_db_session
+from app.core.authorization.authz_user import AuthzUser
+from app.core.data.crud import Crud
 from app.core.data.crud.project_metadata import crud_project_meta
 from app.core.data.dto.project_metadata import (
     ProjectMetadataCreate,
@@ -23,8 +25,13 @@ router = APIRouter(
     description="Creates a new Metadata and returns it with the generated ID.",
 )
 async def create_new_metadata(
-    *, db: Session = Depends(get_db_session), metadata: ProjectMetadataCreate
+    *,
+    db: Session = Depends(get_db_session),
+    metadata: ProjectMetadataCreate,
+    authz_user: AuthzUser = Depends(),
 ) -> ProjectMetadataRead:
+    authz_user.assert_in_project(metadata.project_id)
+
     db_metadata = crud_project_meta.create(db=db, create_dto=metadata)
     return ProjectMetadataRead.model_validate(db_metadata)
 
@@ -36,9 +43,13 @@ async def create_new_metadata(
     description="Returns the Metadata with the given ID.",
 )
 async def get_by_id(
-    *, db: Session = Depends(get_db_session), metadata_id: int
+    *,
+    db: Session = Depends(get_db_session),
+    metadata_id: int,
+    authz_user: AuthzUser = Depends(),
 ) -> ProjectMetadataRead:
-    # TODO Flo: only if the user has access?
+    authz_user.assert_in_same_project_as(Crud.PROJECT_METADATA, metadata_id)
+
     db_obj = crud_project_meta.read(db=db, id=metadata_id)
     return ProjectMetadataRead.model_validate(db_obj)
 
@@ -54,8 +65,10 @@ async def update_by_id(
     db: Session = Depends(get_db_session),
     metadata_id: int,
     metadata: ProjectMetadataUpdate,
+    authz_user: AuthzUser = Depends(),
 ) -> ProjectMetadataRead:
-    # TODO Flo: only if the user has access?
+    authz_user.assert_in_same_project_as(Crud.PROJECT_METADATA, metadata_id)
+
     db_obj = crud_project_meta.update(
         db=db, metadata_id=metadata_id, update_dto=metadata
     )
@@ -69,8 +82,12 @@ async def update_by_id(
     description="Deletes the Metadata with the given ID.",
 )
 async def delete_by_id(
-    *, db: Session = Depends(get_db_session), metadata_id: int
+    *,
+    db: Session = Depends(get_db_session),
+    metadata_id: int,
+    authz_user: AuthzUser = Depends(),
 ) -> ProjectMetadataRead:
-    # TODO Flo: only if the user has access?
+    authz_user.assert_in_same_project_as(Crud.PROJECT_METADATA, metadata_id)
+
     db_obj = crud_project_meta.remove(db=db, id=metadata_id)
     return ProjectMetadataRead.model_validate(db_obj)
