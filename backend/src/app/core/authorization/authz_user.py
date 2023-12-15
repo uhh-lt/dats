88aaddfaa204f04fd3cbfd58c2d2bc4f77adec2a
Rafael Epplée <hello@rafa.ee>
2023-12-15 16:08:35 +0100

    Add authz_user.assert_in_same_project_as_many

--- backend/src/app/core/authorization/authz_user.py
+++ backend/src/app/core/authorization/authz_user.py
@@ -1,3 +1,5 @@
+from typing import List, NoReturn
+
 from fastapi import Depends, Request
 from loguru import logger
 from sqlalchemy.orm import Session
@@ -43,15 +45,40 @@ class AuthzUser:
             # for this line :(
             orm_object = crud.value.read(self.db, object_id)  # type: ignore
         except NoSuchElementError:
-            self.assert_bool(False, f"{crud.name} {object_id} does not exist")
-            return
+            self.deny_access(f"{crud.name} {object_id} does not exist")
 
         project_id = get_parent_project_id(orm_object)
         if project_id is None:
-            self.assert_bool(False, "Object has no parent project")
-            return
+            self.deny_access("Object has no parent project")
 
         self.assert_in_project(project_id)
+
+    def assert_in_same_project_as_many(
+        self, crud: Crud, object_ids: List[int] | List[str]
+    ):
+        if len(object_ids) == 0:
+            return
+
+        # Some read functions take an int, others take a str
+        # (e.g. PreprocessingJobs).
+        # There's no good way to bind the value of `Crud` to the
+        # type of `object_id`, so we switch off the type checker
+        # for this line :(
+        objs = crud.value.read_by_ids(self.db, object_ids)  # type: ignore
+        loaded_object_ids = {obj.id for obj in objs}
+        if loaded_object_ids != set(object_ids):
+            self.deny_access(
+                f"One or several objects were not found: {set(object_ids) - loaded_object_ids}"
+            )
+
+        required_project_ids = {get_parent_project_id(obj) for obj in objs}
+        if None in required_project_ids:
+            self.deny_access("One or several objects have no parent project")
+
+        user_project_ids = {proj.id for proj in self.user.projects}
+
+        for required_project_id in required_project_ids:
+            self.assert_bool(required_project_id in user_project_ids)
 
     def assert_object_has_same_user_id(self, crud: Crud, object_id: int | str):
         try:
@@ -62,13 +89,11 @@ class AuthzUser:
             # for this line :(
             orm_object = crud.value.read(self.db, object_id)  # type: ignore
         except NoSuchElementError:
-            self.assert_bool(False, "Object does not exist")
-            return
+            self.deny_access("Object does not exist")
 
         user_id = get_orm_user_id(orm_object)
         if user_id is None:
-            self.assert_bool(False, "Object has no user id set")
-            return
+            self.deny_access("Object has no user id set")
 
         self.assert_is_same_user(user_id)
 
@@ -83,10 +108,22 @@ class AuthzUser:
             .exists()
         ).scalar()
 
-        self.assert_bool(authorized_project_exists)
+        self.assert_bool(
+            authorized_project_exists, f"User needs to be in project {project_id}"
+        )
 
     def assert_bool(self, is_authorized: bool, note: str = ""):
-        # log decision
+        self.log_check(is_authorized, note)
+
+        if not is_authorized:
+            raise ForbiddenError()
+
+    def deny_access(self, note: str = "") -> NoReturn:
+        self.log_check(False, note)
+
+        raise ForbiddenError()
+
+    def log_check(self, is_authorized: bool, note: str = ""):
         result_description = "passed" if is_authorized else "failed"
 
         note = f", note: {note}" if note != "" else ""
@@ -94,6 +131,3 @@ class AuthzUser:
         logger.opt(depth=1).debug(
             f"check {result_description}: user {self.user.id}, {self.request.method} {self.request.url.path}{note}"
         )
-
-        if not is_authorized:
-            raise ForbiddenError()
--- backend/src/test/test_authz_user.py
+++ backend/src/test/test_authz_user.py
@@ -1,6 +1,7 @@
 from typing import Callable
 
 import pytest
+from sqlalchemy.orm import Session
 
 from app.core.authorization.authz_user import AuthzUser, ForbiddenError
 from app.core.data.crud import Crud
@@ -69,21 +70,62 @@ def test_assert_object_has_same_user_id(
 def test_assert_in_same_project_as(
     authz_user: AuthzUser,
     code: int,
-    session: SQLService,
     make_project: Callable[[], ProjectORM],
+    rollbacked_session: Session,
 ):
     authz_user.assert_in_same_project_as(Crud.CODE, code)
 
-    with session.db_session() as db:
-        project = make_project()
-        crud_project.dissociate_user(db, user_id=authz_user.user.id, proj_id=project.id)
-        code_orm = crud_code.read(db, code)
-        previous_project = code_orm.project
-        code_orm.project_id = project.id
-        db.commit()
+    project = make_project()
+    crud_project.dissociate_user(
+        rollbacked_session, user_id=authz_user.user.id, proj_id=project.id
+    )
+    code_orm = crud_code.read(rollbacked_session, code)
+    previous_project = code_orm.project
+    code_orm.project_id = project.id
 
-        with pytest.raises(ForbiddenError):
-            authz_user.assert_in_same_project_as(Crud.CODE, code)
+    with pytest.raises(ForbiddenError):
+        # Check code that exists but is not in project
+        authz_user.assert_in_same_project_as(Crud.CODE, code)
 
-        code_orm.project_id = previous_project.id
-        db.commit()
+    with pytest.raises(ForbiddenError):
+        # Check code that does not exist
+        authz_user.assert_in_same_project_as(Crud.CODE, code + 1)
+
+    with pytest.raises(ForbiddenError):
+        # Check entity that has no parent project
+        authz_user.assert_in_same_project_as(Crud.USER, authz_user.user.id)
+
+    code_orm.project_id = previous_project.id
+
+
+def test_assert_in_same_project_as_many(
+    authz_user: AuthzUser,
+    code: int,
+    rollbacked_session: Session,
+    make_project: Callable[[], ProjectORM],
+):
+    authz_user.assert_in_same_project_as_many(Crud.CODE, [code])
+    # This should always pass
+    authz_user.assert_in_same_project_as_many(Crud.CODE, [])
+
+    project = make_project()
+    crud_project.dissociate_user(
+        rollbacked_session, user_id=authz_user.user.id, proj_id=project.id
+    )
+    code_orm = crud_code.read(rollbacked_session, code)
+    previous_project = code_orm.project
+    code_orm.project_id = project.id
+
+    with pytest.raises(ForbiddenError):
+        # Check code that exists but is not in project
+        authz_user.assert_in_same_project_as(Crud.CODE, code)
+
+    with pytest.raises(ForbiddenError):
+        # Check code that does not exist
+        authz_user.assert_in_same_project_as(Crud.CODE, code + 1)
+
+    with pytest.raises(ForbiddenError):
+        # Check entity that has no parent project
+        authz_user.assert_in_same_project_as(Crud.USER, authz_user.user.id)
+
+    code_orm.project_id = previous_project.id
