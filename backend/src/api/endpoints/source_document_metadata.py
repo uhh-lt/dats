9141d394bcac7822a4588babc5e27d13d98d16e4
Rafael Epplée <hello@rafa.ee>
2023-12-15 16:45:22 +0100

    Add authorization for source document metadata

--- backend/src/api/endpoints/source_document_metadata.py
+++ backend/src/api/endpoints/source_document_metadata.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from api.dependencies import get_current_user, get_db_session
+from app.core.authorization.authz_user import AuthzUser
+from app.core.data.crud import Crud
 from app.core.data.crud.source_document_metadata import crud_sdoc_meta
 from app.core.data.dto.source_document_metadata import (
     SourceDocumentMetadataCreate,
@@ -22,8 +24,18 @@ router = APIRouter(
     description="Creates a new Metadata and returns it with the generated ID.",
 )
 async def create_new_metadata(
-    *, db: Session = Depends(get_db_session), metadata: SourceDocumentMetadataCreate
+    *,
+    db: Session = Depends(get_db_session),
+    metadata: SourceDocumentMetadataCreate,
+    authz_user: AuthzUser = Depends(),
 ) -> SourceDocumentMetadataRead:
+    authz_user.assert_in_same_project_as(
+        Crud.PROJECT_METADATA, metadata.project_metadata_id
+    )
+    authz_user.assert_in_same_project_as(
+        Crud.SOURCE_DOCUMENT, metadata.source_document_id
+    )
+
     db_metadata = crud_sdoc_meta.create(db=db, create_dto=metadata)
     return SourceDocumentMetadataRead.model_validate(db_metadata)
 
@@ -35,9 +47,13 @@ async def create_new_metadata(
     description="Returns the Metadata with the given ID.",
 )
 async def get_by_id(
-    *, db: Session = Depends(get_db_session), metadata_id: int
+    *,
+    db: Session = Depends(get_db_session),
+    metadata_id: int,
+    authz_user: AuthzUser = Depends(),
 ) -> SourceDocumentMetadataReadResolved:
-    # TODO Flo: only if the user has access?
+    authz_user.assert_in_same_project_as(Crud.SOURCE_DOCUMENT_METADATA, metadata_id)
+
     db_obj = crud_sdoc_meta.read(db=db, id=metadata_id)
     return SourceDocumentMetadataReadResolved.model_validate(db_obj)
 
@@ -53,8 +69,10 @@ async def update_by_id(
     db: Session = Depends(get_db_session),
     metadata_id: int,
     metadata: SourceDocumentMetadataUpdate,
+    authz_user: AuthzUser = Depends(),
 ) -> SourceDocumentMetadataRead:
-    # TODO Flo: only if the user has access?
+    authz_user.assert_in_same_project_as(Crud.SOURCE_DOCUMENT_METADATA, metadata_id)
+
     db_obj = crud_sdoc_meta.update(db=db, metadata_id=metadata_id, update_dto=metadata)
     return SourceDocumentMetadataRead.model_validate(db_obj)
 
@@ -66,8 +84,12 @@ async def update_by_id(
     description="Deletes the Metadata with the given ID.",
 )
 async def delete_by_id(
-    *, db: Session = Depends(get_db_session), metadata_id: int
+    *,
+    db: Session = Depends(get_db_session),
+    metadata_id: int,
+    authz_user: AuthzUser = Depends(),
 ) -> SourceDocumentMetadataRead:
-    # TODO Flo: only if the user has access?
+    authz_user.assert_in_same_project_as(Crud.SOURCE_DOCUMENT_METADATA, metadata_id)
+
     db_obj = crud_sdoc_meta.remove(db=db, id=metadata_id)
     return SourceDocumentMetadataRead.model_validate(db_obj)
