3da4f652a104b640559f3e78d62b16ad81a28836
Rafael Epplée <hello@rafa.ee>
2023-12-15 14:06:22 +0100

    Add authorization for memos

--- backend/src/api/endpoints/memo.py
+++ backend/src/api/endpoints/memo.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from api.dependencies import get_current_user, get_db_session
+from app.core.authorization.authz_user import AuthzUser
+from app.core.data.crud import Crud
 from app.core.data.crud.memo import crud_memo
 from app.core.data.dto.memo import MemoRead, MemoUpdate
 
@@ -16,8 +18,14 @@ router = APIRouter(
     summary="Returns the Memo",
     description="Returns the Memo with the given ID if it exists",
 )
-async def get_by_id(*, db: Session = Depends(get_db_session), memo_id: int) -> MemoRead:
-    # TODO Flo: only if the user has access?
+async def get_by_id(
+    *,
+    db: Session = Depends(get_db_session),
+    memo_id: int,
+    authz_user: AuthzUser = Depends(),
+) -> MemoRead:
+    authz_user.assert_in_same_project_as(Crud.MEMO, memo_id)
+
     db_obj = crud_memo.read(db=db, id=memo_id)
     return crud_memo.get_memo_read_dto_from_orm(db=db, db_obj=db_obj)
 
@@ -29,9 +37,14 @@ async def get_by_id(*, db: Session = Depends(get_db_session), memo_id: int) -> M
     description="Updates the Memo with the given ID if it exists",
 )
 async def update_by_id(
-    *, db: Session = Depends(get_db_session), memo_id: int, memo: MemoUpdate
+    *,
+    db: Session = Depends(get_db_session),
+    memo_id: int,
+    memo: MemoUpdate,
+    authz_user: AuthzUser = Depends(),
 ) -> MemoRead:
-    # TODO Flo: only if the user has access?
+    authz_user.assert_in_same_project_as(Crud.MEMO, memo_id)
+
     db_obj = crud_memo.update(db=db, id=memo_id, update_dto=memo)
     return crud_memo.get_memo_read_dto_from_orm(db=db, db_obj=db_obj)
 
@@ -43,8 +56,12 @@ async def update_by_id(
     description="Removes the Memo with the given ID if it exists",
 )
 async def delete_by_id(
-    *, db: Session = Depends(get_db_session), memo_id: int
+    *,
+    db: Session = Depends(get_db_session),
+    memo_id: int,
+    authz_user: AuthzUser = Depends(),
 ) -> MemoRead:
-    # TODO Flo: only if the user has access?
+    authz_user.assert_in_same_project_as(Crud.MEMO, memo_id)
+
     memo = crud_memo.remove(db=db, id=memo_id)
     return MemoRead.model_validate(memo)
