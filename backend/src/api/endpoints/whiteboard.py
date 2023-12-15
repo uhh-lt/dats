babff2de882742d7069c3e236514c834b3f26a07
Rafael Epplée <hello@rafa.ee>
2023-12-15 18:03:03 +0100

    Add authorization for whiteboards

--- backend/src/api/endpoints/whiteboard.py
+++ backend/src/api/endpoints/whiteboard.py
@@ -4,6 +4,8 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from api.dependencies import get_current_user, get_db_session
+from app.core.authorization.authz_user import AuthzUser
+from app.core.data.crud import Crud
 from app.core.data.crud.whiteboard import crud_whiteboard
 from app.core.data.dto.whiteboard import (
     WhiteboardCreate,
@@ -23,8 +25,14 @@ router = APIRouter(
     description="Creates an Whiteboard",
 )
 async def create(
-    *, db: Session = Depends(get_db_session), whiteboard: WhiteboardCreate
+    *,
+    db: Session = Depends(get_db_session),
+    whiteboard: WhiteboardCreate,
+    authz_user: AuthzUser = Depends(),
 ) -> WhiteboardRead:
+    authz_user.assert_in_project(whiteboard.project_id)
+    authz_user.assert_is_same_user(whiteboard.user_id)
+
     return WhiteboardRead.model_validate(
         crud_whiteboard.create(db=db, create_dto=whiteboard)
     )
@@ -37,8 +45,13 @@ async def create(
     description="Returns the Whiteboard with the given ID if it exists",
 )
 async def get_by_id(
-    *, db: Session = Depends(get_db_session), whiteboard_id: int
+    *,
+    db: Session = Depends(get_db_session),
+    whiteboard_id: int,
+    authz_user: AuthzUser = Depends(),
 ) -> WhiteboardRead:
+    authz_user.assert_in_same_project_as(Crud.WHITEBOARD, whiteboard_id)
+
     db_obj = crud_whiteboard.read(db=db, id=whiteboard_id)
     return WhiteboardRead.model_validate(db_obj)
 
@@ -53,7 +66,10 @@ async def get_by_project(
     *,
     db: Session = Depends(get_db_session),
     project_id: int,
+    authz_user: AuthzUser = Depends(),
 ) -> List[WhiteboardRead]:
+    authz_user.assert_in_project(project_id)
+
     db_objs = crud_whiteboard.read_by_project(db=db, project_id=project_id)
     return [WhiteboardRead.model_validate(db_obj) for db_obj in db_objs]
 
@@ -65,8 +81,14 @@ async def get_by_project(
     description="Returns the Whiteboard of the Project with the given ID and the User with the given ID if it exists",
 )
 async def get_by_project_and_user(
-    *, db: Session = Depends(get_db_session), project_id: int, user_id: int
+    *,
+    db: Session = Depends(get_db_session),
+    project_id: int,
+    user_id: int,
+    authz_user: AuthzUser = Depends(),
 ) -> List[WhiteboardRead]:
+    authz_user.assert_in_project(project_id)
+
     db_objs = crud_whiteboard.read_by_project_and_user(
         db=db, project_id=project_id, user_id=user_id
     )
@@ -84,7 +106,10 @@ async def update_by_id(
     db: Session = Depends(get_db_session),
     whiteboard_id: int,
     whiteboard: WhiteboardUpdate,
+    authz_user: AuthzUser = Depends(),
 ) -> WhiteboardRead:
+    authz_user.assert_in_same_project_as(Crud.WHITEBOARD, whiteboard_id)
+
     db_obj = crud_whiteboard.update(db=db, id=whiteboard_id, update_dto=whiteboard)
     return WhiteboardRead.model_validate(db_obj)
 
@@ -96,8 +121,12 @@ async def update_by_id(
     description="Removes the Whiteboard with the given ID if it exists",
 )
 async def delete_by_id(
-    *, db: Session = Depends(get_db_session), whiteboard_id: int
+    *,
+    db: Session = Depends(get_db_session),
+    whiteboard_id: int,
+    authz_user: AuthzUser = Depends(),
 ) -> WhiteboardRead:
-    # TODO Flo: only if the user has access?
+    authz_user.assert_in_same_project_as(Crud.WHITEBOARD, whiteboard_id)
+
     db_obj = crud_whiteboard.remove(db=db, id=whiteboard_id)
     return WhiteboardRead.model_validate(db_obj)
