17665a0c07653fb756b6879d8343a8724f5003d2
Rafael Epplée <hello@rafa.ee>
2023-12-18 12:50:59 +0100

    Add key attribute to feedback cards

--- frontend/src/views/feedback/FeedbackCards.tsx
+++ frontend/src/views/feedback/FeedbackCards.tsx
@@ -11,7 +11,7 @@ function FeedbackCards({ feedback, showReplyTo }: FeedbackCardsProps) {
   return (
     <Stack spacing={2} px={0.5} pb={2}>
       {feedback.map((feedback) => (
-        <FeedbackCard feedback={feedback} showReplyTo={showReplyTo} />
+        <FeedbackCard feedback={feedback} showReplyTo={showReplyTo} key={feedback.id} />
       ))}
     </Stack>
   );
