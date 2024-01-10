976a243c5ac6c1f02cd902de0f40ec7322155bbb
Tim Fischer <tim.fischer@uni-hamburg.de>
2024-01-10 17:14:34 +0000

    fixed bug: cyclic dependencies of PipelineStep

--- backend/src/app/core/analysis/cota/pipeline/cargo.py
+++ backend/src/app/core/analysis/cota/pipeline/cargo.py
@@ -1,23 +1,27 @@
-from typing import TYPE_CHECKING, Any, Dict, List
+from typing import Any, Dict, List
 
-from pydantic import BaseModel, ConfigDict, Field
+from pydantic import BaseModel, ConfigDict, Field, SkipValidation
 
 from app.core.data.dto.concept_over_time_analysis import COTARefinementJobRead
 
-if TYPE_CHECKING:
-    from app.core.analysis.cota.pipeline.step import PipelineStep
+# if TYPE_CHECKING:
+#     from app.core.analysis.cota.pipeline.step import PipelineStep
 
 
 class Cargo(BaseModel):
     job: COTARefinementJobRead = Field(description="The COTARefinementJob")
 
-    next_steps: List["PipelineStep"] = Field(
+    next_steps: List[
+        SkipValidation
+    ] = Field(  # FIXME: "Hack" to ignore the cyclic dependency problem with "PipelineStep"
         description="Next Tasks to be executed.", default_factory=list
     )
 
     finished_steps: List[
-        "PipelineStep"  # SkipValidation[PipelineStep]
-    ] = Field(description="Tasks that have been executed.", default_factory=list)
+        SkipValidation
+    ] = Field(  # FIXME: "Hack" to ignore the cyclic dependency problem with "PipelineStep"
+        description="Tasks that have been executed.", default_factory=list
+    )
 
     data: Dict[str, Any] = Field(description="data", default_factory=dict)
 
