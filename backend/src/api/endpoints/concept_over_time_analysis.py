23e309d04d92acd076cff8ee0fe8e2deccd67750
floschne <florian.schneider-1@uni-hamburg.de>
2024-01-10 11:32:21 +0000

    removed optional from cota endpoints

--- backend/src/api/endpoints/concept_over_time_analysis.py
+++ backend/src/api/endpoints/concept_over_time_analysis.py
@@ -1,5 +1,3 @@
-from typing import Optional
-
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
@@ -18,19 +16,19 @@ router = APIRouter(prefix="/cota", tags=["conceptOverTimeAnalysis"])
 
 @router.put(
     "",
-    response_model=Optional[COTARead],
+    response_model=COTARead,
     summary="Creates an ConceptOverTimeAnalysis",
     description="Creates an ConceptOverTimeAnalysis",
 )
 async def create(
     *, db: Session = Depends(get_db_session), cota: COTACreate
-) -> Optional[COTARead]:
+) -> COTARead:
     return cotas.create(db=db, cota_create=cota)
 
 
 @router.get(
     "/{cota_id}",
-    response_model=Optional[COTARead],
+    response_model=COTARead,
     summary="Returns the ConceptOverTimeAnalysis",
     description="Returns the ConceptOverTimeAnalysis with the given ID if it exists",
 )
@@ -38,48 +36,27 @@ async def get_by_id(
     *,
     db: Session = Depends(get_db_session),
     cota_id: int,
-    return_sentence_text: bool = False
-) -> Optional[COTARead]:
+    return_sentence_text: bool = False,
+) -> COTARead:
     cota = cotas.read_by_id(
         db=db, cota_id=cota_id, return_sentence_text=return_sentence_text
     )
     return cota
 
 
-# @router.get(
-#     "/project/{project_id}/user/{user_id}",
-#     response_model=List[Union[COTARead, COTAReadWithSentenceText]],
-#     summary="Returns COTAs of the Project of the User",
-#     description="Returns the ConceptOverTimeAnalysis of the Project with the given ID and the User with the given ID if it exists",
-# )
-# async def get_by_project_and_user(
-#     *,
-#     db: Session = Depends(get_db_session),
-#     project_id: int,
-#     user_id: int,
-#     return_sentence_text: bool = False
-# ) -> List[Union[COTARead, COTAReadWithSentenceText]]:
-#     return cotas.read_by_project_and_user(
-#         db=db,
-#         project_id=project_id,
-#         user_id=user_id,
-#         return_sentence_text=return_sentence_text,
-#     )
-
-
 @router.patch(
     "/{cota_id}",
-    response_model=Optional[COTARead],
-    summary="Updates the Analysis Table",
-    description="Updates the Analysis Table with the given ID if it exists",
+    response_model=COTARead,
+    summary="Updates the ConceptOverTimeAnalysis",
+    description="Updates the ConceptOverTimeAnalysis with the given ID if it exists",
 )
 async def update_by_id(
     *,
     db: Session = Depends(get_db_session),
     cota_id: int,
     cota_upate: COTAUpdate,
-    return_sentence_text: bool = False
-) -> Optional[COTARead]:
+    return_sentence_text: bool = False,
+) -> COTARead:
     return cotas.update(
         db=db,
         cota_id=cota_id,
@@ -90,11 +67,11 @@ async def update_by_id(
 
 @router.delete(
     "/{cota_id}",
-    response_model=Optional[COTARead],
+    response_model=COTARead,
     summary="Removes the ConceptOverTimeAnalysis",
     description="Removes the ConceptOverTimeAnalysis with the given ID if it exists",
 )
 async def delete_by_id(
     *, db: Session = Depends(get_db_session), cota_id: int
-) -> Optional[COTARead]:
+) -> COTARead:
     return cotas.delete_by_id(db=db, cota_id=cota_id)
