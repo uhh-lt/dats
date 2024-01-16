52df82ef16ff21b0622c745a1896de21aaab9020
Tim Fischer <tim.fischer@uni-hamburg.de>
2024-01-16 16:45:46 +0000

    fixed renaming bug

--- backend/src/app/core/analysis/cota/pipeline/__init__.py
+++ backend/src/app/core/analysis/cota/pipeline/__init__.py
@@ -8,7 +8,7 @@ def build_cota_refinement_pipeline(foo: str = "bar") -> COTARefinementPipeline:
     from app.core.analysis.cota.pipeline.steps.toy import (
         add_date_to_search_space,
         compute_result,
-        init_or_find_concept_embedding_model,
+        init_concept_embedding_model,
         init_or_load_initial_search_space,
         init_search_space_reduced_embeddings,
         refine_search_space_reduced_embeddings_with_cem,
@@ -22,7 +22,7 @@ def build_cota_refinement_pipeline(foo: str = "bar") -> COTARefinementPipeline:
     pipeline.register_step(
         init_search_space_reduced_embeddings, required_data=["search_space"]
     )
-    pipeline.register_step(init_or_find_concept_embedding_model, required_data=[])
+    pipeline.register_step(init_concept_embedding_model, required_data=[])
     pipeline.register_step(train_cem, required_data=[])
     pipeline.register_step(
         refine_search_space_reduced_embeddings_with_cem, required_data=[]
