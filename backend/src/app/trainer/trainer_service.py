d1686fd595577f17166caec94b3a571346f17f8e
floschne <florian.schneider-1@uni-hamburg.de>
2024-01-19 17:56:28 +0000

    updated trainer service to remove sbert and datasets dep

--- backend/src/app/trainer/trainer_service.py
+++ backend/src/app/trainer/trainer_service.py
@@ -1,15 +1,10 @@
-import os
 from typing import List
 
 import online_triplet_loss.losses as otl
 import torch
 import torch.nn as nn
-from datasets import load_dataset
 from loguru import logger
-from sentence_transformers import InputExample, SentenceTransformer
-from sentence_transformers.losses import CosineSimilarityLoss
 from sqlalchemy.orm import Session
-from torch.utils.data import DataLoader
 
 from app.celery.background_jobs import start_trainer_job_async, use_trainer_model_async
 from app.core.data.crud.project import crud_project
@@ -63,7 +58,7 @@ class TrainerService(metaclass=SingletonMeta):
         trainer_job = self.redis.store_trainer_job(trainer_job)
 
         try:
-            trainer_job = self.__finetune_cota(trainer_job)
+            # TODO: decide from the trainer job what model to train...
             # trainer_job = self.__finetune_sbert_model(trainer_job)
             trainer_job.status = BackgroundJobStatus.FINISHED
             trainer_job = self.redis.store_trainer_job(trainer_job)
@@ -72,87 +67,6 @@ class TrainerService(metaclass=SingletonMeta):
             trainer_job.status = BackgroundJobStatus.ERROR
             trainer_job = self.redis.store_trainer_job(trainer_job)
             raise e
-
-        return trainer_job
-
-    def _use_trainer_model_sync(self, trainer_job_id: str) -> List[float]:
-        trainer_job = self.redis.load_trainer_job(trainer_job_id)
-        logger.info(f"Using trained model: {trainer_job.saved_model_path}")
-
-        model = SentenceTransformer(trainer_job.saved_model_path)
-
-        toy_emb = model.encode(["Hello World!"])[0].tolist()
-
-        del model
-        torch.cuda.empty_cache()
-
-        return toy_emb
-
-    def __prepare_sbert_training_data(self) -> DataLoader:
-        logger.info("Loading dataset...")
-        dataset = load_dataset("SetFit/stsb")
-
-        logger.info("Preparing training data...")
-        train_examples = []
-        train_data = dataset["train"]
-
-        for i in range(len(train_data)):
-            s = train_data[i]
-            train_examples.append(
-                InputExample(texts=[s["text1"], s["text2"]], label=s["label"])
-            )
-
-        train_dataloader = DataLoader(train_examples, shuffle=True, batch_size=32)
-
-        return train_dataloader
-
-    def __finetune_sbert_model(self, trainer_job: TrainerJobRead) -> TrainerJobRead:
-        params = trainer_job.parameters
-
-        base_model_name = params.base_model_name
-        model_name = params.new_model_name
-
-        logger.info(f"Fine-tuning SBert model {base_model_name}...")
-
-        logger.info("Loading model...")
-        model = SentenceTransformer(
-            base_model_name,
-            cache_folder=os.environ["TRANSFORMERS_CACHE"],
-            device="cuda",
-        )
-
-        train_loss = CosineSimilarityLoss(model=model)
-        train_dataloader = self.__prepare_sbert_training_data()
-
-        num_epochs = 1
-        warmup_steps = int(len(train_dataloader) * num_epochs * 0.1)
-
-        logger.info("Starting training...")
-        out_p = (
-            self.repo.get_model_path(proj_id=params.project_id, model_name=model_name)
-            / "output"
-        )
-
-        model.fit(
-            train_objectives=[(train_dataloader, train_loss)],
-            epochs=num_epochs,
-            warmup_steps=warmup_steps,
-            checkpoint_path=str(
-                self.repo.get_model_path(
-                    proj_id=params.project_id, model_name=model_name
-                )
-                / "checkpoints"
-            ),
-            output_path=str(out_p),
-        )
-
-        logger.info("Saving model...")
-        model.save(path=str(out_p), model_name=model_name)
-
-        trainer_job.saved_model_path = str(out_p)
-
-        del model, train_loss, train_dataloader
-        torch.cuda.empty_cache()
 
         return trainer_job
 
@@ -183,22 +97,19 @@ class TrainerService(metaclass=SingletonMeta):
         # this is just to import otl and not get it removed by ruff
 
     def __finetune_cota(self, trainer_job: TrainerJobRead) -> TrainerJobRead:
-        import torch.optim as optim
-        from online_triplet_loss.losses import batch_hard_triplet_loss
-
-        trainloader_path = self.repo.get_dataloader_path(
+        trainloader_path = self.repo.get_dataloader_filename(
             proj_id=trainer_job.parameters.project_id,
             dataloader_name=trainer_job.parameters.train_dataloader_name,
         )
         dataloader = torch.load(trainloader_path)
         print(f"{len(dataloader)=}")
-        model_path = self.repo.get_model_path(
+        model_path = self.repo.get_model_filename(
             proj_id=trainer_job.parameters.project_id,
             model_name=trainer_job.parameters.train_model_name,
         )
         model = torch.load(model_path)
-        criterion = batch_hard_triplet_loss
-        optimizer = optim.AdamW(model.parameters(), lr=0.001)
+        criterion = otl.batch_hard_triplet_loss
+        optimizer = torch.optim.AdamW(model.parameters(), lr=0.001)
         for epoch in range(
             trainer_job.parameters.epochs
         ):  # loop over the dataset multiple times
