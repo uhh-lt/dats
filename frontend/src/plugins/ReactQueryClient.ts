61bc886ac91036ef0d8e6c887cb21cf1f48dca01
Rafael Epplée <hello@rafa.ee>
2024-01-26 14:58:53 +0100

    Add error reporting titles

--- frontend/src/plugins/ReactQueryClient.ts
+++ frontend/src/plugins/ReactQueryClient.ts
@@ -4,13 +4,15 @@ import SnackbarAPI from "../features/Snackbar/SnackbarAPI";
 
 const queryClient = new QueryClient({
   mutationCache: new MutationCache({
-    onError: (error) => {
-      let text = "An unknown error occurred.";
+    onError: (error, _variables, _context, mutation) => {
+      let title = mutation.meta?.errorDescription as string | undefined;
+      let text = "An unknown error occurred. This is a bug. Please report it to the developers!";
       if (error instanceof ApiError) {
         text = error.message + (error.body.detail ? ": " + error.body.detail : "");
       }
       SnackbarAPI.openSnackbar({
         text,
+        title,
         severity: "error",
       });
     },
