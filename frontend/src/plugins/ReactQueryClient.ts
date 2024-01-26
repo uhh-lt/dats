e717b586a75f311b77479d5f13f2a19f1a31f30e
Rafael Epplée <hello@rafa.ee>
2024-01-26 12:03:34 +0100

    Fix generic error snackbar not firing sometimes

--- frontend/src/plugins/ReactQueryClient.ts
+++ frontend/src/plugins/ReactQueryClient.ts
@@ -1,12 +1,16 @@
 import { MutationCache, QueryClient } from "@tanstack/react-query";
+import { ApiError } from "../api/openapi";
 import SnackbarAPI from "../features/Snackbar/SnackbarAPI";
 
 const queryClient = new QueryClient({
   mutationCache: new MutationCache({
     onError: (error) => {
+      let text = "An unknown error occurred.";
+      if (error instanceof ApiError) {
+        text = error.message + (error.body.detail ? ": " + error.body.detail : "");
+      }
       SnackbarAPI.openSnackbar({
-        // @ts-ignore
-        text: error.message + (error.body.detail ? ": " + error.body.detail : ""),
+        text,
         severity: "error",
       });
     },
