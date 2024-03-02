fbce6433614dc21e43f07649ddb0221792b179fa
Tim Fischer <tim.fischer@uni-hamburg.de>
2024-03-02 15:36:46 +0000

    refactored analysis dashboards (table, timeline, whiteboard)

--- frontend/src/features/AnalysisDashboard/AnalysisDashboard.tsx
+++ frontend/src/features/AnalysisDashboard/AnalysisDashboard.tsx
@@ -1,13 +1,14 @@
 import { Box, Card, CardContent, CardHeader, Container, Portal, Typography } from "@mui/material";
-import { ReactNode, useContext } from "react";
+import { MRT_TableInstance, MaterialReactTable } from "material-react-table";
+import { useContext } from "react";
 import { AppBarContext } from "../../layouts/TwoBarLayout.tsx";
+import { AnaylsisDashboardRow } from "./useAnalysisDashboardTable.tsx";
 
 interface AnalysisDashboardProps {
-  children: ReactNode;
   pageTitle: string;
   headerTitle: string;
-  headerCards: ReactNode;
-  bodyTitle: string;
+  subheaderTitle?: string;
+  table: MRT_TableInstance<AnaylsisDashboardRow>;
 }
 
 export default function AnalysisDashboard(props: AnalysisDashboardProps) {
@@ -22,23 +23,15 @@ export default function AnalysisDashboard(props: AnalysisDashboardProps) {
       </Portal>
 
       <Container maxWidth="xl" className="h100" style={{ display: "flex", flexDirection: "column" }} sx={{ py: 2 }}>
-        <Card sx={{ width: "100%", maxHeight: "400px", mb: 2 }} elevation={2} className="myFlexContainer">
-          <CardHeader title={props.headerTitle} />
-          <CardContent className="myFlexFillAllContainer">
-            <Box height="100%" overflow="auto" whiteSpace="nowrap" style={{ display: "flex", alignItems: "stretch" }}>
-              {props.headerCards}
-            </Box>
-          </CardContent>
-        </Card>
         <Card
           sx={{ width: "100%", minHeight: "225.5px" }}
           elevation={2}
           className="myFlexFillAllContainer myFlexContainer"
         >
-          <CardHeader title="Load whiteboard" />
+          <CardHeader title={props.headerTitle} subheader={props.subheaderTitle} />
           <CardContent className="myFlexFillAllContainer" style={{ padding: 0 }}>
             <div className="h100" style={{ width: "100%" }}>
-              {props.children}
+              <MaterialReactTable table={props.table} />
             </div>
           </CardContent>
         </Card>
