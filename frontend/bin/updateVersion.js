b6b7c120e653609e5f3a8c26b0ac8ce193718616
Tim Fischer <tim.fischer@uni-hamburg.de>
2024-05-03 13:46:22 +0000

    also update package-lock.json

--- frontend/bin/updateVersion.js
+++ frontend/bin/updateVersion.js
@@ -8,4 +8,8 @@ const packageJson = JSON.parse(fs.readFileSync("package.json"));
 packageJson.version = version;
 fs.writeFileSync("package.json", JSON.stringify(packageJson));
 
+const packageLockJson = JSON.parse(fs.readFileSync("package-lock.json"));
+packageLockJson.version = version;
+fs.writeFileSync("package-lock.json", JSON.stringify(packageLockJson));
+
 execSync(`npx prettier --write package.json`);
