8033e30a3500d44ac2c138b470e265ed0775ca60
Tim Fischer <tim.fischer@uni-hamburg.de>
2024-05-30 12:07:51 +0000

    updated api

--- frontend/src/api/openapi/models/TimelineAnalysisResult.ts
+++ frontend/src/api/openapi/models/TimelineAnalysisResult.ts
@@ -2,7 +2,7 @@
 /* istanbul ignore file */
 /* tslint:disable */
 /* eslint-disable */
-export type TimelineAnalysisResultNew = {
+export type TimelineAnalysisResult = {
   /**
    * The date.
    */
