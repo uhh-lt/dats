5d1419a1a078e924fd4b9aef4a86a8f02dd38109
Tim Fischer <tim.fischer@uni-hamburg.de>
2024-06-05 13:51:55 +0000

    fix ray

--- backend/src/app/preprocessing/ray_model_worker/apps/blip2.py
+++ backend/src/app/preprocessing/ray_model_worker/apps/blip2.py
@@ -4,7 +4,7 @@ from dto.blip2 import Blip2FilePathInput, Blip2Output
 from fastapi import FastAPI
 from models.blip2 import Blip2Model
 from ray import serve
-from ray.serve.handle import RayServeHandle
+from ray.serve.handle import DeploymentHandle
 
 logger = logging.getLogger("ray.serve")
 
@@ -14,13 +14,12 @@ api = FastAPI()
 @serve.deployment(num_replicas=1, route_prefix="/blip2")
 @serve.ingress(api)
 class Blip2Api:
-    def __init__(self, blip2_model_handle: RayServeHandle) -> None:
+    def __init__(self, blip2_model_handle: DeploymentHandle) -> None:
         self.blip2 = blip2_model_handle
 
     @api.post("/image_captioning", response_model=Blip2Output)
     async def image_captioning(self, input: Blip2FilePathInput):
-        predict_ref = await self.blip2.image_captioning.remote(input)
-        predict_result = await predict_ref
+        predict_result = await self.blip2.image_captioning.remote(input)
         return predict_result
 
 
--- backend/src/app/preprocessing/ray_model_worker/apps/clip.py
+++ backend/src/app/preprocessing/ray_model_worker/apps/clip.py
@@ -8,7 +8,7 @@ from dto.clip import (
 from fastapi import FastAPI
 from models.clip import ClipModel
 from ray import serve
-from ray.serve.handle import RayServeHandle
+from ray.serve.handle import DeploymentHandle
 
 logger = logging.getLogger("ray.serve")
 
@@ -18,19 +18,17 @@ api = FastAPI()
 @serve.deployment(num_replicas=1, route_prefix="/clip")
 @serve.ingress(api)
 class ClipApi:
-    def __init__(self, clip_model_handle: RayServeHandle) -> None:
+    def __init__(self, clip_model_handle: DeploymentHandle) -> None:
         self.clip = clip_model_handle
 
     @api.post("/embedding/text", response_model=ClipEmbeddingOutput)
     async def text_embedding(self, input: ClipTextEmbeddingInput):
-        predict_ref = await self.clip.text_embedding.remote(input)
-        predict_result = await predict_ref
+        predict_result = await self.clip.text_embedding.remote(input)
         return predict_result
 
     @api.post("/embedding/image", response_model=ClipEmbeddingOutput)
     async def image_embedding(self, input: ClipImageEmbeddingInput):
-        predict_ref = await self.clip.image_embedding.remote(input)
-        predict_result = await predict_ref
+        predict_result = await self.clip.image_embedding.remote(input)
         return predict_result
 
 
