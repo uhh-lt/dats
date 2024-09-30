d44dc77ea5fe5aac23297aefc1cb0c53ce024811
Tim Fischer <tim.fischer@uni-hamburg.de>
2024-09-30 12:53:53 +0000

    updated api

---
+++ frontend/src/api/openapi/models/SpanAnnotationUpdateBulk.ts
@@ -0,0 +1,14 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type SpanAnnotationUpdateBulk = {
+  /**
+   * ID of the SpanAnnotation
+   */
+  span_annotation_id: number;
+  /**
+   * Code the SpanAnnotation refers to
+   */
+  code_id: number;
+};
