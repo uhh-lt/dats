bdb78a04725debb98e6b5138de2c0402001b02da
Ahmad Khalidi <Auren@gmx.net>
2024-10-08 09:51:57 +0000

    make is_init as obsolete as possible

--- backend/src/app/preprocessing/pipeline/steps/text/create_pptd.py
+++ backend/src/app/preprocessing/pipeline/steps/text/create_pptd.py
@@ -8,25 +8,6 @@ repo = RepoService()
 
 
 def create_pptd(cargo: PipelineCargo) -> PipelineCargo:
-    filepath = repo._get_dst_path_for_project_sdoc_file(
-        proj_id=cargo.ppj_payload.project_id, filename=cargo.ppj_payload.filename
-    )
-    if not filepath.exists():
-        raise FileNotFoundError(f"File {filepath} not found in repository!")
-
-    pptd = PreProTextDoc(
-        filename=cargo.ppj_payload.filename,
-        project_id=cargo.ppj_payload.project_id,
-        mime_type=cargo.ppj_payload.mime_type,
-        filepath=filepath,
-    )
-
-    cargo.data["pptd"] = pptd
-
-    return cargo
-
-
-def create_pptd_with_metadata(cargo: PipelineCargo) -> PipelineCargo:
     logger.info("create pptd")
     filepath = repo._get_dst_path_for_project_sdoc_file(
         proj_id=cargo.ppj_payload.project_id, filename=cargo.ppj_payload.filename
@@ -34,19 +15,20 @@ def create_pptd_with_metadata(cargo: PipelineCargo) -> PipelineCargo:
     if not filepath.exists():
         raise FileNotFoundError(f"File {filepath} not found in repository!")
 
+    additional_parameters = dict()
+    if "metadata" in cargo.data:
+        additional_parameters["metadata"] = cargo.data["metadata"]
+    if "sdoc_link" in cargo.data:
+        additional_parameters["sdoc_link_create_dtos"] = cargo.data["sdoc_link"]
+    if "tags" in cargo.data:
+        additional_parameters["tags"] = cargo.data["tags"]
     pptd = PreProTextDoc(
         filename=cargo.ppj_payload.filename,
         project_id=cargo.ppj_payload.project_id,
         mime_type=cargo.ppj_payload.mime_type,
         filepath=filepath,
-        metadata=cargo.data["metadata"],
-        sdoc_link_create_dtos=cargo.data["sdoc_link"],
-        tags=cargo.data["tags"],
+        **additional_parameters,
     )
-    for auto in cargo.data["annotations"]:
-        if auto.code not in pptd.spans:
-            pptd.spans[auto.code] = list()
-        pptd.spans[auto.code].append(auto)
 
     cargo.data["pptd"] = pptd
 
