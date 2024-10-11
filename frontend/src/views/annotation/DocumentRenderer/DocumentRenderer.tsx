c85a13c4cd975e10915686b7f72b4640e4ab56fb
Tim Fischer <tim.fischer@uni-hamburg.de>
2024-10-11 08:40:56 +0000

    fixed: images not rendering when no annotator is selected

--- frontend/src/views/annotation/DocumentRenderer/DocumentRenderer.tsx
+++ frontend/src/views/annotation/DocumentRenderer/DocumentRenderer.tsx
@@ -1,6 +1,6 @@
 import { Box, BoxProps } from "@mui/material";
 import { useVirtualizer } from "@tanstack/react-virtual";
-import React, { useEffect, useMemo, useRef } from "react";
+import React, { useCallback, useEffect, useMemo, useRef } from "react";
 import "./DocumentRenderer.css";
 
 import { DOMNode, Element, HTMLReactParserOptions, domToReact } from "html-react-parser";
@@ -77,16 +77,70 @@ function DocumentRenderer({
     estimateSize: () => 155,
   });
 
+  const basicProcessingInstructions = useCallback(
+    (options: HTMLReactParserOptions) => (domNode: Element) => {
+      // images
+      if (domNode.name === "img" && domNode.attribs.src) {
+        const filename = domNode.attribs.src;
+        return <SdocImage key={`image-link-${filename}`} filename={filename} projectId={projectId} />;
+      }
+      //  videos
+      else if (domNode.name === "video" && domNode.attribs.src) {
+        const filename = domNode.attribs.src;
+        return (
+          <SdocVideoLink
+            key={`video-link-${filename}`}
+            filename={filename}
+            toPrefix={"../annotation/"}
+            projectId={projectId}
+          />
+        );
+      }
+      // audios
+      else if (domNode.name === "audio" && domNode.attribs.src) {
+        const filename = domNode.attribs.src;
+        return (
+          <SdocAudioLink
+            key={`audio-link-${filename}`}
+            filename={filename}
+            toPrefix={"../annotation/"}
+            projectId={projectId}
+          />
+        );
+      }
+      // sentences
+      else if (domNode.name === "sent" && domNode.attribs.id) {
+        const sentenceId = parseInt(domNode.attribs.id);
+        return (
+          <span
+            key={`sentence-${sentenceId}`}
+            className={"sentence " + (isViewer ? "hoversentence " : "")}
+            data-sentenceid={sentenceId}
+          >
+            {domToReact(domNode.children as DOMNode[], options)}
+          </span>
+        );
+      } else {
+        return false;
+      }
+    },
+    [projectId, isViewer],
+  );
+
   // Order matters. Instructions are processed in
   // the order they're defined
   const processingInstructions: HTMLReactParserOptions = useMemo(() => {
     if (!annotationsPerToken || !tokenData || !annotationMap) {
       const options: HTMLReactParserOptions = {
         replace(domNode) {
-          if (domNode instanceof Element) {
-            // sentences & tokens
-            if (domNode.name === "sent" || domNode.name === "t") {
+          if (domNode instanceof Element && domNode.attribs) {
+            const basicResult = basicProcessingInstructions(options)(domNode);
+            if (basicResult) {
+              return basicResult;
+              // tokens
+            } else if (domNode.name === "t") {
               return <span>{domToReact(domNode.children as DOMNode[], options)}</span>;
+              // fallback case
             } else {
               return domToReact(domNode.children as DOMNode[], options);
             }
@@ -98,65 +152,28 @@ function DocumentRenderer({
       const options: HTMLReactParserOptions = {
         replace(domNode) {
           if (domNode instanceof Element && domNode.attribs) {
-            // images
-            if (domNode.name === "img" && domNode.attribs.src) {
-              const filename = domNode.attribs.src;
-              return <SdocImage key={`image-link-${filename}`} filename={filename} projectId={projectId} />;
-            }
-            //  videos
-            else if (domNode.name === "video" && domNode.attribs.src) {
-              const filename = domNode.attribs.src;
-              return (
-                <SdocVideoLink
-                  key={`video-link-${filename}`}
-                  filename={filename}
-                  toPrefix={"../annotation/"}
-                  projectId={projectId}
-                />
-              );
-            }
-            // audios
-            else if (domNode.name === "audio" && domNode.attribs.src) {
-              const filename = domNode.attribs.src;
-              return (
-                <SdocAudioLink
-                  key={`audio-link-${filename}`}
-                  filename={filename}
-                  toPrefix={"../annotation/"}
-                  projectId={projectId}
-                />
-              );
-            }
-            // sentences
-            else if (domNode.name === "sent" && domNode.attribs.id) {
-              const sentenceId = parseInt(domNode.attribs.id);
-              return (
-                <span
-                  key={`sentence-${sentenceId}`}
-                  className={"sentence " + (isViewer ? "hoversentence " : "")}
-                  data-sentenceid={sentenceId}
-                >
-                  {domToReact(domNode.children as DOMNode[], options)}
-                </span>
-              );
-            }
-            // tokens
-            else if (domNode.name === "t" && domNode.attribs.id) {
+            // only basic processing
+            const basicResult = basicProcessingInstructions(options)(domNode);
+            if (basicResult) {
+              return basicResult;
+              // tokens
+            } else if (domNode.name === "t" && domNode.attribs.id) {
               const tokenId = parseInt(domNode.attribs.id);
               const token = tokenData[tokenId];
               const spanAnnotations = (annotationsPerToken.get(tokenId) || []).map(
                 (annotationId) => annotationMap.get(annotationId)!,
               );
               return <Token key={`token-${tokenId}`} token={token} spanAnnotations={spanAnnotations} />;
+              // fallback case
             } else {
-              return domToReact(domNode.children as DOMNode[], processingInstructions);
+              return domToReact(domNode.children as DOMNode[], options);
             }
           }
         },
       };
       return options;
     }
-  }, [annotationMap, annotationsPerToken, isViewer, projectId, tokenData]);
+  }, [annotationMap, annotationsPerToken, tokenData, basicProcessingInstructions]);
 
   return (
     <Box ref={listRef} {...props}>
