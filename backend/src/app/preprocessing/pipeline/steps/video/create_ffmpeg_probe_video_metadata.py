2c07f0d97c448b1ffd56fb46d7e016e7177aa8c4
Tim Fischer <tim.fischer@uni-hamburg.de>
2024-10-18 12:55:27 +0000

    fixed bugs

--- backend/src/app/preprocessing/pipeline/steps/video/create_ffmpeg_probe_video_metadata.py
+++ backend/src/app/preprocessing/pipeline/steps/video/create_ffmpeg_probe_video_metadata.py
@@ -9,11 +9,11 @@ def create_ffmpeg_probe_video_metadata(cargo: PipelineCargo) -> PipelineCargo:
     ffmpeg_probe = ffmpeg.probe(ppvd.filepath)
 
     for k, v in ffmpeg_probe["format"].items():
-        ppvd.metadata[k] = v
+        ppvd.metadata[k] = str(v)
 
     vidx = 0 if ffmpeg_probe["streams"][0]["codec_type"] == "video" else 1
     for md in ["width", "height", "display_aspect_ratio"]:
         if md in ffmpeg_probe["streams"][vidx]:
-            ppvd.metadata[md] = ffmpeg_probe["streams"][vidx][md]
+            ppvd.metadata[md] = str(ffmpeg_probe["streams"][vidx][md])
 
     return cargo
