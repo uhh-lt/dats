e3a665a500d688f9a2dc0caa5a5f6a6106497aac
Ahmad Khalidi <Auren@gmx.net>
2024-10-22 09:29:59 +0000

    Fixing bbox import

--- backend/src/app/preprocessing/pipeline/steps/image/run_object_detection.py
+++ backend/src/app/preprocessing/pipeline/steps/image/run_object_detection.py
@@ -1,3 +1,5 @@
+from loguru import logger
+
 from app.core.data.crud.user import SYSTEM_USER_ID
 from app.preprocessing.pipeline.model.image.autobbox import AutoBBox
 from app.preprocessing.pipeline.model.image.preproimagedoc import PreProImageDoc
@@ -13,6 +15,7 @@ def run_object_detection(cargo: PipelineCargo) -> PipelineCargo:
 
     input = DETRFilePathInput(image_fp=str(ppid.filepath))
     result = rms.detr_object_detection(input)
+    logger.info(f"Bounding boxes already found are: {ppid.bboxes}")
     for box in result.bboxes:
         code_name = box.label
         bbox = AutoBBox(
@@ -23,8 +26,10 @@ def run_object_detection(cargo: PipelineCargo) -> PipelineCargo:
             y_max=box.y_max,
             user_id=SYSTEM_USER_ID,
         )
+        logger.info(f" Found bbox by detr with {bbox.model_dump_json(indent=4)}")
         if code_name not in ppid.bboxes:
             ppid.bboxes[code_name] = set()
         ppid.bboxes[code_name].add(bbox)
+    logger.info(f"Resulted in {ppid.bboxes}")
 
     return cargo
