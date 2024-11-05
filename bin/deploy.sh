4a8074fb10e49926657391080c8ca822fa9a77e2
Tim Fischer <tim.fischer@uni-hamburg.de>
2024-11-05 14:13:22 +0000

    updated backend_checks

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -9,6 +9,7 @@ git pull
 # update .env file
 cp .env.example .env
 sed -i 's/COMPOSE_PROJECT_NAME=demo/COMPOSE_PROJECT_NAME=prod-dats/' .env
+sed -i 's/CELERY_DEBUG_PORT=45678/CELERY_DEBUG_PORT=10178/' .env
 sed -i 's/131/101/g' .env
 
 # pull & start docker containers
