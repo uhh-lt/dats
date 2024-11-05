58107e8d0b4ba3dbd40b1905bc93663db75caea8
Tim Fischer <tim.fischer@uni-hamburg.de>
2024-11-05 13:10:29 +0000

    removed vit_gpt2

--- backend/src/app/preprocessing/ray_model_service.py
+++ backend/src/app/preprocessing/ray_model_service.py
@@ -19,10 +19,6 @@ from app.preprocessing.ray_model_worker.dto.detr import (
     DETRObjectDetectionOutput,
 )
 from app.preprocessing.ray_model_worker.dto.spacy import SpacyInput, SpacyPipelineOutput
-from app.preprocessing.ray_model_worker.dto.vit_gpt2 import (
-    ViTGPT2FilePathInput,
-    ViTGPT2Output,
-)
 from app.preprocessing.ray_model_worker.dto.whisper import (
     WhisperFilePathInput,
     WhisperTranscriptionOutput,
@@ -104,13 +100,6 @@ class RayModelService(metaclass=SingletonMeta):
     ) -> DETRObjectDetectionOutput:
         response = self._make_post_request("/detr/object_detection", input.model_dump())
         return DETRObjectDetectionOutput.model_validate(response.json())
-
-    def vit_gpt2_image_captioning(self, input: ViTGPT2FilePathInput) -> ViTGPT2Output:
-        raise NotImplementedError
-        response = self._make_post_request(
-            "/vit_gpt2/image_captioning", input.model_dump()
-        )
-        return ViTGPT2Output.model_validate(response.json())
 
     def blip2_image_captioning(self, input: Blip2FilePathInput) -> Blip2Output:
         response = self._make_post_request(
