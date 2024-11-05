b9883fa2c6b2f7d5bb836b835d3c944113db7a21
Tim Fischer <tim.fischer@uni-hamburg.de>
2024-11-05 12:07:24 +0000

    test 10

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 echo "Connection successful! :)" > ~/test-output.txt
+
+user=$DEMO_USER
+password=$DEMO_PASSWORD
+
+echo "User: $user" >> ~/test-output.txt
+echo "Password: $password" >> ~/test-output.txt
