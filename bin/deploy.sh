d06866ec36332d2ce01dc3259ea85e2a2e8cfac6
Tim Fischer <tim.fischer@uni-hamburg.de>
2024-11-11 22:01:21 +0000

    update deploy.sh script to set JWT_SECRET

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -11,7 +11,8 @@ cp .env.example .env
 sed -i 's/COMPOSE_PROJECT_NAME=demo/COMPOSE_PROJECT_NAME=prod-dats/' .env
 sed -i 's/CELERY_DEBUG_PORT=45678/CELERY_DEBUG_PORT=10178/' .env
 sed -i 's/131/101/g' .env
+sed -i "s/JWT_SECRET=/JWT_SECRET=$(pwgen 32 1)/" .env
 
 # pull & start docker containers
 docker compose pull
-docker compose up -d
+docker compose up --wait
