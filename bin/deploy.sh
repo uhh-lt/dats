12d4e6cc489af7ffa0cf4d00948973248e04357e
Tim Fischer <tim.fischer@uni-hamburg.de>
2024-11-13 16:16:37 +0000

    renamed docker-compose to compose

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -9,7 +9,6 @@ git pull
 # update .env file
 cp .env.example .env
 sed -i 's/COMPOSE_PROJECT_NAME=demo/COMPOSE_PROJECT_NAME=prod-dats/' .env
-sed -i 's/CELERY_DEBUG_PORT=45678/CELERY_DEBUG_PORT=10178/' .env
 sed -i 's/131/101/g' .env
 sed -i "s/JWT_SECRET=/JWT_SECRET=$(pwgen 32 1)/" .env
 sed -i "s/UID=121/UID=$(id -u)/" .env
@@ -17,4 +16,4 @@ sed -i "s/GID=126/GID=$(id -g)/" .env
 
 # pull & start docker containers
 docker compose pull
-docker compose up --wait
+docker compose up -f compose.yml -f compose.production.yml --wait
