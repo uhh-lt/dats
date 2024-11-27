cd41750c4b6f86975c411dae5a25759330457399
Tim Fischer <tim.fischer@uni-hamburg.de>
2024-11-27 16:52:20 +0000

    updated weaviate backup script

--- bin/backup-weaviate.sh
+++ bin/backup-weaviate.sh
@@ -14,7 +14,22 @@ fi
 cd docker || exit
 
 BACKUP_NAME="backup_$(date +%Y_%m_%d_%H_%M)"
+
+# 1) Start backup process
 docker compose -f compose.yml -f compose.production.yml exec -i weaviate sh -c "wget --header='Content-Type: application/json' --post-data='{
      \"id\": \"$BACKUP_NAME\"
     }' -O- http://localhost:8080/v1/backups/filesystem"
-echo "Weaviate data backup created successfully with ID: $BACKUP_NAME."
+echo "Started weaviate data backup with ID: $BACKUP_NAME."
+
+sleep 10
+
+# 2) Check status of the backup operation in a loop
+while true; do
+    STATUS=$(docker compose -f compose.yml -f compose.production.yml exec -i weaviate sh -c "wget -qO- http://localhost:8080/v1/backups/filesystem/$BACKUP_NAME | grep -o '\"status\":\"[^\"]*\"' | awk -F':' '{print $2}' | tr -d '\"'")
+    echo "$STATUS"
+    if [ "$STATUS" = "status:SUCCESS" ] || [ "$STATUS" = "status:FAILED" ]; then
+        break
+    fi
+    sleep 10
+done
+echo "Weaviate data backup $BACKUP_NAME completed with $STATUS"
