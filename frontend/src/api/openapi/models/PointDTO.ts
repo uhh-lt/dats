e9efdad4ea8a41ed2345d9da13e2c104239dd557
Noah Scheld <9scheld@ltdwise.server.uni-hamburg.de>
2024-12-01 19:05:58 +0000

    start of better typing

---
+++ frontend/src/api/openapi/models/PointDTO.ts
@@ -0,0 +1,14 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type PointDTO = {
+  /**
+   * x-coord
+   */
+  x: number;
+  /**
+   * y-coord
+   */
+  y: number;
+};
