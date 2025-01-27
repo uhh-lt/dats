93aeca3ec85acfa8260bcb0f53534b4eef4a4000
Tim Fischer <tim.fischer@uni-hamburg.de>
2025-01-27 14:36:08 +0000

    used new status and status message to build better background task view

--- frontend/src/components/ProjectSettings/backgroundtasks/LLMJobListItem.tsx
+++ frontend/src/components/ProjectSettings/backgroundtasks/LLMJobListItem.tsx
@@ -1,18 +1,24 @@
 import { TabContext, TabList, TabPanel } from "@mui/lab";
-import { Box, Button, Stack, Tab, TextField } from "@mui/material";
-import { useState } from "react";
+import { Box, Button, List, ListItemButton, ListItemIcon, ListItemText, Stack, Tab, TextField } from "@mui/material";
+import { useVirtualizer } from "@tanstack/react-virtual";
+import { useRef, useState } from "react";
+import { Link as RouterLink } from "react-router-dom";
 import LLMHooks from "../../../api/LLMHooks.ts";
 import { ApproachType } from "../../../api/openapi/models/ApproachType.ts";
 import { BackgroundJobStatus } from "../../../api/openapi/models/BackgroundJobStatus.ts";
+import { DocType } from "../../../api/openapi/models/DocType.ts";
 import { FewShotParams } from "../../../api/openapi/models/FewShotParams.ts";
 import { LLMJobRead } from "../../../api/openapi/models/LLMJobRead.ts";
+import { LLMJobResult } from "../../../api/openapi/models/LLMJobResult.ts";
 import { LLMPromptTemplates } from "../../../api/openapi/models/LLMPromptTemplates.ts";
 import { ModelTrainingParams } from "../../../api/openapi/models/ModelTrainingParams.ts";
 import { ZeroShotParams } from "../../../api/openapi/models/ZeroShotParams.ts";
 import { useAppDispatch } from "../../../plugins/ReduxHooks.ts";
 import { dateToLocaleString } from "../../../utils/DateUtils.ts";
+import { docTypeToIcon } from "../../../utils/docTypeToIcon.tsx";
 import { CRUDDialogActions } from "../../dialogSlice.ts";
 import BackgroundJobListItem from "./BackgroundJobListItem.tsx";
+import { statusToTypographyColor } from "./StatusToTypographyColor.ts";
 
 interface LLMJobListItemProps {
   initialLLMJob: LLMJobRead;
@@ -45,6 +51,13 @@ function LLMJobListItem({ initialLLMJob }: LLMJobListItemProps) {
     dispatch(CRUDDialogActions.llmDialogOpenFromBackgroundTask(llmJob.data));
   };
 
+  // tabs
+  // tab state
+  const [tab, setTab] = useState("Status");
+  const handleChangeTab = (_: React.SyntheticEvent, newValue: string) => {
+    setTab(newValue);
+  };
+
   if (llmJob.isSuccess) {
     return (
       <BackgroundJobListItem
@@ -54,16 +67,34 @@ function LLMJobListItem({ initialLLMJob }: LLMJobListItemProps) {
         subTitle={subTitle}
       >
         <Stack sx={{ px: 9 }}>
-          {llmJob.data.status === BackgroundJobStatus.FINISHED ? (
-            <Button variant="contained" sx={{ width: "fit-content" }} onClick={handleViewResults}>
-              View {llmJob.data.parameters.llm_job_type} results
-            </Button>
-          ) : llmJob.data.status === BackgroundJobStatus.RUNNING ? (
-            <Button variant="contained" sx={{ width: "fit-content" }} onClick={handleViewResults}>
-              View {llmJob.data.parameters.llm_job_type} progress
-            </Button>
-          ) : null}
-          <LLMJobDetailViewer llmJob={llmJob.data} />
+          <TabContext value={tab}>
+            <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+              <TabList onChange={handleChangeTab}>
+                <Tab key={"Status"} label={"Status"} value={"Status"} />
+                <Tab key={"Inputs"} label={"Inputs"} value={"Inputs"} />
+                <div
+                  style={{
+                    flexGrow: 1,
+                  }}
+                />
+                {llmJob.data.status === BackgroundJobStatus.FINISHED ? (
+                  <Button variant="contained" onClick={handleViewResults} sx={{ m: 0.5 }}>
+                    View {llmJob.data.parameters.llm_job_type} results
+                  </Button>
+                ) : llmJob.data.status === BackgroundJobStatus.RUNNING ? (
+                  <Button variant="contained" onClick={handleViewResults} sx={{ m: 0.5 }}>
+                    View {llmJob.data.parameters.llm_job_type} progress
+                  </Button>
+                ) : null}
+              </TabList>
+            </Box>
+            <TabPanel key={"Inputs"} value={"Inputs"} sx={{ p: 0 }}>
+              <InputViewer llmJob={llmJob.data} />
+            </TabPanel>
+            <TabPanel key={"Status"} value={"Status"} sx={{ p: 0 }}>
+              {llmJob.data.result ? <StatusViewer llmJobResult={llmJob.data.result} /> : "No results available"}
+            </TabPanel>
+          </TabContext>
         </Stack>
       </BackgroundJobListItem>
     );
@@ -72,7 +103,7 @@ function LLMJobListItem({ initialLLMJob }: LLMJobListItemProps) {
   }
 }
 
-function LLMJobDetailViewer({ llmJob }: { llmJob: LLMJobRead }) {
+function InputViewer({ llmJob }: { llmJob: LLMJobRead }) {
   switch (llmJob.parameters.llm_approach_type) {
     case ApproachType.LLM_ZERO_SHOT:
       return <PromptViewer prompts={(llmJob.parameters.specific_approach_parameters as ZeroShotParams).prompts} />;
@@ -99,12 +130,12 @@ function PromptViewer({ prompts }: { prompts: LLMPromptTemplates[] }) {
       <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
         <TabList onChange={handleChangeTab}>
           {prompts.map((prompt) => (
-            <Tab key={prompt.language} label={prompt.language} value={prompt.language} />
+            <Tab key={prompt.language} label={prompt.language + " Prompts"} value={prompt.language} />
           ))}
         </TabList>
       </Box>
       {prompts.map((prompt) => (
-        <TabPanel key={prompt.language} value={prompt.language} sx={{ px: 0 }}>
+        <TabPanel key={prompt.language} value={prompt.language} sx={{ pl: 0, pr: 1, height: 352, overflowY: "auto" }}>
           <Stack gap={3}>
             <TextField
               fullWidth
@@ -136,6 +167,70 @@ function TrainingParameterViewer({ parameters }: { parameters: ModelTrainingPara
         <TextField key={key} fullWidth label={key} value={value} type="number" inputProps={{ readOnly: true }} />
       ))}
     </Stack>
+  );
+}
+
+function StatusViewer({ llmJobResult }: { llmJobResult: LLMJobResult }) {
+  const parentRef = useRef<HTMLDivElement>(null);
+  const virtualizer = useVirtualizer({
+    count: llmJobResult.specific_task_result.results.length,
+    getScrollElement: () => parentRef.current,
+    estimateSize: () => 45,
+  });
+
+  const items = virtualizer.getVirtualItems();
+  return (
+    <Box ref={parentRef} sx={{ overflowY: "auto", height: 400 }}>
+      <div
+        style={{
+          height: virtualizer.getTotalSize(),
+          width: "100%",
+          position: "relative",
+        }}
+      >
+        <List
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            transform: `translateY(${items[0]?.start ?? 0}px)`,
+          }}
+        >
+          {items.map((item) => (
+            <div key={item.key} data-index={item.index} ref={virtualizer.measureElement}>
+              <LLMResultStatusItem result={llmJobResult.specific_task_result.results[item.index]} />
+            </div>
+          ))}
+        </List>
+      </div>
+    </Box>
+  );
+}
+
+interface ResultStatusItem {
+  /**
+   * Status of the Result
+   */
+  status: BackgroundJobStatus;
+  /**
+   * Status message of the result
+   */
+  status_message: string;
+  /**
+   * ID of the source document
+   */
+  sdoc_id: number;
+}
+
+function LLMResultStatusItem({ result }: { result: ResultStatusItem }) {
+  return (
+    <ListItemButton component={RouterLink} to={`./annotation/${result.sdoc_id}`}>
+      <ListItemIcon sx={{ color: `${statusToTypographyColor[result.status]}` }}>
+        {docTypeToIcon[DocType.TEXT]}
+      </ListItemIcon>
+      <ListItemText primary={`Document with ID ${result.sdoc_id} - ${result.status_message}`} />
+    </ListItemButton>
   );
 }
 
