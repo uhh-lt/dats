c94e58e7aae382a51fce0a235c838ac5ffdca3f6
Fynn Petersen-Frey <fynn.petersen-frey@uni-hamburg.de>
2025-02-25 15:45:24 +0000

    more cleanup

---
+++ backend/src/app/core/data/orm/source_document_job_status.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+from enum import IntEnum
+from typing import TYPE_CHECKING, Optional
+
+from sqlalchemy import DateTime, ForeignKey, Integer
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.core.data.orm.orm_base import ORMBase
+
+if TYPE_CHECKING:
+    from app.core.data.orm.source_document import SourceDocumentORM
+
+
+class JobType(IntEnum):
+    QUOTATION_ATTRIBUTION = 100
+
+
+class JobStatus(IntEnum):
+    UNQUEUED = 0  # (not queued/planned)
+    WAITING = 1  # (not started yet)
+    RUNNING = 2  # (currently in progress)
+    FINISHED = 3  # (successfully finished)
+    ERROR = 4  # (failed to finish)
+    ABORTED = 5  # (aborted by user)
+
+
+class SourceDocumentJobStatusORM(ORMBase):
+    id: Mapped[int] = mapped_column(
+        Integer,
+        ForeignKey("sourcedocument.id", ondelete="CASCADE"),
+        primary_key=True,
+        nullable=False,
+        index=True,
+    )
+    source_document: Mapped["SourceDocumentORM"] = relationship("SourceDocumentORM")
+    type: Mapped[JobType] = mapped_column(
+        Integer, nullable=False, index=False, primary_key=True
+    )
+    status: Mapped[JobStatus] = mapped_column(Integer, nullable=False, index=False)
+    timestamp: Mapped[Optional[datetime]] = mapped_column(DateTime)
--- backend/src/app/core/db/import_all_orms.py
+++ backend/src/app/core/db/import_all_orms.py
@@ -22,7 +22,7 @@ from app.core.data.orm.refresh_token import RefreshTokenORM
 from app.core.data.orm.sentence_annotation import SentenceAnnotationORM
 from app.core.data.orm.source_document import SourceDocumentORM
 from app.core.data.orm.source_document_data import SourceDocumentDataORM
-from app.core.data.orm.source_document_job import SourceDocumentJobORM
+from app.core.data.orm.source_document_job_status import SourceDocumentJobStatusORM
 from app.core.data.orm.source_document_link import SourceDocumentLinkORM
 from app.core.data.orm.source_document_metadata import SourceDocumentMetadataORM
 from app.core.data.orm.span_annotation import SpanAnnotationORM
