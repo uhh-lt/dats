8981339d778a6aa772ddb5e0fd40d963053bfeb0
floschne <florian.schneider-1@uni-hamburg.de>
2025-03-06 13:55:41 +0000

    using ollama vlm to generate the image caption

--- backend/src/app/preprocessing/pipeline/steps/image/process/generate_image_caption.py
+++ backend/src/app/preprocessing/pipeline/steps/image/process/generate_image_caption.py
@@ -1,19 +1,33 @@
+import base64
+import io
+from pathlib import Path
+
+from PIL import Image
+
+from app.core.data.llm.ollama_service import OllamaService
+from app.core.data.llm.prompts.image_captioning_prompt import IC_USER_PROMPT
 from app.preprocessing.pipeline.model.image.preproimagedoc import PreProImageDoc
 from app.preprocessing.pipeline.model.pipeline_cargo import PipelineCargo
-from app.preprocessing.ray_model_service import RayModelService
-from app.preprocessing.ray_model_worker.dto.blip2 import Blip2FilePathInput
 
-rms = RayModelService()
+ollama = OllamaService()
+
+
+def __load_image(img_path: Path | str) -> Image.Image:
+    return Image.open(img_path).convert("RGB")
+
+
+def __image_to_base64(image: Image.Image) -> str:
+    buffered = io.BytesIO()
+    image.save(buffered, format="JPEG")
+    return base64.b64encode(buffered.getvalue()).decode("utf-8")
 
 
 def generate_image_caption(cargo: PipelineCargo) -> PipelineCargo:
     ppid: PreProImageDoc = cargo.data["ppid"]
     if "caption" not in ppid.metadata:
-        input = Blip2FilePathInput(
-            image_fp=str(ppid.filename), project_id=ppid.project_id
-        )
-        result = rms.blip2_image_captioning(input)
-
-        ppid.metadata["caption"] = result.caption
+        image = __load_image(ppid.filepath)
+        image_b64 = __image_to_base64(image)
+        caption, _ = ollama.vlm_chat(user_prompt=IC_USER_PROMPT, b64_images=[image_b64])
+        ppid.metadata["caption"] = caption
 
     return cargo
