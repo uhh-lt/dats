d19f4268f98c5251faa39e74f2f046b012c0c82a
Tim Fischer <tim.fischer@uni-hamburg.de>
2025-03-07 16:39:06 +0000

    added folders for production and backup

--- bin/setup-folders.sh
+++ bin/setup-folders.sh
@@ -5,5 +5,16 @@ mkdir -p ../docker/models_cache
 mkdir -p ../docker/spacy_models
 mkdir -p ../docker/numba_cache
 mkdir -p ../docker/ollama_cache
+
+# for production
+mkdir -p ../docker/elasticsearch_data
+mkdir -p ../docker/pg_data
+mkdir -p ../docker/rabbitmq_data
+mkdir -p ../docker/redis_data
+mkdir -p ../docker/weaviate_data
+
+# for backups
 mkdir -p ../backups/weaviate
 mkdir -p ../backups/elasticsearch
+mkdir -p ../backups/postgres
+mkdir -p ../backups/repo
