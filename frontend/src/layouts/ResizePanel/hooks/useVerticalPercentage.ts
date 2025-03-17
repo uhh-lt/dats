6fc3194992ec927bf2e242b982ec229fe416a272
Tim Fischer <tim.fischer@uni-hamburg.de>
2025-03-17 13:40:15 +0000

    added use callback

--- frontend/src/layouts/ResizePanel/hooks/useVerticalPercentage.ts
+++ frontend/src/layouts/ResizePanel/hooks/useVerticalPercentage.ts
@@ -1,13 +1,16 @@
+import { useCallback } from "react";
 import { useAppDispatch, useAppSelector } from "../../../plugins/ReduxHooks";
 import { LayoutActions } from "../../layoutSlice";
 
 export function useVerticalPercentage(componentName: string, defaultPercentage: number = 50) {
   const dispatch = useAppDispatch();
   const percentage = useAppSelector((state) => state.layout.verticalPercentages[componentName] ?? defaultPercentage);
-
-  const handleResize = (newPercentage: number) => {
-    dispatch(LayoutActions.setVerticalPercentage({ componentName, percentage: newPercentage }));
-  };
+  const handleResize = useCallback(
+    (newPercentage: number) => {
+      dispatch(LayoutActions.setVerticalPercentage({ componentName, percentage: newPercentage }));
+    },
+    [componentName, dispatch],
+  );
 
   return { percentage, handleResize };
 }
