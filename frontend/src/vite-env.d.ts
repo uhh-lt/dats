49d5f85d66e154a045b7dbe1cc61fa2e630a01b9
Tim Fischer <tim.fischer@uni-hamburg.de>
2025-03-25 16:23:58 +0000

    added open id connect supppot

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly VITE_APP_SERVER: string;
   readonly VITE_APP_CONTENT: string;
   readonly VITE_APP_TITLE: string;
+  readonly VITE_APP_SSO_PROVIDER: string;
 }
 
 interface ImportMeta {
