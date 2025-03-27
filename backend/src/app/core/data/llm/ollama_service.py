17b754235023d237ef6d493fcec03d6d81a9ea5a
Tim Fischer <tim.fischer@uni-hamburg.de>
2025-03-27 09:45:05 +0000

    updated ollama service, settings now in config

--- backend/src/app/core/data/llm/ollama_service.py
+++ backend/src/app/core/data/llm/ollama_service.py
@@ -1,5 +1,5 @@
 import time
-from typing import Type, TypeVar
+from typing import Dict, List, Optional, Tuple, Type, TypedDict, TypeVar
 from uuid import uuid4
 
 from loguru import logger
@@ -10,6 +10,16 @@ from app.util.singleton_meta import SingletonMeta
 from config import conf
 
 T = TypeVar("T", bound=BaseModel)
+
+
+class ModelDict(TypedDict):
+    llm: str
+    vlm: str
+
+
+class ModelParams(TypedDict):
+    llm: Dict
+    vlm: Dict
 
 
 class OllamaService(metaclass=SingletonMeta):
@@ -23,72 +33,50 @@ class OllamaService(metaclass=SingletonMeta):
                 raise Exception(
                     f"Cant connect to Ollama on {conf.ollama.host}:{conf.ollama.port}"
                 )
-            cls.__model = dict()
+            cls.__model: ModelDict = {
+                "llm": "",
+                "vlm": "",
+            }
+            cls.__default_kwargs: ModelParams = {
+                "llm": {},
+                "vlm": {},
+            }
             cls.__client = ollamac
 
-            # ensure that the configured models are available
+            # check if the configured models are available
             available_models = [x.model for x in ollamac.list()["models"]]
             logger.info(f"Available models: {available_models}")
             for model_type in ["llm", "vlm"]:
-                model_name = conf.ollama.model.get(model_type, None)
-                if model_name is None:
-                    raise RuntimeError(
-                        f"{model_type.capitalize()} Model is not configured. Please set the model in the config file."
-                    )
+                model_name = conf.ollama[model_type].model
                 if model_name not in available_models:
-                    logger.info(f"Model {model_name} is not available. Creating it...")
-                    context_size = conf.ollama.context_size.get(model_type, None)
-                    if context_size is not None:
-                        logger.info(
-                            f"Creating custom {model_type.capitalize()} model {model_name}..."
-                        )
-                        ollamac.create(
-                            "custom-" + model_name,
-                            from_=model_name,
-                            parameters={
-                                "num_ctx": context_size,
-                            },
-                        )
-                    else:
-                        logger.info(
-                            f"Creating {model_type.capitalize()} model {model_name}..."
-                        )
-                        ollamac.create(
-                            model_name,
-                            from_=model_name,
-                        )
-
                     logger.info(
-                        f"{model_type.capitalize()} Model {model_name} has been created successfully."
+                        f"Model {model_name} is not available. Downloading it..."
+                    )
+                    ollamac.pull(model_name)
+                    logger.info(
+                        f"{model_type.capitalize()} Model {model_name} has been downloaded successfully."
                     )
 
+            # ensure that the models are available
             available_models = [x.model for x in ollamac.list()["models"]]
+            logger.info(f"Available models: {available_models}")
             for model_type in ["llm", "vlm"]:
-                model_name = conf.ollama.model.get(model_type)
-                # since the available model name can start with "custom-" we cannot simply use `model_name in available_models`...
-                is_available = False
-                for available_model in available_models:
-                    if model_name in available_model:
-                        is_available = True
-                        break
-                if not is_available:
+                model_name = conf.ollama[model_type].model
+                if model_name not in available_models:
                     raise RuntimeError(
                         f"{model_type.capitalize()} Model {model_name} is not available. Available models are: {available_models}"
                     )
+
                 cls.__model[model_type] = model_name
+                cls.__default_kwargs[model_type] = conf.ollama[
+                    model_type
+                ].default_params
+                logger.info(
+                    f"Default parameters for {model_type} {model_name}: {cls.__default_kwargs[model_type]}"
+                )
 
-            cls.__default_vlm_kwargs = {
-                "temperature": 0.0,
-                "seed": 1337,
-                "num_predict": 8192,
-                "num_ctx": 16384,  # context window (default of Granite 3.2 Vision)
-                "top_p": 0.9,
-                "top_k": 40,
-                "repetition_penalty": 1.1,
-            }
-
-            cls.__vlm_chat_sessions: dict[str, list[dict]] = dict()
-            cls.__vlm_chat_session_timestamps: dict[str, float] = dict()
+            cls.__vlm_chat_sessions: Dict[str, List[Dict]] = dict()
+            cls.__vlm_chat_session_timestamps: Dict[str, float] = dict()
             cls.__max_vlm_chat_sessions = 50
             cls.__max_vlm_chat_session_age = 7 * 24 * 60 * 60  # 7 days
 
@@ -102,8 +90,15 @@ class OllamaService(metaclass=SingletonMeta):
         return super(OllamaService, cls).__new__(cls)
 
     def llm_chat(
-        self, system_prompt: str, user_prompt: str, response_model: Type[T]
+        self,
+        system_prompt: str,
+        user_prompt: str,
+        response_model: Type[T],
+        gen_kwargs: Optional[Dict[str, str]] = None,
     ) -> T:
+        if gen_kwargs is None:
+            gen_kwargs = self.__default_kwargs["llm"]
+
         response = self.__client.chat(
             model=self.__model["llm"],
             messages=[
@@ -116,6 +111,7 @@ class OllamaService(metaclass=SingletonMeta):
                     "content": user_prompt.strip(),
                 },
             ],
+            options=gen_kwargs,
             format=response_model.model_json_schema(),
         )
         if response.message.content is None:
@@ -140,7 +136,7 @@ class OllamaService(metaclass=SingletonMeta):
 
         return session_id
 
-    def _get_vlm_chat_session(self, session_id: str) -> list[dict]:
+    def _get_vlm_chat_session(self, session_id: str) -> List[Dict]:
         history = self.__vlm_chat_sessions.get(session_id, [])
         if len(history) == 0:
             logger.warning(f"Session ID {session_id} is does not exist.")
@@ -149,14 +145,14 @@ class OllamaService(metaclass=SingletonMeta):
     def vlm_chat(
         self,
         user_prompt: str,
-        b64_images: list[str] | None = None,
-        system_prompt: str | None = None,
-        response_model: Type[T] | None = None,
-        gen_kwargs: dict[str, str] | None = None,
-        session_id: str | None = None,
-    ) -> tuple[str | T, str]:
+        b64_images: Optional[List[str]] = None,
+        system_prompt: Optional[str] = None,
+        response_model: Optional[Type[T]] = None,
+        gen_kwargs: Optional[Dict[str, str]] = None,
+        session_id: Optional[str] = None,
+    ) -> Tuple[str | T, str]:
         if gen_kwargs is None:
-            gen_kwargs = self.__default_vlm_kwargs
+            gen_kwargs = self.__default_kwargs["vlm"]
 
         if session_id is None:
             session_id = self._start_vlm_chat_session()
@@ -179,7 +175,7 @@ class OllamaService(metaclass=SingletonMeta):
                 }
             )
         # build user message
-        user_message: dict = {
+        user_message: Dict = {
             "role": "user",
             "content": user_prompt.strip(),
         }
