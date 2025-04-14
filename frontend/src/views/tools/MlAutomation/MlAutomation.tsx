0e29596a64a15db423c54c834d60df6cd8ddd43d
Noah Scheld <9scheld@ltdwise.server.uni-hamburg.de>
2025-04-14 12:18:52 +0000

    changed explanation text

--- frontend/src/views/tools/MlAutomation/MlAutomation.tsx
+++ frontend/src/views/tools/MlAutomation/MlAutomation.tsx
@@ -4,19 +4,29 @@ import RestartAltIcon from "@mui/icons-material/RestartAlt";
 import SwitchAccessShortcutAddIcon from "@mui/icons-material/SwitchAccessShortcutAdd";
 import {
   Avatar,
+  Button,
   Card,
   CardContent,
   CardHeader,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
   IconButton,
   List,
   ListItem,
   ListItemAvatar,
   ListItemIcon,
   ListItemText,
+  TextField,
   Tooltip,
 } from "@mui/material";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import MLHooks from "../../../api/MLHooks.ts";
+import { BackgroundJobStatus } from "../../../api/openapi/models/BackgroundJobStatus.ts";
+import { MLJobRead } from "../../../api/openapi/models/MLJobRead.ts";
 import { MLJobType } from "../../../api/openapi/models/MLJobType.ts";
 import ConfirmationAPI from "../../../components/ConfirmationDialog/ConfirmationAPI.ts";
 import ContentContainerLayout from "../../../layouts/ContentLayouts/ContentContainerLayout.tsx";
@@ -36,6 +46,7 @@ function MlAutomation() {
 
   // actions
   const startMlJob = MLHooks.useStartMLJob();
+  const pollMlJob = MLHooks.usePollMLJob(currentJobId, undefined);
 
   // quotation detection jobs
   const handleStartNewQuotationDetection = () => {
@@ -97,44 +108,14 @@ function MlAutomation() {
     );
   };
 
-  const handleTopicModelingStarted = (data: MLJobRead) => {
-    setCurrentJobId(data.id);
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
   };
 
-  const handleStartTopicModeling = (recompute: boolean = false) => {
-    startMlJob.mutate(
-      {
-        requestBody: {
-          ml_job_type: MLJobType.TOPIC_MODELING,
-          project_id: projectId,
-          specific_ml_job_parameters: {
-            recompute: recompute,
-            ml_job_type: MLJobType.TOPIC_MODELING,
-            nr_topics: currentNrTopics,
-            min_topic_size: currentMinTopicSize,
-            top_n_words: currentTopNWords,
-          },
-        },
-      },
-      { onSuccess: handleTopicModelingStarted },
-    );
-  };
-
-  const handleTextInput = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>, id: string) => {
-    const value = event.target.value;
-
-    if (!isNaN(Number(value)) || value === "") {
-      switch (id) {
-        case TextInputId.NrTopics:
-          return setNrTopics(Number(value));
-        case TextInputId.MinTopicSize:
-          return setMinTopicSize(Number(value));
-        case TextInputId.TopNWords:
-          return setTopNWords(Number(value));
-        default:
-          return console.log("TextFieldId not Found!");
-      }
-    }
+  const handleClose = () => {
+    setOpen(false);
   };
 
   const handleTextInput = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>, id: string) => {
@@ -293,6 +274,81 @@ function MlAutomation() {
                     <RestartAltIcon />
                   </IconButton>
                 </Tooltip>
+              </ListItemIcon>
+            </ListItem>
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar>
+                  <FormatQuoteIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText
+                primary="Topic Modeling with BERTopic"
+                secondary={"Generates topics based on uploaded text data"}
+              />
+              <TextField
+                id="nr_topics"
+                label="Amount of Topics"
+                type="number"
+                onChange={(event) => handleTextInput(event, "nr_topics")}
+              ></TextField>
+              <TextField
+                id="min_topic_size"
+                label="Topic size"
+                type="number"
+                onChange={(event) => handleTextInput(event, "min_topic_size")}
+              ></TextField>
+              <TextField
+                id="top_n_words"
+                label="Amount of Words"
+                type="number"
+                onChange={(event) => handleTextInput(event, "top_n_words")}
+              ></TextField>
+              <ListItemIcon>
+                <React.Fragment>
+                  <Tooltip title="Deletes all Topics and generates new ones">
+                    <IconButton
+                      onClick={handleClickOpen}
+                      loading={
+                        startMlJob.isPending ||
+                        pollMlJob.data?.status == BackgroundJobStatus.RUNNING ||
+                        pollMlJob.data?.status == BackgroundJobStatus.WAITING
+                      }
+                      color="error"
+                    >
+                      <RestartAltIcon />
+                    </IconButton>
+                  </Tooltip>
+                  <Dialog
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                  >
+                    <DialogTitle id="alert-dialog-title">{"Potential dataloss ahead! Are you sure?"}</DialogTitle>
+                    <DialogContent>
+                      <DialogContentText id="alert-dialog-description">
+                        Remove all automatic quotation annotations including any manually created, linked data such as
+                        memos?
+                      </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                      <Button onClick={handleClose} variant="outlined">
+                        Cancel
+                      </Button>
+                      <Button
+                        onClick={() => {
+                          handleStartTopicModeling(true);
+                          handleClose();
+                        }}
+                        color="error"
+                        variant="contained"
+                      >
+                        Delete & re-compute
+                      </Button>
+                    </DialogActions>
+                  </Dialog>
+                </React.Fragment>
               </ListItemIcon>
             </ListItem>
           </List>
