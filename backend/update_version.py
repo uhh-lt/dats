5898f0fccc751cc86a0f4002156f95f9aae80056
Tim Fischer <tim.fischer@uni-hamburg.de>
2025-04-17 22:52:53 +0000

    fixed issue in update-version script

--- backend/update_version.py
+++ backend/update_version.py
@@ -33,9 +33,9 @@ with open("./uv.lock", "r") as f:
 previous_line = ""
 for i, line in enumerate(uv_lock):
     if previous_line.startswith('name = "dats-backend"') and line.startswith(
-        "version: "
+        "version = "
     ):
-        uv_lock[i] = f"version: {args.version}\n"
+        uv_lock[i] = f"version = {args.version}\n"
         break
     previous_line = line
 with open("./uv.lock", "w") as f:
