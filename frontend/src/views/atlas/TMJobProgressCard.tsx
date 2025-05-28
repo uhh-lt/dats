5fe584dba9a79a079cbf387cfb6f1a9603fb4a99
Tim Fischer <tim.fischer@uni-hamburg.de>
2025-05-28 11:09:36 +0000

    added new progress card component to show detailed status information about the current tm job

---
+++ frontend/src/views/atlas/TMJobProgressCard.tsx
@@ -0,0 +1,73 @@
+import { Card, CardContent, CircularProgress, Stack, Step, StepLabel, Stepper, Typography } from "@mui/material";
+import { BackgroundJobStatus } from "../../api/openapi/models/BackgroundJobStatus.ts";
+import { TMJobRead } from "../../api/openapi/models/TMJobRead.ts";
+import { TMJobType } from "../../api/openapi/models/TMJobType.ts";
+
+const tmJobType2Title: Record<TMJobType, string> = {
+  // Aspects
+  [TMJobType.CREATE_ASPECT]: "Map Creation",
+  [TMJobType.ADD_MISSING_DOCS_TO_ASPECT]: "Add Missing Documents to Map",
+  // Topics
+  [TMJobType.ADD_TOPIC]: "Add Topic to Map",
+  [TMJobType.REMOVE_TOPIC]: "Remove Topic from Map",
+  [TMJobType.SPLIT_TOPIC]: "Split Topic in Map",
+  [TMJobType.MERGE_TOPICS]: "Merge Topics in Map",
+  // Topic Model
+  [TMJobType.REFINE_TOPIC_MODEL]: "Refine Topic Model",
+  [TMJobType.RESET_TOPIC_MODEL]: "Reset Topic Model",
+};
+
+const jobStatus2Title: Record<BackgroundJobStatus, string> = {
+  [BackgroundJobStatus.WAITING]: "Pending",
+  [BackgroundJobStatus.RUNNING]: "in Progress",
+  [BackgroundJobStatus.FINISHED]: "Finished",
+  [BackgroundJobStatus.ERRORNEOUS]: "Failed",
+  [BackgroundJobStatus.ABORTED]: "Canceled",
+};
+
+interface TMJobProgressCardProps {
+  tmJob: TMJobRead;
+}
+
+function TMJobProgressCard({ tmJob }: TMJobProgressCardProps) {
+  return (
+    <Card variant="outlined">
+      <CardContent sx={{ p: 3 }}>
+        <Stack spacing={4}>
+          <Stack direction={"row"} spacing={2} alignItems="center" justifyContent="center">
+            <Typography variant="h6" color="primary.dark" textAlign="center">
+              {tmJobType2Title[tmJob.tm_job_type]} {tmJob.status ? jobStatus2Title[tmJob.status] : "?"}
+            </Typography>
+            {tmJob.status === BackgroundJobStatus.RUNNING && <CircularProgress size={24} color="primary" />}
+          </Stack>
+          <Stepper activeStep={tmJob.step}>
+            {tmJob.steps.map((label, index) => {
+              const labelProps: {
+                optional?: React.ReactNode;
+                error?: boolean;
+              } = {};
+              if (index === tmJob.step && tmJob.status === BackgroundJobStatus.ERRORNEOUS) {
+                labelProps.optional = (
+                  <Typography variant="caption" color="error">
+                    An error occurred!
+                  </Typography>
+                );
+                labelProps.error = true;
+              }
+
+              return (
+                <Step key={label}>
+                  <StepLabel {...labelProps}>{label}</StepLabel>
+                </Step>
+              );
+            })}
+          </Stepper>
+          <Typography variant="body1" color="textSecondary">
+            Detailed status: {tmJob.status_msg}
+          </Typography>
+        </Stack>
+      </CardContent>
+    </Card>
+  );
+}
+export default TMJobProgressCard;
