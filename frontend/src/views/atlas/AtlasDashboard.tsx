562909012599d5e8769d030891fc0b9abdd3d5e2
Tim Fischer <tim.fischer@uni-hamburg.de>
2025-06-01 19:07:33 +0000

    added map thumbnail, sllight improvements to atlas dashboard

--- frontend/src/views/atlas/AtlasDashboard.tsx
+++ frontend/src/views/atlas/AtlasDashboard.tsx
@@ -1,11 +1,12 @@
-import { Box, MenuItem, Stack, TextField, Typography } from "@mui/material";
+import { Box, Button, MenuItem, Stack, TextField, Typography } from "@mui/material";
 import ContentContainerLayout from "../../layouts/ContentLayouts/ContentContainerLayout.tsx";
 import MapCard from "./MapCard.tsx";
 
 import { ChangeEvent, useState } from "react";
 import TopicModellingHooks from "../../api/TopicModellingHooks.ts";
+import { getIconComponent, Icon } from "../../utils/icons/iconUtils.tsx";
 import { useDebounce } from "../../utils/useDebounce.ts";
-import CreateMapCard from "./CreateMapCard.tsx";
+import AspectCreationDialog from "./AspectCreationDialog.tsx";
 
 function AtlasDashboard() {
   const aspects = TopicModellingHooks.useGetAllAspectsList();
@@ -29,60 +30,81 @@ function AtlasDashboard() {
       return data.name.includes(debouncedFilterQuery);
     }) || [];
 
+  // creation dialog
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <ContentContainerLayout>
-      <Stack spacing={2}>
-        <Typography variant="h4" component="h1">
-          Atlas Dashboard
-        </Typography>
-        <Stack direction={"row"} spacing={2} alignItems="center">
-          <TextField
-            type="text"
-            value={filterQuery}
-            onChange={handleFilterQueryChange}
-            placeholder="Search by name ..."
-            variant="outlined"
-            size="small"
-            fullWidth
-            sx={{
-              backgroundColor: "white",
-              boxShadow: "0px 1px 3px rgba(0,0,0,0.1)",
-              borderRadius: "4px",
-            }}
-          />
-          <TextField
-            select
-            label="Sort by"
-            value={sortBy}
-            onChange={handleSortByChange}
-            size="small"
-            variant="outlined"
-            sx={{
-              width: 200,
-              backgroundColor: "white",
-              boxShadow: "0px 1px 3px rgba(0,0,0,0.1)",
-              borderRadius: "4px",
-            }}
-          >
-            <MenuItem value="name">Name</MenuItem>
-            <MenuItem value="creationDate">Creation Date</MenuItem>
-            <MenuItem value="size">Size (Datapoints)</MenuItem>
-          </TextField>
-        </Stack>
-        <Box display="flex" gap={2} paddingY={0} flexWrap="wrap">
-          <CreateMapCard />
-          {filteredAspects.map((aspect) => (
-            <MapCard
-              key={aspect.id}
-              to={`./map-details/${aspect.id}`}
-              title={aspect.name}
-              color={"#77dd77"}
-              description={""}
+    <>
+      <AspectCreationDialog open={open} onClose={handleClose} />
+      <ContentContainerLayout>
+        <Stack spacing={2}>
+          <Stack spacing={2} direction={"row"} alignItems="center" justifyContent="space-between">
+            <Box>
+              <Typography variant="h4" component="h1" color="primary.dark">
+                Atlas Dashboard
+              </Typography>
+              <Typography pt={1} color="textSecondary">
+                Create your custom document visualization!
+              </Typography>
+            </Box>
+            <Button
+              onClick={handleOpen}
+              variant="contained"
+              color="primary"
+              sx={{ width: 174 }}
+              endIcon={getIconComponent(Icon.CREATE)}
+            >
+              Create Map
+            </Button>
+          </Stack>
+          <Stack direction={"row"} spacing={2} alignItems="center">
+            <TextField
+              type="text"
+              value={filterQuery}
+              onChange={handleFilterQueryChange}
+              placeholder="Search by name ..."
+              variant="outlined"
+              size="small"
+              fullWidth
+              sx={{
+                backgroundColor: "white",
+                boxShadow: "0px 1px 3px rgba(0,0,0,0.1)",
+                borderRadius: "4px",
+              }}
             />
-          ))}
-        </Box>
-      </Stack>
-    </ContentContainerLayout>
+            <TextField
+              select
+              label="Sort by"
+              value={sortBy}
+              onChange={handleSortByChange}
+              size="small"
+              variant="outlined"
+              sx={{
+                width: 200,
+                backgroundColor: "white",
+                boxShadow: "0px 1px 3px rgba(0,0,0,0.1)",
+                borderRadius: "4px",
+              }}
+            >
+              <MenuItem value="name">Name</MenuItem>
+              <MenuItem value="creationDate">Creation Date</MenuItem>
+              <MenuItem value="size">Size (Datapoints)</MenuItem>
+            </TextField>
+          </Stack>
+          <Box display="flex" gap={2} paddingY={0} flexWrap="wrap">
+            {filteredAspects.map((aspect) => (
+              <MapCard key={aspect.id} to={`./map-details/${aspect.id}`} title={aspect.name} aspect={aspect} />
+            ))}
+          </Box>
+        </Stack>
+      </ContentContainerLayout>
+    </>
   );
 }
 
--- frontend/src/views/atlas/MapCard.tsx
+++ frontend/src/views/atlas/MapCard.tsx
@@ -1,27 +1,34 @@
-import { Card, CardActionArea, CardActions, CardContent, Typography } from "@mui/material";
+import { Box, Card, CardActionArea, CardContent, CardMedia, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
+import { AspectRead } from "../../api/openapi/models/AspectRead.ts";
+import TopicModellingHooks from "../../api/TopicModellingHooks.ts";
+import BackgroundJobStatusBadge from "./BackgroundJobStatusBadge.tsx";
 
 interface MapCardProps {
-  to: string;
+  aspect: AspectRead;
   title: string;
-  description: string;
-  color?: string;
+  to: string;
 }
 
-function MapCard({ to, title, description, color }: MapCardProps) {
+function MapCard({ aspect, to, title }: MapCardProps) {
+  const tmJob = TopicModellingHooks.usePollTMJob(aspect.most_recent_job_id, undefined);
+
   return (
-    <Card sx={{ width: 360, flexShrink: 0 }}>
+    <Card sx={{ width: 360, flexShrink: 0, position: "relative" }}>
+      <Box position={"absolute"} top={8} right={8} zIndex={1}>
+        <BackgroundJobStatusBadge status={tmJob.data?.status} />
+      </Box>
       <CardActionArea component={Link} to={to}>
-        <CardContent sx={{ padding: "0px !important" }}>
-          <Typography variant="body2" color="textPrimary" bgcolor={color || "lightgrey"} p={2} height={200}>
-            {description}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Typography variant="subtitle1" sx={{ flexGrow: 1 }}>
+        <CardMedia
+          component="img"
+          image={`/content/projects/${aspect.project_id}/plots/aspect_${aspect.id}_map_thumbnail.png`}
+          title="Atlas Map Preview"
+        />
+        <CardContent sx={{ py: 0.5, pb: "4px !important", borderTop: "1px solid", borderColor: "grey.300" }}>
+          <Typography variant="subtitle1" fontWeight="800" color="textSecondary">
             {title}
           </Typography>
-        </CardActions>
+        </CardContent>
       </CardActionArea>
     </Card>
   );
