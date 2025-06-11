160b2f3375f27bea8c5d20f217e377f0c732aa1f
Tim Fischer <tim.fischer@uni-hamburg.de>
2025-06-11 10:51:47 +0000

    fixed base64 encoded image handling in html files

---
+++ DATSIMPORT.md
@@ -0,0 +1,9 @@
+uv run importer/dats_importer.py --input_dir /home/tfischer/Development/interactive-topic-modelling/datasets/20ngtest --backend_url http://localhost:19220/ --project_id 4 --tag_key tags --is_json --doctype text --content_key content --mime_type text/plain
+
+https://docs.nomic.ai/atlas/data-maps/topic-modeling
+
+https://docs.nomic.ai/atlas/data-maps/controls#lasso-and-tagging
+
+https://docs.nomic.ai/atlas/data-maps/guides/collaborative-tagging
+
+https://atlas.nomic.ai/data/nomic/airline-reviews-data
--- backend/src/app/preprocessing/pipeline/steps/text/init/add_text_init_steps.py
+++ backend/src/app/preprocessing/pipeline/steps/text/init/add_text_init_steps.py
@@ -8,11 +8,14 @@ def add_text_init_steps(pipeline: PreprocessingPipeline) -> None:
     from app.preprocessing.pipeline.steps.text.init.create_pptd import (
         create_pptd,
     )
+    from app.preprocessing.pipeline.steps.text.init.extract_content_in_html_from_html_docs import (
+        extract_content_in_html_from_html_docs,
+    )
     from app.preprocessing.pipeline.steps.text.init.extract_content_in_html_from_pdf_docs import (
         extract_content_in_html_from_pdf_docs,
     )
-    from app.preprocessing.pipeline.steps.text.init.extract_content_in_html_from_raw_text_docs import (
-        extract_content_in_html_from_raw_text_docs,
+    from app.preprocessing.pipeline.steps.text.init.extract_content_in_html_from_text_docs import (
+        extract_content_in_html_from_text_docs,
     )
     from app.preprocessing.pipeline.steps.text.init.extract_content_in_html_from_word_docs import (
         extract_content_in_html_from_word_docs,
@@ -35,7 +38,12 @@ def add_text_init_steps(pipeline: PreprocessingPipeline) -> None:
 
     pipeline.register_step(
         required_data=["pptd"],
-        func=extract_content_in_html_from_raw_text_docs,
+        func=extract_content_in_html_from_html_docs,
+    )
+
+    pipeline.register_step(
+        required_data=["pptd"],
+        func=extract_content_in_html_from_text_docs,
     )
 
     pipeline.register_step(
