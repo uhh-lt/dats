1c505048b01331ae1f54681fd81c481312600c69
Tim Fischer <tim.fischer@uni-hamburg.de>
2025-07-08 08:12:26 +0000

    Release v1.7.0

--- frontend/src/api/openapi/core/OpenAPI.ts
+++ frontend/src/api/openapi/core/OpenAPI.ts
@@ -21,7 +21,7 @@ export type OpenAPIConfig = {
 
 export const OpenAPI: OpenAPIConfig = {
   BASE: "",
-  VERSION: "1.6.5",
+  VERSION: "1.7.0",
   WITH_CREDENTIALS: false,
   CREDENTIALS: "include",
   TOKEN: undefined,
