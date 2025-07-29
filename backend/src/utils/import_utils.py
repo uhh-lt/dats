200125280cdb45b47a019244edaa4d73ba325adc
Tim Fischer <tim.fischer@uni-hamburg.de>
2025-07-29 13:52:11 +0000

    fixed bug with paths during production

--- backend/src/utils/import_utils.py
+++ backend/src/utils/import_utils.py
@@ -5,7 +5,7 @@ from types import ModuleType
 
 
 def import_by_suffix(suffix: str) -> list[ModuleType]:
-    root_dir = os.path.join(os.path.dirname(__file__), "../")
+    root_dir = os.path.dirname(os.path.dirname(__file__))
     modules = [
         importlib.import_module(module.replace("/", ".").replace(".py", ""))
         for module in glob.iglob(rf"**/*{suffix}", recursive=True, root_dir=root_dir)
