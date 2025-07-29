afe330f57a3c0bca4134628328408e8fba5ae5b5
Tim Fischer <tim.fischer@uni-hamburg.de>
2025-07-29 11:21:17 +0000

    fixed path issues

--- backend/src/celery_background_jobs_worker_entrypoint.sh
+++ backend/src/celery_background_jobs_worker_entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-source ../.venv/bin/activate
+source .venv/bin/activate
 
 export OMP_NUM_THREADS=1
 export MKL_NUM_THREADS=1
@@ -9,4 +9,5 @@ export MKL_NUM_THREADS=1
 LOG_LEVEL=${LOG_LEVEL:-debug}
 CELERY_BACKGROUND_JOBS_WORKER_CONCURRENCY=${CELERY_BACKGROUND_JOBS_WORKER_CONCURRENCY:-1}
 
+cd src
 celery -A core.celery.background_jobs.tasks worker -Q bgJobsQ,celery -P threads -l "$LOG_LEVEL" -c "$CELERY_BACKGROUND_JOBS_WORKER_CONCURRENCY" --without-gossip --without-mingle --without-heartbeat
