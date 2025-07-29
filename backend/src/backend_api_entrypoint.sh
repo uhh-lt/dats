35fc48032ebc4bc2ac74fe3e688b335f9097f4c4
Tim Fischer <tim.fischer@uni-hamburg.de>
2025-07-29 11:21:17 +0000

    fixed path issues

--- backend/src/backend_api_entrypoint.sh
+++ backend/src/backend_api_entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 set -e
-
-source ../.venv/bin/activate
+source .venv/bin/activate
 
 LOG_LEVEL=${LOG_LEVEL:-debug}
 API_PORT=${API_PORT:-5500}
 API_WORKERS=${API_WORKERS:-10}
 
+cd src
 uvicorn --log-level "${LOG_LEVEL}" --port "${API_PORT}" --host "0.0.0.0" --workers " ${API_WORKERS}" main:app
