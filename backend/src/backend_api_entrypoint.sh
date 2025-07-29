b60ed35f1504dc05f7f85cd1c358f890ef55d7ea
Tim Fischer <tim.fischer@uni-hamburg.de>
2025-07-29 13:52:11 +0000

    fixed bug with paths during production

--- backend/src/backend_api_entrypoint.sh
+++ backend/src/backend_api_entrypoint.sh
@@ -7,5 +7,4 @@ LOG_LEVEL=${LOG_LEVEL:-debug}
 API_PORT=${API_PORT:-5500}
 API_WORKERS=${API_WORKERS:-10}
 
-cd src
 uvicorn --log-level "${LOG_LEVEL}" --port "${API_PORT}" --host "0.0.0.0" --workers " ${API_WORKERS}" main:app
