505afd7d3fe4e816ce1be5b679ee9b57c3d53092
Tim Fischer <tim.fischer@uni-hamburg.de>
2025-07-29 10:03:00 +0000

    fixed entrypoint

--- backend/src/backend_api_entrypoint.sh
+++ backend/src/backend_api_entrypoint.sh
@@ -8,4 +8,4 @@ LOG_LEVEL=${LOG_LEVEL:-debug}
 API_PORT=${API_PORT:-5500}
 API_WORKERS=${API_WORKERS:-10}
 
-uvicorn --log-level "${LOG_LEVEL}" --port "${API_PORT}" --host "0.0.0.0" - -workers " ${API_WORKERS}" main:app
+uvicorn --log-level "${LOG_LEVEL}" --port "${API_PORT}" --host "0.0.0.0" --workers " ${API_WORKERS}" main:app
