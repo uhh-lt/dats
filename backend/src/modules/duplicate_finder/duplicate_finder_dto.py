df3fac0c4fa98239e79e614d382d9fe63096a213
Tim Fischer <tim.fischer@uni-hamburg.de>
2025-07-30 16:21:14 +0000

    new duplicate finder

--- backend/src/modules/duplicate_finder/duplicate_finder_dto.py
+++ backend/src/modules/duplicate_finder/duplicate_finder_dto.py
@@ -1,22 +1,15 @@
-from typing import List
-
 from pydantic import BaseModel, Field
 from systems.job_system.job_dto import JobInputBase
 
 
 class DuplicateFinderInput(JobInputBase):
     project_id: int = Field(..., description="Project ID to search for duplicates")
-    user_id: int = Field(..., description="User ID who started the job")
-
-
-class DuplicateInfo(BaseModel):
-    doc_id: int = Field(..., description="Document ID")
-    duplicate_of: int = Field(
-        ..., description="ID of the document this is a duplicate of"
+    max_different_words: int = Field(
+        ..., description="Number of different words allowed between duplicates"
     )
 
 
 class DuplicateFinderOutput(BaseModel):
-    project_id: int = Field(..., description="Project ID")
-    user_id: int = Field(..., description="User ID")
-    duplicates: List[DuplicateInfo] = Field(..., description="List of found duplicates")
+    duplicates: list[list[int]] = Field(
+        ..., description="List of found duplicate clusters"
+    )
--- backend/src/modules/duplicate_finder/duplicate_finder_jobs.py
+++ backend/src/modules/duplicate_finder/duplicate_finder_jobs.py
@@ -1,11 +1,18 @@
 import time
 
+import networkx as nx
+import numpy as np
+from common.doc_type import DocType
+from loguru import logger
 from modules.duplicate_finder.duplicate_finder_dto import (
     DuplicateFinderInput,
     DuplicateFinderOutput,
-    DuplicateInfo,
 )
+from modules.word_frequency.word_frequency_crud import crud_word_frequency
+from repos.db.sql_repo import SQLRepo
 from rq import get_current_job
+from scipy import sparse
+from sklearn.metrics.pairwise import manhattan_distances
 from systems.job_system.job_register_decorator import register_job
 
 
@@ -21,26 +28,98 @@ def find_duplicates_job(
     job = get_current_job()
     assert job is not None, "Job must be running in a worker context"
 
-    job.meta["status_message"] = "Started duplicate search"
+    job.meta["status_message"] = "Started duplicate finding"
     job.save_meta()
 
-    time.sleep(1)
-
-    job.meta["status_message"] = "50% complete"
+    logger.info("Finding duplicate text sdocs")
+    t0 = time.time()
+    with SQLRepo().db_session() as db:
+        result = crud_word_frequency.read_by_project_and_doctype(
+            db, project_id=payload.project_id, doctype=DocType.text
+        )
+    t1 = time.time()
+    job.meta["status_message"] = "Fetched word frequencies from database"
     job.save_meta()
+    logger.info(f"query took: {t1 - t0}")
 
-    time.sleep(1)
-    duplicates = [
-        DuplicateInfo(doc_id=1, duplicate_of=2),
-        DuplicateInfo(doc_id=3, duplicate_of=4),
-    ]
-    output_dto = DuplicateFinderOutput(
-        project_id=payload.project_id,
-        user_id=payload.user_id,
-        duplicates=duplicates,
+    t0 = time.time()
+    # unique words in project
+    words = set([r.word.lower() for r in result])
+    word2idx = {w: i for i, w in enumerate(words)}
+    vocab_size = len(words)
+
+    # process result to map
+    sdoc_id2word_id2word_freq = {}
+    for wf in result:
+        word_id2_word_freq = sdoc_id2word_id2word_freq.get(wf.sdoc_id, {})
+        word_id2_word_freq[word2idx[wf.word.lower()]] = wf.count
+        sdoc_id2word_id2word_freq[wf.sdoc_id] = word_id2_word_freq
+
+    # X.create document vectors
+    idx2sdoc_id = {}
+    values = []
+    indices = []
+    index = []
+    for idx, sdoc_id in enumerate(sdoc_id2word_id2word_freq.keys()):
+        word_id2_word_freq = sdoc_id2word_id2word_freq[sdoc_id]
+
+        indices.extend(word_id2_word_freq.keys())
+        values.extend(word_id2_word_freq.values())
+        index.extend([idx] * len(word_id2_word_freq))
+
+        idx2sdoc_id[idx] = sdoc_id
+
+    document_vectors = sparse.csc_matrix(
+        (values, (index, indices)), shape=(len(idx2sdoc_id), vocab_size)
     )
-
-    job.meta["status_message"] = "Done with duplicate finding!"
+    t1 = time.time()
+    job.meta["status_message"] = "Created document word vectors"
     job.save_meta()
+    logger.info(f"document vector creation took: {t1 - t0}")
+    logger.info(f"vocab size: {vocab_size}")
+    logger.info(f"document_vectors shape: {document_vectors.shape}")
 
-    return output_dto
+    # compute distances
+    t0 = time.time()
+    word_dists = manhattan_distances(document_vectors, document_vectors)
+    t1 = time.time()
+    job.meta["status_message"] = "Computed distances between documents"
+    job.save_meta()
+    logger.info(f"manhatten distance took: {t1 - t0}")
+
+    # mask out self distances and one half of the matrix
+    zeroed_minuses = np.triu(np.ones_like(word_dists) * -1, k=0)
+    zeroed_word_dists = np.tril(word_dists, k=-1)
+    masked_word_dists = zeroed_word_dists + zeroed_minuses
+
+    # find duplicates
+    t0 = time.time()
+    duplicate_pairs = np.transpose(
+        np.where(
+            (masked_word_dists <= payload.max_different_words)
+            & (masked_word_dists >= 0)
+        )
+    ).tolist()
+    t1 = time.time()
+    job.meta["status_message"] = "Identified duplicate pairs"
+    job.save_meta()
+    logger.info(f"finding duplicates took: {t1 - t0}")
+
+    # map back to sdoc_ids
+    duplicate_sdoc_id_pairs = [
+        (idx2sdoc_id[pair[0]], idx2sdoc_id[pair[1]]) for pair in duplicate_pairs
+    ]
+
+    # we now create a graph with sdocs as nodes and edges between duplicates
+    # we will use this graph to identify connected components, each subgraph is a group of duplicates
+    t0 = time.time()
+    G = nx.Graph()
+    G.add_edges_from(duplicate_sdoc_id_pairs)
+    G.to_undirected()
+    subgraph_nodes = [list(subgraph) for subgraph in nx.connected_components(G)]
+    t1 = time.time()
+    job.meta["status_message"] = "Finished finding duplicates!"
+    job.save_meta()
+    logger.info(f"graph grouping took: {t1 - t0}")
+
+    return DuplicateFinderOutput(duplicates=subgraph_nodes)
