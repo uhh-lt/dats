edcdd00604973b5c362b57d0363ebfa87f3fcc45
Tim Fischer <tim.fischer@uni-hamburg.de>
2025-07-31 10:28:40 +0000

    removed unnecessary project_id definition

--- backend/src/modules/duplicate_finder/duplicate_finder_dto.py
+++ backend/src/modules/duplicate_finder/duplicate_finder_dto.py
@@ -3,7 +3,6 @@ from systems.job_system.job_dto import JobInputBase
 
 
 class DuplicateFinderInput(JobInputBase):
-    project_id: int = Field(..., description="Project ID to search for duplicates")
     max_different_words: int = Field(
         ..., description="Number of different words allowed between duplicates"
     )
