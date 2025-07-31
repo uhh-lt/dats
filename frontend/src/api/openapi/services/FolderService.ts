769c82a96e40e4501391d2224bd3b7560caa6920
Tim Fischer <tim.fischer@uni-hamburg.de>
2025-07-31 14:45:27 +0000

    updated api

--- frontend/src/api/openapi/services/FolderService.ts
+++ frontend/src/api/openapi/services/FolderService.ts
@@ -5,6 +5,7 @@
 import type { FolderCreate } from "../models/FolderCreate";
 import type { FolderRead } from "../models/FolderRead";
 import type { FolderTreeRead } from "../models/FolderTreeRead";
+import type { FolderType } from "../models/FolderType";
 import type { FolderUpdate } from "../models/FolderUpdate";
 import type { CancelablePromise } from "../core/CancelablePromise";
 import { OpenAPI } from "../core/OpenAPI";
@@ -97,6 +98,30 @@ export class FolderService {
       url: "/folder/project/{project_id}/tree",
       path: {
         project_id: projectId,
+      },
+      errors: {
+        422: `Validation Error`,
+      },
+    });
+  }
+  /**
+   * Returns the folders of the folder_type of the project with the given ID
+   * @returns FolderRead Successful Response
+   * @throws ApiError
+   */
+  public static getFoldersByProjectAndType({
+    projectId,
+    folderType,
+  }: {
+    projectId: number;
+    folderType: FolderType;
+  }): CancelablePromise<Array<FolderRead>> {
+    return __request(OpenAPI, {
+      method: "GET",
+      url: "/folder/project/{project_id}/folder/{folder_type}",
+      path: {
+        project_id: projectId,
+        folder_type: folderType,
       },
       errors: {
         422: `Validation Error`,
