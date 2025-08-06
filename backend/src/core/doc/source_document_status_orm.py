b7898d3bcbd79bedc9eba4fe924af2e593da5058
Fynn Petersen-Frey <fynn.petersen-frey@uni-hamburg.de>
2025-08-06 14:05:53 +0000

    spacy service

---
+++ backend/src/core/doc/source_document_status_orm.py
@@ -0,0 +1,23 @@
+from typing import TYPE_CHECKING
+
+from repos.db.orm_base import ORMBase
+from sqlalchemy import Boolean, ForeignKey, Integer
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+if TYPE_CHECKING:
+    from core.doc.source_document_orm import SourceDocumentORM
+
+
+class SourceDocumentStatusORM(ORMBase):
+    id: Mapped[int] = mapped_column(
+        Integer,
+        ForeignKey("sourcedocument.id", ondelete="CASCADE"),
+        primary_key=True,
+        nullable=False,
+        index=True,
+    )
+    source_document: Mapped["SourceDocumentORM"] = relationship("SourceDocumentORM")
+    spacy: Mapped[bool] = mapped_column(Boolean, nullable=False)
+
+    def get_project_id(self) -> int:
+        return self.source_document.get_project_id()
