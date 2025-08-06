462553ade5fa7ac5194a27edccf0b62727c82660
Fynn Petersen-Frey <fynn.petersen-frey@uni-hamburg.de>
2025-08-06 14:43:14 +0000

    es_job & lang detect job

---
+++ backend/src/modules/text_processing/detect_language_job.py
@@ -0,0 +1,95 @@
+from common.doc_type import DocType
+from common.meta_type import MetaType
+from core.doc.source_document_data_crud import crud_sdoc_data
+from core.doc.source_document_status_crud import crud_sdoc_status
+from core.doc.source_document_status_dto import SourceDocumentStatusUpdate
+from core.metadata.project_metadata_crud import crud_project_meta
+from core.metadata.source_document_metadata_crud import crud_sdoc_meta
+from core.metadata.source_document_metadata_dto import SourceDocumentMetadataCreate
+from ray_model_worker.dto.glotlid import GlotLIDInput, GlotLIDOutput
+from repos.db.sql_repo import SQLRepo
+from repos.ray_repo import RayRepo
+from systems.job_system.job_dto import (
+    EndpointGeneration,
+    Job,
+    JobInputBase,
+    JobPriority,
+)
+from systems.job_system.job_register_decorator import register_job
+
+ray = RayRepo()
+
+
+class LanguageNotSupportedError(Exception):
+    def __init__(self, detected_language: str) -> None:
+        super().__init__(
+            f"The detected language '{detected_language}' is not supported by DATS!"
+        )
+
+
+class DetectLanguageJobInput(JobInputBase):
+    sdoc_id: int
+    text: str | None
+    doctype: DocType | None
+
+
+@register_job(
+    job_type="detect_language",
+    input_type=DetectLanguageJobInput,
+    output_type=None,
+    priority=JobPriority.DEFAULT,
+    generate_endpoints=EndpointGeneration.NONE,
+)
+def handle_detect_language_job(payload: DetectLanguageJobInput, job: Job) -> None:
+    # if we re-run this job, text is None or doctype is None, we need to query it from db
+    with SQLRepo().db_session() as db:
+        if payload.text is None or payload.doctype is None:
+            sdoc_data = crud_sdoc_data.read(db=db, id=payload.sdoc_id)
+            payload.text = sdoc_data.content
+            payload.doctype = DocType(sdoc_data.source_document.doctype)
+
+        # DETECT LANGUAGE
+        # TODO Flo: what to do with mixed lang docs?
+        glotlid_input = GlotLIDInput(text=payload.text)
+        glotlid_output: GlotLIDOutput = ray.language_identification(glotlid_input)
+
+        # map the GlodLID language code to the ISO 639-1 language code we support in our spaCy Pipeline
+        # TODO: we should set this in a config file or so
+        code_map = {
+            "eng_Latn": "en",
+            "deu_Latn": "de",
+            "ita_Latn": "it",
+        }
+
+        lang_code = glotlid_output.best_match.lang_code
+        lang = code_map.get(lang_code, None)
+        if lang is None:
+            raise LanguageNotSupportedError(detected_language=lang_code)
+
+        # Store language in db
+        lang_project_metadata = (
+            crud_project_meta.read_by_project_and_key_and_metatype_and_doctype(
+                db=db,
+                project_id=payload.project_id,
+                key="language",
+                metatype=MetaType.STRING,
+                doctype=payload.doctype,
+            )
+        )
+        assert lang_project_metadata is not None, "Language metadata does not exist!"
+        crud_sdoc_meta.create(
+            db=db,
+            create_dto=SourceDocumentMetadataCreate.with_metatype(
+                value=lang,
+                source_document_id=payload.sdoc_id,
+                project_metadata_id=lang_project_metadata.id,
+                metatype=MetaType.STRING,
+            ),
+        )
+
+        # Set db status
+        crud_sdoc_status.update(
+            db=db,
+            id=payload.sdoc_id,
+            update_dto=SourceDocumentStatusUpdate(lang_detect=True),
+        )
---
+++ backend/src/modules/text_processing/es_index_job.py
@@ -0,0 +1,54 @@
+from core.doc.sdoc_elastic_crud import crud_elastic_sdoc
+from core.doc.sdoc_elastic_dto import ElasticSearchDocumentCreate
+from core.doc.source_document_data_crud import crud_sdoc_data
+from core.doc.source_document_status_crud import crud_sdoc_status
+from core.doc.source_document_status_dto import SourceDocumentStatusUpdate
+from repos.db.sql_repo import SQLRepo
+from repos.elastic.elastic_repo import ElasticSearchRepo
+from systems.job_system.job_dto import (
+    EndpointGeneration,
+    Job,
+    JobInputBase,
+    JobPriority,
+)
+from systems.job_system.job_register_decorator import register_job
+
+
+class ESIndexJobInput(JobInputBase):
+    sdoc_id: int
+    filename: str | None
+    text: str | None
+
+
+@register_job(
+    job_type="es_index",
+    input_type=ESIndexJobInput,
+    output_type=None,
+    priority=JobPriority.DEFAULT,
+    generate_endpoints=EndpointGeneration.NONE,
+)
+def handle_es_index_job(payload: ESIndexJobInput, job: Job) -> None:
+    # if we re-run this job, filename and text is None, we need to query it from db
+    if payload.filename is None or payload.text is None:
+        with SQLRepo().db_session() as db:
+            sdoc_data = crud_sdoc_data.read(db=db, id=payload.sdoc_id)
+            payload.text = sdoc_data.content
+            payload.filename = sdoc_data.source_document.filename
+
+    esdoc = ElasticSearchDocumentCreate(
+        filename=payload.filename,
+        content=payload.text,
+        sdoc_id=payload.sdoc_id,
+        project_id=payload.project_id,
+    )
+
+    crud_elastic_sdoc.create(
+        client=ElasticSearchRepo().client,
+        create_dto=esdoc,
+        proj_id=payload.project_id,
+    )
+
+    # Set db status
+    crud_sdoc_status.update(
+        db=db, id=payload.sdoc_id, update_dto=SourceDocumentStatusUpdate(es_index=True)
+    )
