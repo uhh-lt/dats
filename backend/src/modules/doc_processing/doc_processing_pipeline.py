0cbd958a180a2e4536864efe016b0eb1b821b978
Fynn Petersen-Frey <fynn.petersen-frey@uni-hamburg.de>
2025-08-11 04:57:08 +0000

    new text pipeline

--- backend/src/modules/doc_processing/doc_processing_pipeline.py
+++ backend/src/modules/doc_processing/doc_processing_pipeline.py
@@ -1,40 +1,32 @@
+from common.doc_type import DocType
 from common.job_type import JobType
 from core.doc.source_document_status_crud import crud_sdoc_status
 from core.doc.source_document_status_dto import SourceDocumentStatusUpdate
 from loguru import logger
 from modules.doc_image_processing.image_sdoc_job import ImageSdocJobInput
 from modules.doc_processing.text_init_job import TextInitJobInput, TextInitJobOutput
+from modules.doc_text_processing.detect_language_job import (
+    DetectLanguageJobInput,
+    DetectLanguageJobOutput,
+)
+from modules.doc_text_processing.es_index_job import ESIndexJobInput
 from modules.doc_text_processing.html_extraction_job import (
     ExtractHTMLJobInput,
     ExtractHTMLJobOutput,
 )
+from modules.doc_text_processing.html_mapping_job import (
+    ExtractPlainTextJobInput,
+    ExtractPlainTextJobOutput,
+    HTMLMappingJobInput,
+)
+from modules.doc_text_processing.sentence_embedding_job import SentenceEmbeddingJobInput
+from modules.doc_text_processing.spacy_job import SpacyJobInput, SpacyJobOutput
 from pydantic import BaseModel
 from repos.db.sql_repo import SQLRepo
-from systems.event_system.events import job_finished
 from systems.job_system.job_dto import JobInputBase
 from systems.job_system.job_service import JobService
 
 js = JobService()
-
-
-@job_finished.connect
-def job_finished_handler(
-    sender, job_type: str, input: JobInputBase, output: BaseModel | None
-):
-    logger.info(f"Job finished: {job_type}, Input: {input}, Output: {output}")
-
-    if job_type == "text_init":
-        assert isinstance(input, TextInitJobInput), "Input must be TextInitJobInput"
-        assert isinstance(output, TextInitJobOutput), "Output must be TextInitJobOutput"
-        logger.info(f"Text initialization completed for {input.filepath.name}")
-        js.start_job(
-            job_type=JobType.EXTRACT_HTML,
-            payload=ExtractHTMLJobInput(
-                project_id=input.project_id,
-                sdoc_id=output.sdoc_id,
-                filepath=input.filepath,
-            ),
-        )
 
 
 def handle_job_finished(
@@ -54,12 +46,11 @@ def handle_job_finished(
                 update_dto=SourceDocumentStatusUpdate(**kwargs),
             )
 
+    # Jobs without sdoc_id
     match job_type:
         case JobType.TEXT_INIT:
-            assert isinstance(input, TextInitJobInput), "Input must be TextInitJobInput"
-            assert isinstance(output, TextInitJobOutput), (
-                "Output must be TextInitJobOutput"
-            )
+            assert isinstance(input, TextInitJobInput)
+            assert isinstance(output, TextInitJobOutput)
             logger.info(f"Text initialization completed for {input.filepath.name}")
             js.start_job(
                 job_type=JobType.EXTRACT_HTML,
@@ -67,23 +58,25 @@ def handle_job_finished(
                     project_id=input.project_id,
                     sdoc_id=output.sdoc_id,
                     filepath=input.filepath,
+                    doctype=DocType.text,
                 ),
             )
+
+    assert sdoc_id is not None, "sdoc_id must be set"
+
+    # Jobs requiring sdoc_id are below:
+    match job_type:
         case JobType.EXTRACT_HTML:
-            assert isinstance(input, ExtractHTMLJobInput), (
-                "Input must be ExtractHTMLJobInput"
-            )
-            assert isinstance(output, ExtractHTMLJobOutput), (
-                "Output must be TextInitJobOutput"
-            )
-            assert sdoc_id is not None, "sdoc_id must be set"
-            logger.info(f"Text initialization completed for {input.filepath.name}")
+            assert isinstance(input, ExtractHTMLJobInput)
+            assert isinstance(output, ExtractHTMLJobOutput)
             js.start_job(
-                job_type=JobType.EXTRACT_HTML,
-                payload=ExtractHTMLJobInput(
+                job_type=JobType.EXTRACT_PLAIN_TEXT,
+                payload=ExtractPlainTextJobInput(
                     project_id=input.project_id,
                     sdoc_id=sdoc_id,
-                    filepath=input.filepath,
+                    html=output.html,
+                    filename=input.filepath.name,
+                    doctype=input.doctype,
                 ),
             )
             # FIXME: create folder... in EXTRACT_HTML job?
@@ -97,3 +90,62 @@ def handle_job_finished(
                         folder_id=folder_id,
                     ),
                 )
+        case JobType.EXTRACT_PLAIN_TEXT:
+            assert isinstance(input, ExtractPlainTextJobInput)
+            assert isinstance(output, ExtractPlainTextJobOutput)
+            js.start_job(
+                job_type=JobType.DETECT_LANGUAGE,
+                payload=DetectLanguageJobInput(
+                    project_id=input.project_id,
+                    sdoc_id=sdoc_id,
+                    text=output.text,
+                    doctype=input.doctype,
+                    html=input.html,
+                ),
+            )
+            js.start_job(
+                job_type=JobType.ES_INDEX,
+                payload=ESIndexJobInput(
+                    project_id=input.project_id,
+                    sdoc_id=sdoc_id,
+                    text=output.text,
+                    filename=input.filename,
+                ),
+            )
+        case JobType.DETECT_LANGUAGE:
+            assert isinstance(input, DetectLanguageJobInput)
+            assert isinstance(output, DetectLanguageJobOutput)
+            js.start_job(
+                job_type=JobType.SPACY,
+                payload=SpacyJobInput(
+                    project_id=input.project_id,
+                    sdoc_id=sdoc_id,
+                    text=output.text,
+                    doctype=input.doctype,
+                    language=output.language,
+                    html=input.html,
+                ),
+            )
+        case JobType.SPACY:
+            assert isinstance(input, SpacyJobInput)
+            assert isinstance(output, SpacyJobOutput)
+            js.start_job(
+                job_type=JobType.HTML_MAPPING,
+                payload=HTMLMappingJobInput(
+                    project_id=input.project_id,
+                    sdoc_id=sdoc_id,
+                    raw_html=input.html,
+                    sentence_starts=output.sentence_starts,
+                    sentence_ends=output.sentence_ends,
+                    token_starts=output.token_starts,
+                    token_ends=output.token_ends,
+                ),
+            )
+            js.start_job(
+                job_type=JobType.SENTENCE_EMBEDDING,
+                payload=SentenceEmbeddingJobInput(
+                    project_id=input.project_id,
+                    sdoc_id=sdoc_id,
+                    sentences=output.sentences,
+                ),
+            )
