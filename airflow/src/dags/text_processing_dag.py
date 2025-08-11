c7851fab4408d3a62f483cda6882cc7311f3141f
Tim Fischer <tim.fischer@uni-hamburg.de>
2025-08-11 15:10:37 +0000

    added airflow idea

---
+++ airflow/src/dags/text_processing_dag.py
@@ -0,0 +1,46 @@
+import pendulum
+from airflow.sdk import dag, task
+
+
+@dag(
+    schedule=None,
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+    tags=["preprocessing"],
+)
+def text_processing_dag():
+    """
+    ### TaskFlow API Tutorial Documentation
+    This is a simple data pipeline example which demonstrates the use of
+    the TaskFlow API using three simple tasks for Extract, Transform, and Load.
+    Documentation that goes along with the Airflow TaskFlow API tutorial is
+    located
+    [here](https://airflow.apache.org/docs/apache-airflow/stable/tutorial_taskflow_api.html)
+    """
+
+    @task.external_python(
+        task_id="create_project", python="/dats_code/dats-venv/bin/python"
+    )
+    def create_project() -> int:
+        from modules.doc_text_processing.example_text_processing_task import (  # pyright: ignore[reportMissingImports]
+            create_project_task,
+        )
+
+        return create_project_task()
+
+    @task()
+    def print_project(project_id: int):
+        """
+        #### Print Project task
+        A simple Load task which takes in the result of the Transform task and
+        instead of saving it to end user review, just prints it out.
+        """
+
+        print(f"A project was recently created! The id is {project_id}.")
+
+    project_id = create_project()
+    assert isinstance(project_id, int), "Project ID should be an integer."
+    print_project(project_id=project_id)
+
+
+text_processing_dag()
