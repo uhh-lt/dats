7dd52410ec2ba09b72aa40daaf047a77f3f3080c
Tim Fischer <tim.fischer@uni-hamburg.de>
2025-08-13 13:45:36 +0000

    missed dto update

--- backend/src/core/doc/source_document_status_dto.py
+++ backend/src/core/doc/source_document_status_dto.py
@@ -15,19 +15,30 @@ class SourceDocumentStatusCreate(SourceDocumenStatusBaseDTO):
 
 
 class SourceDocumentStatusUpdate(BaseModel, UpdateDTOBase):
-    # TEXT
-    text_init: bool | None = Field(description="Text Init done?", default=None)
-    spacy: bool | None = Field(description="Spacy done?", default=None)
-    es_index: bool | None = Field(description="ES Index done?", default=None)
-    lang_detect: bool | None = Field(
-        description="Language Detection done?", default=None
+    # KEEP THE SAME ORDER AS source_document_status_orm.py!
+
+    # init
+    sdoc_init: bool | None = Field(description="SDoc Init done?", default=None)
+    extract_archive: bool | None = Field(
+        description="Extract Archive done?", default=None
     )
-    html_mapping: bool | None = Field(description="HTML Mapping done?", default=None)
-    html_extraction: bool | None = Field(
-        description="HTML Extraction done?", default=None
+    pdf_checking: bool | None = Field(description="PDF Checking done?", default=None)
+    extract_html: bool | None = Field(description="Extract HTML done?", default=None)
+
+    # HTML
+    text_extraction: bool | None = Field(
+        description="Text Extraction done?", default=None
     )
-    sentence_embedding: bool | None = Field(
-        description="Sentence Embedding done?", default=None
+    text_language_detection: bool | None = Field(
+        description="Text Language Detection done?", default=None
+    )
+    text_spacy: bool | None = Field(description="Text Spacy done?", default=None)
+    text_es_index: bool | None = Field(description="Text ES Index done?", default=None)
+    text_sentence_embedding: bool | None = Field(
+        description="Text Sentence Embedding done?", default=None
+    )
+    text_html_mapping: bool | None = Field(
+        description="Text HTML Mapping done?", default=None
     )
 
     # IMAGES
@@ -37,13 +48,13 @@ class SourceDocumentStatusUpdate(BaseModel, UpdateDTOBase):
     image_embedding: bool | None = Field(
         description="Image Embedding done?", default=None
     )
-    image_metadata: bool | None = Field(
+    image_metadata_extraction: bool | None = Field(
         description="Image Metadata Extraction done?", default=None
     )
     image_thumbnail: bool | None = Field(
         description="Image Thumbnail Generation done?", default=None
     )
-    object_detection: bool | None = Field(
+    image_object_detection: bool | None = Field(
         description="Object Detection done?", default=None
     )
 
@@ -54,7 +65,9 @@ class SourceDocumentStatusUpdate(BaseModel, UpdateDTOBase):
     audio_thumbnail: bool | None = Field(
         description="Audio Thumbnail Generation done?", default=None
     )
-    transcription: bool | None = Field(description="Transcription done?", default=None)
+    audio_transcription: bool | None = Field(
+        description="Transcription done?", default=None
+    )
 
     # VIDEO
     video_metadata: bool | None = Field(
@@ -62,4 +75,7 @@ class SourceDocumentStatusUpdate(BaseModel, UpdateDTOBase):
     )
     video_thumbnail: bool | None = Field(
         description="Video Thumbnail Generation done?", default=None
+    )
+    video_audio_extraction: bool | None = Field(
+        description="Video Audio Extraction done?", default=None
     )
