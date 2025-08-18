da22b6030893ccdea5709782ae320fe8aeca8d83
Tim Fischer <tim.fischer@uni-hamburg.de>
2025-08-18 14:24:28 +0000

    remove spacy

--- backend/src/modules/ml/source_document_job_status_dto.py
+++ backend/src/modules/ml/source_document_job_status_dto.py
@@ -6,7 +6,6 @@ from repos.db.dto_base import UpdateDTOBase
 
 
 class JobType(IntEnum):
-    SPACY = 10
     QUOTATION_ATTRIBUTION = 100
     COREFERENCE_RESOLUTION = 101
     DOCUMENT_EMBEDDING = 102
