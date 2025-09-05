b0c193a4547a0810abc6a1dcf51e9bbb258de23c
Tim Fischer <tim.fischer@uni-hamburg.de>
2025-09-05 15:16:15 +0000

    added new HF_HUB_TOKEN env variable to log-in to huggingface to download models!

--- backend/src/modules/classifier/models/text_class_model_service.py
+++ backend/src/modules/classifier/models/text_class_model_service.py
@@ -1,7 +1,10 @@
 from abc import ABC, abstractmethod
 
+from huggingface_hub import login
+from loguru import logger
 from sqlalchemy.orm import Session
 
+from config import conf
 from modules.classifier.classifier_dto import (
     ClassifierJobInput,
     ClassifierJobOutput,
@@ -10,6 +13,10 @@ from systems.job_system.job_dto import Job
 
 
 class TextClassificationModelService(ABC):
+    def __init__(self):
+        login(conf.api.hf_hub_token)
+        logger.info("Logged in to Hugging Face Hub!")
+
     @abstractmethod
     def train(
         self, db: Session, job: Job, payload: ClassifierJobInput
