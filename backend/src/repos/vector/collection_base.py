f9a19800a06bcf831a326f8564f7710166f7f1cb
Steve Ali <steveali61@gmail.com>
2025-11-20 13:15:23 +0000

    added weaviate postfix (for testing purposes)

--- backend/src/repos/vector/collection_base.py
+++ backend/src/repos/vector/collection_base.py
@@ -2,6 +2,8 @@ from loguru import logger
 from weaviate import WeaviateClient
 from weaviate.classes.config import Configure, Property, VectorDistances
 
+from config import conf
+
 
 class BaseCollection:
     name: str
@@ -12,7 +14,7 @@ class BaseCollection:
     def create_collection(cls, client: WeaviateClient):
         if not client.collections.exists(cls.name):
             client.collections.create(
-                name=cls.name,
+                name=cls.name + conf.weaviate.collection_postfix,
                 description=cls.description,
                 properties=list(cls.properties.values()),
                 # All configurations below apply to all collections:
