40d6400d621fca29ab1c341fc9030b58fe3b29ad
Tim Fischer <tim.fischer@uni-hamburg.de>
2025-12-03 13:50:58 +0000

    udpated api

--- frontend/src/api/openapi/models/AspectRead.ts
+++ frontend/src/api/openapi/models/AspectRead.ts
@@ -2,6 +2,8 @@
 /* istanbul ignore file */
 /* tslint:disable */
 /* eslint-disable */
+import type { DocType } from "./DocType";
+import type { PipelineSettings } from "./PipelineSettings";
 export type AspectRead = {
   /**
    * Name of the aspect
@@ -19,6 +21,18 @@ export type AspectRead = {
    * Whether the aspect is hierarchical
    */
   is_hierarchical: boolean;
+  /**
+   * Modality of the documents of this aspect
+   */
+  modality: DocType;
+  /**
+   * Pipeline settings for this aspect
+   */
+  pipeline_settings: PipelineSettings;
+  /**
+   * ID of the tag associated with this aspect.
+   */
+  tag_id?: number | null;
   /**
    * ID of the aspect
    */
