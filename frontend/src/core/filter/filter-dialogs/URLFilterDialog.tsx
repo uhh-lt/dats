8731043fc88308708080d5daa6da30c9171a7ac6
Tim Fischer <tim.fischer@uni-hamburg.de>
2026-03-25 18:22:03 +0000

    improved URLFilterDialog typing

--- frontend/src/core/filter/filter-dialogs/URLFilterDialog.tsx
+++ frontend/src/core/filter/filter-dialogs/URLFilterDialog.tsx
@@ -1,67 +1,65 @@
 import { useURLConnector } from "@hooks/useURLConnector";
 import { RootState } from "@store/store";
 import { useAppSelector } from "@store/storeHooks";
-import { AnyRouter, RouteApi } from "@tanstack/react-router";
-import { memo, useCallback, useMemo } from "react";
-import { ColumnInfo, MyFilter, MyFilterExpression } from "../filterUtils";
-import {
-  FILTER_EXPERT_MODE_PARAM,
-  FILTER_PARAM,
-  deserializeFilterFromSearchParam,
-  serializeFilterToSearchParam,
-} from "../store";
+import type { RouteApi } from "@tanstack/react-router";
+import type { AnyRouter, RouteTypesById } from "@tanstack/router-core";
+import { memo } from "react";
+import { ColumnInfo, MyFilterExpression } from "../filterUtils";
+import { FILTER_EXPERT_MODE_PARAM, FILTER_PARAM, useFilterURLConnector } from "../store";
 import { FilterDialog, InternalFilterDialogProps } from "./FilterDialog";
 
-export interface URLFilterDialogProps {
+// This ensures that the routeAPI passed to URLFilterDialog has the required search params for filter (FILTER_PARAM) and expert mode (FILTER_EXPERT_MODE_PARAM)
+type RouteApiWithFilterSearchParams<TId extends string, TRouter extends AnyRouter> = RouteApi<TId, TRouter> &
+  (RouteTypesById<TRouter, TId>["fullSearchSchema"] extends Record<typeof FILTER_PARAM, string | undefined> &
+    Record<typeof FILTER_EXPERT_MODE_PARAM, boolean | undefined>
+    ? unknown
+    : never);
+
+export interface URLFilterDialogProps<
+  TId extends string = string,
+  TRouter extends AnyRouter = AnyRouter,
+  TFilter extends string = string,
+> {
   filterName: string;
-  routeApi: RouteApi<string, AnyRouter>;
-  defaultFilterExpression: MyFilterExpression;
+  routeApi: RouteApiWithFilterSearchParams<TId, TRouter>;
+  filterColumnsEnum?: Record<string, TFilter>;
+  defaultFilterExpression: MyFilterExpression<TFilter>;
   column2InfoSelector: (state: RootState) => Record<string, ColumnInfo>;
 }
 
-export const URLFilterDialog = memo(
-  ({
-    filterName,
-    routeApi,
-    defaultFilterExpression,
-    column2InfoSelector,
-    //
-    anchorEl,
-    buttonProps,
-    anchorOrigin,
-    transformOrigin,
-  }: URLFilterDialogProps &
-    Pick<InternalFilterDialogProps, "anchorEl" | "buttonProps" | "transformOrigin" | "anchorOrigin">) => {
-    const column2Info = useAppSelector(column2InfoSelector);
+const URLFilterDialogComponent = <TId extends string, TRouter extends AnyRouter, TFilter extends string = string>({
+  filterName,
+  routeApi,
+  filterColumnsEnum,
+  defaultFilterExpression,
+  column2InfoSelector,
+  //
+  anchorEl,
+  buttonProps,
+  anchorOrigin,
+  transformOrigin,
+}: URLFilterDialogProps<TId, TRouter, TFilter> &
+  Pick<InternalFilterDialogProps, "anchorEl" | "buttonProps" | "transformOrigin" | "anchorOrigin">) => {
+  const column2Info = useAppSelector(column2InfoSelector);
 
-    const [expertMode, setExpertMode] = useURLConnector(routeApi, FILTER_EXPERT_MODE_PARAM);
+  const [expertMode, setExpertMode] = useURLConnector(routeApi, FILTER_EXPERT_MODE_PARAM);
+  const [filter, setFilter] = useFilterURLConnector(routeApi, filterName, FILTER_PARAM, filterColumnsEnum);
 
-    const [serializedFilter, setSerializedFilter] = useURLConnector(routeApi, FILTER_PARAM);
-    const filter = useMemo(
-      () => deserializeFilterFromSearchParam(serializedFilter, filterName),
-      [filterName, serializedFilter],
-    );
-    const setFilter = useCallback(
-      (nextFilter: MyFilter) => {
-        setSerializedFilter(serializeFilterToSearchParam(nextFilter));
-      },
-      [setSerializedFilter],
-    );
+  return (
+    <FilterDialog
+      anchorEl={anchorEl}
+      buttonProps={buttonProps}
+      anchorOrigin={anchorOrigin}
+      transformOrigin={transformOrigin}
+      filterName={filterName}
+      defaultFilterExpression={defaultFilterExpression}
+      filter={filter}
+      onFilterChange={setFilter}
+      expertMode={expertMode}
+      onExpertModeChange={setExpertMode}
+      column2Info={column2Info}
+    />
+  );
+};
 
-    return (
-      <FilterDialog
-        anchorEl={anchorEl}
-        buttonProps={buttonProps}
-        anchorOrigin={anchorOrigin}
-        transformOrigin={transformOrigin}
-        filterName={filterName}
-        defaultFilterExpression={defaultFilterExpression}
-        filter={filter}
-        onFilterChange={setFilter}
-        expertMode={expertMode}
-        onExpertModeChange={setExpertMode}
-        column2Info={column2Info}
-      />
-    );
-  },
-);
+export const URLFilterDialog = memo(URLFilterDialogComponent) as typeof URLFilterDialogComponent;
