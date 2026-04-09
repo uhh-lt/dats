1a6c93f819185bf27aad9bdb070f013648ca2248
Fynn Petersen-Frey <fynn.petersen-frey@uni-hamburg.de>
2026-04-09 13:25:51 +0000

    change analysis card colors

--- frontend/src/views/analysis/Analysis.tsx
+++ frontend/src/views/analysis/Analysis.tsx
@@ -2,6 +2,8 @@ import { Box } from "@mui/material";
 import ContentContainerLayout from "../../layouts/ContentLayouts/ContentContainerLayout.tsx";
 import AnalysisCard from "./AnalysisCard.tsx";
 
+const cardColor = "rgb(150, 200, 255)";
+
 function Analysis() {
   return (
     <ContentContainerLayout>
@@ -10,63 +12,63 @@ function Analysis() {
           to={"code-frequency"}
           title={"Code Frequency Analysis"}
           description={"Analyse the frequencies and occurrences of all codes in this project."}
-          color={"#77dd77"}
+          color={cardColor}
         />
 
         <AnalysisCard
           to={"timeline"}
           title={"Timeline Analysis"}
           description={"Analyse the occurrence of concepts over time."}
-          color={"#77dd77"}
+          color={cardColor}
         />
 
         <AnalysisCard
           to={"span-annotations"}
           title={"Span Annotation Table"}
           description={"View, search, edit span annotations in a table."}
-          color={"#77dd77"}
+          color={cardColor}
         />
 
         <AnalysisCard
           to={"sentence-annotations"}
           title={"Sentence Annotation Table"}
           description={"View, search, edit sentence annotations in a table."}
-          color={"#77dd77"}
+          color={cardColor}
         />
 
         <AnalysisCard
           to={"bbox-annotations"}
-          title={"BBox Annotation Table"}
+          title={"Bounding-Box Annotation Table"}
           description={"View, search, edit bbox annotations in a table."}
-          color={"#77dd77"}
+          color={cardColor}
         />
 
         <AnalysisCard
           to={"word-frequency"}
           title={"Word Frequency Analysis"}
           description={"Analyse the frequencies and occurrences of all words in this project."}
-          color={"#77dd77"}
+          color={cardColor}
         />
 
         <AnalysisCard
           to={"concepts-over-time-analysis"}
           title={"Concepts Over Time Analysis"}
           description={"Analyse the occurrence of concepts over time."}
-          color={"#77dd77"}
+          color={cardColor}
         />
 
         <AnalysisCard
           to={"annotation-scaling"}
           title={"Annotation Scaling"}
           description={"Semi-automatically scale annotations"}
-          color={"#77dd77"}
+          color={cardColor}
         />
 
         <AnalysisCard
           to={"tag-recommendations"}
           title={"Tag Recommendations"}
           description={"Semi-automatically scale tags"}
-          color={"#77dd77"}
+          color={cardColor}
         />
       </Box>
     </ContentContainerLayout>
