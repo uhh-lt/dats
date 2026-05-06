9ca2881c9a931de9e17933304e525d51b01d14cc
Fynn Petersen-Frey <fynn.petersen-frey@uni-hamburg.de>
2026-05-06 14:40:10 +0000

    Release v1.9.2

--- frontend/src/api/openapi/core/OpenAPI.ts
+++ frontend/src/api/openapi/core/OpenAPI.ts
@@ -21,7 +21,7 @@ export type OpenAPIConfig = {
 
 export const OpenAPI: OpenAPIConfig = {
   BASE: "",
-  VERSION: "1.9.1",
+  VERSION: "1.9.2",
   WITH_CREDENTIALS: false,
   CREDENTIALS: "include",
   TOKEN: undefined,
