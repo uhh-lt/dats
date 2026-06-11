029fe84a1d40959ee69c13686fc5e2af4422df42
Tim Fischer <tim.fischer@uni-hamburg.de>
2026-06-11 14:22:40 +0000

    fixed bug

--- backend/src/modules/search/search_dto.py
+++ backend/src/modules/search/search_dto.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, Field
 
+from core.annotation.span_annotation_dto import SpanAnnotationCreate
 from core.code.code_dto import CodeRead
 from core.doc.folder_dto import FolderRead
 from core.doc.source_document_dto import SourceDocumentRead
