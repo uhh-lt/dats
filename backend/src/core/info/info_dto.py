d14a4ef9a7ce4cfd8f2f035167a521fdf95389b3
Tim Fischer <tim.fischer@uni-hamburg.de>
2026-06-29 16:18:05 +0000

    added glitchtip info to the info endpoint. frontend can now fetch glitchtip details dynamically

--- backend/src/core/info/info_dto.py
+++ backend/src/core/info/info_dto.py
@@ -5,3 +5,5 @@ class InstanceInfo(BaseModel):
     is_oidc_enabled: bool = Field(description="Is OIDC enabled")
     oidc_provider_name: str = Field(description="OIDC provider name")
     is_stable: bool = Field(description="Is stable")
+    glitchtip_public_key: str | None = Field(description="Glitchtip public key")
+    glitchtip_project_id: int | None = Field(description="Glitchtip project ID")
