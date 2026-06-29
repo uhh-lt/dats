19319aaef44d9d52813ebfca78c970b382b9d81d
Tim Fischer <tim.fischer@uni-hamburg.de>
2026-06-29 16:23:57 +0000

    changed frontend env variable names to indicate that they are not available within the app

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,9 +1,7 @@
 /// <reference types="vite/client" />
 
-interface ImportMetaEnv {
-  readonly VITE_APP_SERVER: string;
-  readonly VITE_APP_CONTENT: string;
-}
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+interface ImportMetaEnv {}
 
 interface ImportMeta {
   readonly env: ImportMetaEnv;
