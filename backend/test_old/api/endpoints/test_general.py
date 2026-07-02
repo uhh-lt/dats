68b2535aee48e203a1233ef1e9aca421d48783a0
Tim Fischer <tim.fischer@uni-hamburg.de>
2026-07-02 21:08:04 +0000

    removed unnecessary old test

--- backend/test_old/api/endpoints/test_general.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from fastapi.testclient import TestClient
-
-
-@pytest.mark.anyio
-async def test_index(client: TestClient):
-    response = client.get("/", follow_redirects=False)
-    assert response.status_code == 307
-    assert response.headers.get("Location") == "/docs"
