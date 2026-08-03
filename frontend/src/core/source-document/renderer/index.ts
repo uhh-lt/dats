85d4de90a3b7ac9df17c57d30ac5178116921fd4
Tim Fischer <tim.fischer@uni-hamburg.de>
2026-08-03 09:19:27 +0000

    added memo indicator to search table

--- frontend/src/core/source-document/renderer/index.ts
+++ frontend/src/core/source-document/renderer/index.ts
@@ -1,5 +1,6 @@
 export * from "./SdocAnnotatorsRenderer";
 export * from "./SdocFolderRenderer";
+export * from "./SdocMemoIndicator";
 export * from "./SdocRenderer";
 export * from "./SdocSentenceRenderer";
 export * from "./SdocTagRenderer";
