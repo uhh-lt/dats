6c64dc462aa9256cfd6d4e35e6c4cc257604a059
Tim Fischer <tim.fischer@uni-hamburg.de>
2026-08-20 11:43:53 +0000

    make DATSDialogHeader more customizable

--- frontend/src/components/DATSDialogHeader.tsx
+++ frontend/src/components/DATSDialogHeader.tsx
@@ -1,33 +1,39 @@
 import { Close, Fullscreen, FullscreenExit } from "@mui/icons-material";
 import { IconButton, Stack, Toolbar, Typography } from "@mui/material";
-import { memo } from "react";
+import { memo, ReactNode } from "react";
 
 interface DialogHeaderProps {
   title: string;
   onClose: () => void;
   isMaximized: boolean;
   onToggleMaximize: () => void;
+  startActions?: ReactNode;
+  endActions?: ReactNode;
 }
 
-export const DATSDialogHeader = memo(({ title, onClose, isMaximized, onToggleMaximize }: DialogHeaderProps) => {
-  return (
-    <Toolbar
-      sx={(theme) => ({
-        backgroundColor: theme.palette.primary.main,
-        color: theme.palette.primary.contrastText,
-      })}
-    >
-      <Stack direction="row" alignItems="center" spacing={1} width="100%">
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          {title}
-        </Typography>
-        <IconButton edge="end" color="inherit" onClick={onToggleMaximize}>
-          {isMaximized ? <FullscreenExit /> : <Fullscreen />}
-        </IconButton>
-        <IconButton edge="end" color="inherit" onClick={onClose}>
-          <Close />
-        </IconButton>
-      </Stack>
-    </Toolbar>
-  );
-});
+export const DATSDialogHeader = memo(
+  ({ title, onClose, isMaximized, onToggleMaximize, startActions, endActions }: DialogHeaderProps) => {
+    return (
+      <Toolbar
+        sx={(theme) => ({
+          backgroundColor: theme.palette.primary.main,
+          color: theme.palette.primary.contrastText,
+        })}
+      >
+        <Stack direction="row" alignItems="center" spacing={1} width="100%">
+          {startActions}
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            {title}
+          </Typography>
+          {endActions}
+          <IconButton edge="end" color="inherit" onClick={onToggleMaximize}>
+            {isMaximized ? <FullscreenExit /> : <Fullscreen />}
+          </IconButton>
+          <IconButton edge="end" color="inherit" onClick={onClose}>
+            <Close />
+          </IconButton>
+        </Stack>
+      </Toolbar>
+    );
+  },
+);
