1f9864d87c3836c5d5fef9f8c673c66c475d1721
Tim Fischer <tim.fischer@uni-hamburg.de>
2026-08-20 13:31:36 +0000

    removed unused sdoc memo indicator component

--- frontend/src/core/source-document/renderer/index.ts
+++ frontend/src/core/source-document/renderer/index.ts
@@ -1,6 +1,5 @@
 export * from "./SdocAnnotatorsRenderer";
 export * from "./SdocFolderRenderer";
-export * from "./SdocMemoIndicator";
 export * from "./SdocRenderer";
 export * from "./SdocSentenceRenderer";
 export * from "./SdocTagRenderer";
