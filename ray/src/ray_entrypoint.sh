e3be0e22f3d3e5efb4987a2aa832eec5692341a9
Tim Fischer <tim.fischer@uni-hamburg.de>
2026-08-31 11:49:52 +0000

    fixed entrypoint script: it previously tried to install dev dependencies

--- ray/src/ray_entrypoint.sh
+++ ray/src/ray_entrypoint.sh
@@ -8,7 +8,7 @@ source src/install_spacy_models.sh
 
 # generate the ray spec file
 cd src || exit 1
-uv run generate_specs.py --spec_out_fp /tmp/spec.yaml || exit 1
+uv run --no-dev generate_specs.py --spec_out_fp /tmp/spec.yaml || exit 1
 
 # start the ray cluster
 ray start --head --dashboard-host '0.0.0.0'
